6f2e1249127d20aa5d0e4e94529973b293505310
Nick Schimek <cfme00@gmail.com>
2018-10-23 12:20:26 -0700

    Remove comment

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 # initialize our db
 db = SQLAlchemy()
 
-
-from flask_bcrypt import Bcrypt
-#######
-# existing code remains #
-#######
 bcrypt = Bcrypt()
