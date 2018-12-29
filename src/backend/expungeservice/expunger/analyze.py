50b202aba8772b336922e9305cc7ab9132a5f7f1
Arun C Ilango <arunc.ilango@gmail.com>
2018-12-29 06:35:47 -0800

    expunger: add type and type eligibility checks
    This patch adds functionality for running a type and time eligibility checks
    on a client's cases/charges. A few checks are added to the analysis. More to
    follow in later patches.

--- src/backend/expungeservice/expunger/analyze.py
+++ src/backend/expungeservice/expunger/analyze.py
@@ -103,6 +103,15 @@ class Charge(object):
         self.level = level
         self.date = date
         self.disposition = disposition
+        self._result = None
+
+    @property
+    def type_elig_result(self):
+        return self._result
+
+    @type_elig_result.setter
+    def type_elig_result(self, result):
+        self._result = result
 
 CaseState = enum.Enum('CaseState', 'OPEN CLOSED')
 
@@ -140,3 +149,159 @@ class Client(object):
         for case in self.cases:
             num += case.num_charges()
         return num
+
+class ResultCode(enum.Enum):
+    INELIGIBLE = 'Ineligible'
+    ELIGIBLE = 'Eligible'
+    FURTHER_ANALYSIS = 'Further analysis needed'
+    EXAMINE = 'Examine'
+    NO_ACTION = 'Take no action'
+    OPEN_CASE = 'Open Case'
+
+"""
+A class for storing individual analysis steps
+
+Attributes:
+    result: A boolean that's the result of the check
+    check: A string that breifly describes the check
+    check_desc: A string that elaborates the check (optional)
+"""
+CheckResult = collections.namedtuple('CheckResult', 'result check check_desc',
+                                     defaults=[None])
+
+class Result(object):
+    """
+    A class for storing analysis results
+
+    Attributes:
+        code: A ResultCode instance.
+        statute: A Statute instance that's associated with arrived result.
+        date: A datetime.date instance that specifies eligibility date.
+        analysis: A list of CheckResult instances that describes the analysis done.
+    """
+    def __init__(self, code=None, analysis=None,
+                 statute=None, date=None):
+        self.code = code
+        self.analysis = analysis
+        self.statute = statute
+        self.date = date
+
+    def __str__(self):
+        return ' '.join([str(self.code), str(self.analysis), str(self.statute), str(self.date)])
+
+class ResultInElig_137_225_5(Result):
+    def __init__(self, **kwargs):
+        Result.__init__(self, code=ResultCode.INELIGIBLE,
+                        statute=Statute(137, 225, 5), **kwargs)
+
+class RecordAnalyzer(object):
+    """
+    A class for analyzing client's records
+
+    Attributes:
+        client: A Client instance
+    """
+
+    def __init__(self, client):
+        self.client = client
+
+    def _is_charge_level(charge, type_, class_):
+        check = 'Is the charge a {}'.format(type_)
+        if class_:
+            check += ' class {}'.format(class_)
+
+        result = (charge.level.type_ ==  type_ and
+                  (not class_ or charge.level.class_ == class_))
+        return CheckResult(check=check, result=result)
+
+    def _is_charge_statute(charge, statute):
+        check = 'Does the charge fall under statute: ' + str(statute)
+        return CheckResult(check=check, result=charge.statute == statute)
+
+    def _is_charge_in_statute_list(charge, statutes, desc):
+        check = ('Does the charge fall under any of these statutes: ' +
+                 ','.join(str(statute) for statute in statutes))
+        # TODO implement this
+        return CheckResult(check=check, result=False, check_desc=desc)
+
+    def _is_charge_sex_crime(charge):
+        # TODO update
+        _statutes_sex_crimes = []
+        return RecordAnalyzer._is_charge_in_statute_list(
+            charge, _statutes_sex_crimes, 'Is the charge a sex crime')
+
+    def _is_charge_traffic_crime(charge):
+        # TODO update
+        _statutes_traffic_crimes = []
+        return RecordAnalyzer._is_charge_in_statute_list(
+            charge, _statutes_traffic_crimes, 'Is the charge a traffic crime')
+
+    def _have_open_case(self):
+        check = 'Is there a open case for the client'
+        result = any([case.state == CaseState.OPEN for case in self.client.cases])
+        return CheckResult(check=check, result=result)
+
+    """
+    Run Time Eligibility analysis on the client and their records
+
+    TODO: make it return analysis for each charge as well (which is supposed
+    to be an update on eligibility date and relevan statutes)
+
+    Returns:
+        An Result instance
+    """
+    def time_eligibility(self):
+        analysis = []
+
+        analysis.append(self._have_open_case())
+        if analysis[-1].result:
+            return Result(ResultCode.OPEN_CASE, analysis)
+
+        # TODO implement the rest
+
+        return Result(ResultCode.NO_ACTION)
+
+    """
+    Run Type Eligibility analysis on a charge
+
+    Args:
+        charge: A Charge instance
+
+    Returns:
+        A Result instance
+    """
+    def type_eligibility(self, charge):
+        analysis = []
+
+        analysis.append(RecordAnalyzer._is_charge_level(charge, 'Felony', 'A'))
+        if analysis[-1].result:
+            return ResultInElig_137_225_5(analysis=analysis)
+
+        analysis.append(RecordAnalyzer._is_charge_sex_crime(charge))
+        if analysis[-1].result:
+            return ResultInElig_137_225_5(analysis=analysis)
+
+        analysis.append(RecordAnalyzer._is_charge_traffic_crime(charge))
+        if analysis[-1].result:
+            return ResultInElig_137_225_5(analysis=analysis)
+
+        # TODO add remaining analysis
+
+        return Result(ResultCode.FURTHER_ANALYSIS, analysis)
+
+    """
+    Analyze which records are expungeable
+
+    The method sets the Charge instance's result attribute with type eligibility
+    analysis result.
+
+    Returns:
+        A tuple consisting of:
+            - A Result instance that describes the Time Eligibility analysis.
+    """
+    def analyze(self):
+        for case in self.client.cases:
+            for charge in case.charges:
+                charge.type_elig_result = self.type_eligibility(charge)
+
+        return self.time_eligibility()
--- src/backend/expungeservice/test/test_expunger.py
+++ src/backend/expungeservice/test/test_expunger.py
@@ -1,5 +1,6 @@
 import datetime
 import copy
+import unittest
 
 from expungeservice.expunger import *
 
@@ -28,25 +29,54 @@ def test_statute():
         else:
             assert(0)
 
+def get_convicted_disp():
+    return Disposition(DispositionType.CONVICTED, datetime.date(1996, 1, 1))
 
-def get_disp():
-    date = datetime.date(1996, 1, 1)
-    disp_type = DispositionType.CONVICTED
-    return Disposition(disp_type, date)
+def get_dummy_statute():
+    return Statute(113, 45, 5, 'd')
 
+def get_charge_crime_level(type_, class_):
+    disp = get_convicted_disp()
+    statute = get_dummy_statute()
+    level = CrimeLevel(type_, class_)
+    return Charge('%s %s charge' % (type_, class_), statute, level,
+                  datetime.date(1995, 1, 1), disp)
+
+"""
+This mainly tests if we're able to construct the objects.
+"""
 def test_expunger_classes():
-    disp = get_disp()
-    statute = Statute(113, 45, 5, 'd')
-    level = CrimeLevel('Felony', 'A')
+    disp = get_convicted_disp()
+    statute = get_dummy_statute()
     charges = [
-        Charge('Charge1', statute, level,
-               datetime.date(1995, 1, 1), disp),
-        Charge('Charge2', statute, level,
-               datetime.date(1995, 1, 1), disp)
+        get_charge_crime_level('Felony', 'A'),
+        get_charge_crime_level('Felony', 'A'),
     ]
     cases = [
         Case(charges, CaseState.OPEN, 100.50),
-        Case(charges, CaseState.CLOSED, 100.50),
+        Case(charges, CaseState.CLOSED, 0),
     ]
     client = Client('John Doe', datetime.date(1970, 1, 1), cases)
     assert(client.num_charges() == 4)
+
+class TestExpunger(unittest.TestCase):
+    def setUp(self):
+        # add charge(s) when using this in a test
+        self.open_case = Case(None, CaseState.OPEN, 0)
+        self.closed_case = Case(None, CaseState.CLOSED, 0)
+        # add case(s) when using this in a test
+        self.client = Client('John Doe', datetime.date(1970, 1, 1), None)
+        self.statute_137_225_5 = Statute(137, 225, 5)
+
+    def test_type_elig_felony(self):
+        record_analyzer = RecordAnalyzer(self.client)
+        result = record_analyzer.type_eligibility(
+                    get_charge_crime_level('Felony', 'A'))
+        assert(result.code == ResultCode.INELIGIBLE)
+        assert(result.statute == self.statute_137_225_5)
+
+    def test_time_elig_open_case(self):
+        self.client.cases = [self.closed_case, self.open_case]
+        record_analyzer = RecordAnalyzer(self.client)
+        result = record_analyzer.time_eligibility()
+        assert(result.code == ResultCode.OPEN_CASE)
