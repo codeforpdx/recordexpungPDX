23631fb1cfc628a6ff2fd7235a7048a6eecfd95b
Nick Schimek <cfme00@gmail.com>
2019-01-20 13:54:01 -0800

    Add initial search functionality
    In order to navigate to the search page the crawler needs to collect the
    payload data, via the node parser, from the response page after logging
    in to the site. This collects the required payload and submits a POST
    request to navigate to the search page.

--- src/backend/expungeservice/crawler/crawler.py
+++ src/backend/expungeservice/crawler/crawler.py
@@ -1,5 +1,6 @@
 import requests
 
+from expungeservice.crawler.parsers.node_parser import NodeParser
 from expungeservice.crawler.parsers.record_parser import RecordParser
 from expungeservice.crawler.request import *
 
@@ -17,6 +18,16 @@ class Crawler:
 
         self.response = self.session.post(url, data=payload)
         return Crawler.__login_validation(self.response, url)
+
+    def search(self, first_name, last_name, middle_name='', birth_date=''):
+        url = 'https://publicaccess.courts.oregon.gov/PublicAccessLogin/Search.aspx?ID=100'
+        node_response = self.__parse_nodes(url)
+
+    def __parse_nodes(self, url):
+        node_parser = NodeParser()
+        node_parser.feed(self.response.text)
+        payload = {'NodeID': node_parser.node_id, 'NodeDesc': 'All+Locations'}
+        return self.session.post(url, data=payload)
 
     @staticmethod
     def __login_validation(response, login_url):
