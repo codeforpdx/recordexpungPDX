679583efc1aec2aaa114052e082829e50c42de98
Nick Schimek <cfme00@gmail.com>
2019-01-20 13:59:22 -0800

    Create payload for search request
    Once on the search page the response is parsed to create the payload for
    the search request.

--- src/backend/expungeservice/crawler/crawler.py
+++ src/backend/expungeservice/crawler/crawler.py
@@ -1,5 +1,6 @@
 import requests
 
+from expungeservice.crawler.parsers.param_parser import ParamParser
 from expungeservice.crawler.parsers.node_parser import NodeParser
 from expungeservice.crawler.parsers.record_parser import RecordParser
 from expungeservice.crawler.request import *
@@ -22,12 +23,19 @@ class Crawler:
     def search(self, first_name, last_name, middle_name='', birth_date=''):
         url = 'https://publicaccess.courts.oregon.gov/PublicAccessLogin/Search.aspx?ID=100'
         node_response = self.__parse_nodes(url)
+        payload = Crawler.__extract_payload(node_response, last_name, first_name, middle_name, birth_date)
 
     def __parse_nodes(self, url):
         node_parser = NodeParser()
         node_parser.feed(self.response.text)
         payload = {'NodeID': node_parser.node_id, 'NodeDesc': 'All+Locations'}
         return self.session.post(url, data=payload)
+
+    @staticmethod
+    def __extract_payload(node_response, last_name, first_name, middle_name, birth_date):
+        param_parser = ParamParser()
+        param_parser.feed(node_response.text)
+        return Payload.payload(param_parser, last_name, first_name, middle_name, birth_date)
 
     @staticmethod
     def __login_validation(response, login_url):
