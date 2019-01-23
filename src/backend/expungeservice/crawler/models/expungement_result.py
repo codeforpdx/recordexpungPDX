e5151113bdb404837f5099e7182e19c144f27f2c
Nick Schimek <cfme00@gmail.com>
2019-01-22 21:30:16 -0800

    Add mutator methods
    Added mutator methods for expungement result attributes.

--- src/backend/expungeservice/crawler/models/expungement_result.py
+++ src/backend/expungeservice/crawler/models/expungement_result.py
@@ -3,3 +3,9 @@ class ExpungementResult:
     def __init__(self):
         self.type_eligibility = False
         self.reason = 'Ineligible under 137.225(5)'
+
+    def set_type_eligibility(self, type_eligibility):
+        self.type_eligibility = type_eligibility
+
+    def set_reason(self, reason):
+        self.reason = reason
