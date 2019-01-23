400ab9bb3cfd8068d37813d6ec1ca8a6495ddb83
Nick Schimek <cfme00@gmail.com>
2019-01-22 21:44:40 -0800

    Add check for acquittal charges

--- src/backend/expungeservice/expungement_analyzer/type_analyzer.py
+++ src/backend/expungeservice/expungement_analyzer/type_analyzer.py
@@ -1,6 +1,6 @@
 class TypeAnalyzer:
 
     def evaluate(self, charges):
-        if charges[0].disposition.ruling == 'Dismissed':
+        if charges[0].disposition.ruling == 'Dismissed' or charges[0].disposition.ruling == 'Acquitted':
             charges[0].expungement_result.set_type_eligibility(True)
             charges[0].expungement_result.set_reason('Eligible under 137.225(1)(b)')
