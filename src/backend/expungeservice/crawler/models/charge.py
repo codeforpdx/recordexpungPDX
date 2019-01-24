10c32ff8926cc79349d9e2e1a019fb4117ec17de
Nick Schimek <cfme00@gmail.com>
2019-01-24 09:36:21 -0800

    Normalize charge statute at initialization
    Statutes are 8 characters long and include one '.' and two parentheses,
    however they're persisted to the API's database inconsistently. Created
    a method to strip the statute of all characters that are not
    alphanumeric. This should enable us to consistently extract the
    different parts of the statute for analysis.

--- src/backend/expungeservice/crawler/models/charge.py
+++ src/backend/expungeservice/crawler/models/charge.py
@@ -1,3 +1,5 @@
+import re
+
 from expungeservice.crawler.models.disposition import Disposition
 
 
@@ -5,7 +7,11 @@ class Charge:
 
     def __init__(self, name, statute, level, date):
         self.name = name
-        self.statute = statute
+        self.statute = Charge.__strip_non_alphanumeric_chars(statute)
         self.level = level
         self.date = date
         self.disposition = Disposition()
+
+    @staticmethod
+    def __strip_non_alphanumeric_chars(statute):
+        return re.sub(r'[^a-zA-Z0-9*]', '', statute)
