f0021dfa8d629be35e4623eeea64b325b66c2731
Nick Schimek <cfme00@gmail.com>
2019-01-24 09:56:43 -0800

    Add tests for normalizing statute

---
+++ src/backend/tests/models/test_charge.py
@@ -0,0 +1,20 @@
+import unittest
+
+from expungeservice.crawler.models.charge import Charge
+
+class TestCaseWithDisposition(unittest.TestCase):
+
+    def setUp(self):
+        self.charge = {'name': 'PCS', 'level': 'Misdemeanor', 'date': '12/12/1999'}
+
+    def test_it_initializes_simple_statute(self):
+        self.charge['statute'] = '1231235b'
+        charge = Charge(**self.charge)
+
+        assert charge.statute == '1231235b'
+
+    def test_it_normalizes_statute(self):
+        self.charge['statute'] = '-123.123(5)(b)'
+        charge = Charge(**self.charge)
+
+        assert charge.statute == '1231235b'
