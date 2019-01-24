280e14de67e99de96c1d7af933c49e25607576e4
Nick Schimek <cfme00@gmail.com>
2019-01-23 22:18:17 -0800

    Add felony class C level check

--- src/backend/expungeservice/expungement_analyzer/type_analyzer.py
+++ src/backend/expungeservice/expungement_analyzer/type_analyzer.py
@@ -4,3 +4,8 @@ class TypeAnalyzer:
         if charges[0].disposition.ruling == 'Dismissed' or charges[0].disposition.ruling == 'Acquitted' or charges[0].disposition.ruling == 'No Complaint':
             charges[0].expungement_result.set_type_eligibility(True)
             charges[0].expungement_result.set_reason('Eligible under 137.225(1)(b)')
+
+        elif charges[0].level == 'Felony Class C':
+            charges[0].expungement_result.set_type_eligibility(True)
+            charges[0].expungement_result.set_reason('Eligible under 137.225(5)(b)')
+
