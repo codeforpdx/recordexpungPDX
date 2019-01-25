08a983b53a6f40714a2e1161a57b9e9a784a8739
Nick Schimek <cfme00@gmail.com>
2019-01-25 15:45:04 -0800

    Add crimes against person ranges to check through
    Sex crimes fall into four categories, and each category has a range of
    statutes. This checks if the statute is within that range and marks it
    as ineligible.

--- src/backend/expungeservice/expungement_analyzer/type_analyzer.py
+++ src/backend/expungeservice/expungement_analyzer/type_analyzer.py
@@ -5,6 +5,15 @@ class TypeAnalyzer:
             charges[0].expungement_result.set_type_eligibility(True)
             charges[0].expungement_result.set_reason('Eligible under 137.225(1)(b)')
 
+        elif TypeAnalyzer.__crime_against_person(charges):
+            pass
+
         elif charges[0].level == 'Felony Class C' or charges[0].level == 'Misdemeanor Class C':
             charges[0].expungement_result.set_type_eligibility(True)
             charges[0].expungement_result.set_reason('Eligible under 137.225(5)(b)')
+
+    @staticmethod
+    def __crime_against_person(charges):
+        statute = int(charges[0].statute[0:6])
+        statute_ranges = (range(163305, 163480), range(163670, 163694), range(167008, 167108), range(167057, 167081))
+        return any(statute in statute_range for statute_range in statute_ranges)
