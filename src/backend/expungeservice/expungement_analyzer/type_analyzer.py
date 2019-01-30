f498c2db39f0e1f14552f2eeaccc0655eb9f42d1
Nick Schimek <cfme00@gmail.com>
2019-01-30 11:48:11 -0800

    Add check for marijuana ineligible charges
    Created static list of marijuana ineligible statutes to check against.

--- src/backend/expungeservice/expungement_analyzer/type_analyzer.py
+++ src/backend/expungeservice/expungement_analyzer/type_analyzer.py
@@ -5,6 +5,10 @@ class TypeAnalyzer:
             charges[0].expungement_result.set_type_eligibility(True)
             charges[0].expungement_result.set_reason('Eligible under 137.225(1)(b)')
 
+        elif TypeAnalyzer.__marijuana_ineligible(charges):
+            charges[0].expungement_result.set_type_eligibility(False)
+            charges[0].expungement_result.set_reason('Ineligible under 137.226')
+
         elif TypeAnalyzer.__crime_against_person(charges):
             pass
 
@@ -17,3 +21,8 @@ class TypeAnalyzer:
         statute = int(charges[0].statute[0:6])
         statute_ranges = (range(163305, 163480), range(163670, 163694), range(167008, 167108), range(167057, 167081))
         return any(statute in statute_range for statute_range in statute_ranges)
+
+    @staticmethod
+    def __marijuana_ineligible(charges):
+        ineligible_statutes = ['475B3493C', '475B359', '475B367', '475B371', '167262']
+        return charges[0].statute in ineligible_statutes
