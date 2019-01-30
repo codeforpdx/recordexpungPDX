f5e450d54d0911be99089dfde9a3a1cde33e1415
Nick Schimek <cfme00@gmail.com>
2019-01-30 11:47:27 -0800

    Add marijuana ineligible tests

--- src/backend/tests/expungement_analyzer/test_type_analyzer_convicted_charges.py
+++ src/backend/tests/expungement_analyzer/test_type_analyzer_convicted_charges.py
@@ -166,3 +166,58 @@ class TestSingleChargeConvictions(unittest.TestCase):
 
         assert sex_crime_charge.expungement_result.type_eligibility is False
         assert sex_crime_charge.expungement_result.reason == 'Ineligible under 137.225(5)'
+
+    def test_marijuana_ineligible_statute_475b3493c(self):
+        self.single_charge['name'] = 'Unlawful Manufacture of Marijuana Item'
+        self.single_charge['statute'] = '475B.349(3)(C)'
+        self.single_charge['level'] = 'Felony Class C'
+        marijuana_felony_class_c = self.create_recent_charge()
+        self.charges.append(marijuana_felony_class_c)
+        self.type_analyzer.evaluate(self.charges)
+
+        assert marijuana_felony_class_c.expungement_result.type_eligibility is False
+        assert marijuana_felony_class_c.expungement_result.reason == 'Ineligible under 137.226'
+
+    def test_marijuana_ineligible_statute_475b359(self):
+        self.single_charge['name'] = 'Arson incident to manufacture of cannabinoid extract in first degree'
+        self.single_charge['statute'] = '475b.359'
+        self.single_charge['level'] = 'Felony Class A'
+        marijuana_felony_class_a = self.create_recent_charge()
+        self.charges.append(marijuana_felony_class_a)
+        self.type_analyzer.evaluate(self.charges)
+
+        assert marijuana_felony_class_a.expungement_result.type_eligibility is False
+        assert marijuana_felony_class_a.expungement_result.reason == 'Ineligible under 137.226'
+
+    def test_marijuana_ineligible_statute_475b367(self):
+        self.single_charge['name'] = 'Causing another person to ingest marijuana'
+        self.single_charge['statute'] = '475B.367'
+        self.single_charge['level'] = 'Felony Class A'
+        marijuana_felony_class_a = self.create_recent_charge()
+        self.charges.append(marijuana_felony_class_a)
+        self.type_analyzer.evaluate(self.charges)
+
+        assert marijuana_felony_class_a.expungement_result.type_eligibility is False
+        assert marijuana_felony_class_a.expungement_result.reason == 'Ineligible under 137.226'
+
+    def test_marijuana_ineligible_statute_475b371(self):
+        self.single_charge['name'] = 'Administration to another person under 18 years of age'
+        self.single_charge['statute'] = '475B.371'
+        self.single_charge['level'] = 'Felony Class A'
+        marijuana_felony_class_a = self.create_recent_charge()
+        self.charges.append(marijuana_felony_class_a)
+        self.type_analyzer.evaluate(self.charges)
+
+        assert marijuana_felony_class_a.expungement_result.type_eligibility is False
+        assert marijuana_felony_class_a.expungement_result.reason == 'Ineligible under 137.226'
+
+    def test_marijuana_ineligible_statute_167262(self):
+        self.single_charge['name'] = 'Use of minor in controlled substance or marijuana item offense'
+        self.single_charge['statute'] = '167.262'
+        self.single_charge['level'] = 'Misdemeanor Class A'
+        marijuana_misdemeanor_class_a = self.create_recent_charge()
+        self.charges.append(marijuana_misdemeanor_class_a)
+        self.type_analyzer.evaluate(self.charges)
+
+        assert marijuana_misdemeanor_class_a.expungement_result.type_eligibility is False
+        assert marijuana_misdemeanor_class_a.expungement_result.reason == 'Ineligible under 137.226'
