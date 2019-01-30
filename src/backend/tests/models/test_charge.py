47748055916957ca989c259cca8d10c8b2eea29a
Nick Schimek <cfme00@gmail.com>
2019-01-30 12:36:31 -0800

    Add test to check for uppercase statute

--- src/backend/tests/models/test_charge.py
+++ src/backend/tests/models/test_charge.py
@@ -2,19 +2,26 @@ import unittest
 
 from expungeservice.crawler.models.charge import Charge
 
+
 class TestCaseWithDisposition(unittest.TestCase):
 
     def setUp(self):
         self.charge = {'name': 'PCS', 'level': 'Misdemeanor', 'date': '12/12/1999'}
 
     def test_it_initializes_simple_statute(self):
-        self.charge['statute'] = '1231235b'
+        self.charge['statute'] = '1231235B'
         charge = Charge(**self.charge)
 
-        assert charge.statute == '1231235b'
+        assert charge.statute == '1231235B'
 
     def test_it_normalizes_statute(self):
-        self.charge['statute'] = '-123.123(5)(b)'
+        self.charge['statute'] = '-123.123(5)()B'
         charge = Charge(**self.charge)
 
-        assert charge.statute == '1231235b'
+        assert charge.statute == '1231235B'
+
+    def test_it_converts_statute_to_uppercase(self):
+        self.charge['statute'] = '-123.123(5)()b'
+        charge = Charge(**self.charge)
+
+        assert charge.statute == '1231235B'
