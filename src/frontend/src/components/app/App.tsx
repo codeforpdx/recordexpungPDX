b2c8c8f37942dfd55f049c2ddfd48491190188b8
Max Wallace <maxfield.wallace@gmail.com>
2019-04-26 16:38:17 -0700

    Merge new/old frontend

--- src/frontend/src/components/app/App.tsx
+++ src/frontend/src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { FooterComponent } from "../footer/footer.component";
 import { LoginComponent } from "../login/login.component";
 
--- src/new-frontend/src/redux/store.tsx
+++
@@ -1,9 +0,0 @@
-// See https://redux-starter-kit.js.org/usage/usage-guide
-import { configureStore } from 'redux-starter-kit'
-import rootReducer from './reducers/index';
-
-const store = configureStore({
-  reducer: rootReducer
-})
-
-export default store;
