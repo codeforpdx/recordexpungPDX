1e3e9cce769609477c44b2834a704a0d9f332c2f
Max Wallace <maxfield.wallace@gmail.com>
2019-04-26 15:32:31 -0700

    hello world

--- src/new-frontend/src/index.tsx
+++ src/new-frontend/src/index.tsx
@@ -1,10 +1,22 @@
+// These imports are all defaults from create-react-app.
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// The following is based on the example code at
+// https://react-redux.js.org/introduction/quick-start
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
