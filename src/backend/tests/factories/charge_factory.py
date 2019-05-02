8f5b8c255189bb0a035ab73be78aae3bd8233c72
Nick Schimek <cfme00@gmail.com>
2019-05-01 18:31:08 -0700

    Implement charge knows its case

--- src/backend/tests/factories/charge_factory.py
+++ src/backend/tests/factories/charge_factory.py
@@ -1,15 +1,17 @@
 from expungeservice.crawler.models.charge import Charge
+from tests.factories.case_factory import CaseFactory
 
 
 class ChargeFactory:
 
     @staticmethod
-    def create(name='Theft of services',
+    def create(case=CaseFactory.create(),
+               name='Theft of services',
                statute='164.125',
                level='Misdemeanor Class A',
                date='1/1/0001',
                disposition=None):
-        charge = Charge(name, statute, level, date)
+        charge = Charge(case, name, statute, level, date)
         if disposition:
             charge.disposition.ruling, charge.disposition.date = disposition
 
