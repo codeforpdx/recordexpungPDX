4c9c1c996e08a4ff30d3f6c18eba0bab51d25fdd
Nick Schimek <cfme00@gmail.com>
2019-05-05 13:39:54 -0700

    Create test for most recent convictions
    Most recent conviction is the latest conviction from the last 10 years.
    Second MRC is the next latest conviction from the last 10 years.

--- src/backend/tests/expungement_analyzer/test_expunger.py
+++ src/backend/tests/expungement_analyzer/test_expunger.py
@@ -3,12 +3,15 @@ import unittest
 from datetime import date
 from dateutil.relativedelta import relativedelta
 from expungeservice.expungement_analyzer.expunger import Expunger
+from expungeservice.crawler.models.disposition import Disposition
 from tests.factories.case_factory import CaseFactory
 from tests.factories.charge_factory import ChargeFactory
 
 
 class TestExpungementAnalyzerUnitTests(unittest.TestCase):
 
+    TEN_YEARS = (date.today() + relativedelta(years=-10)).strftime('%m/%d/%Y')
+    LESS_THAN_TEN_YEARS_AGO = (date.today() + relativedelta(years=-10, days=+1)).strftime('%m/%d/%Y')
     LESS_THAN_THREE_YEARS_AGO = (date.today() + relativedelta(years=-3, days=+1)).strftime('%m/%d/%Y')
     THREE_YEARS_AGO = (date.today() + relativedelta(years=-3)).strftime('%m/%d/%Y')
     TWO_YEARS_AGO = (date.today() + relativedelta(years=-2)).strftime('%m/%d/%Y')
@@ -55,3 +58,68 @@ class TestExpungementAnalyzerUnitTests(unittest.TestCase):
         expunger.run()
 
         assert expunger._most_recent_dismissal is mrd_charge
+
+    def test_it_sets_most_recent_conviction_from_the_last_10yrs(self):
+        case = CaseFactory.create()
+        mrc_charge = ChargeFactory.create()
+        mrc_charge.disposition = Disposition(self.LESS_THAN_TEN_YEARS_AGO, 'Convicted')
+        case.charges = [mrc_charge]
+
+        expunger = Expunger([case])
+        expunger.run()
+
+        assert expunger._most_recent_conviction is mrc_charge
+
+    def test_it_sets_the_second_most_recent_conviction_within_the_last_10yrs(self):
+        case = CaseFactory.create()
+        mrc_charge = ChargeFactory.create()
+        mrc_charge.disposition = Disposition(self.TWO_YEARS_AGO, 'Convicted')
+        second_mrc_charge = ChargeFactory.create()
+        second_mrc_charge.disposition = Disposition(self.LESS_THAN_TEN_YEARS_AGO, 'Convicted')
+
+        case.charges = [second_mrc_charge, mrc_charge]
+
+        expunger = Expunger([case])
+        expunger.run()
+
+        assert expunger._second_most_recent_conviction is second_mrc_charge
+
+    def test_it_does_not_set_mrc_when_greater_than_10yrs(self):
+        case = CaseFactory.create()
+        mrc_charge = ChargeFactory.create()
+        mrc_charge.disposition = Disposition(self.TEN_YEARS, 'Convicted')
+        case.charges = [mrc_charge]
+
+        expunger = Expunger([case])
+        expunger.run()
+
+        assert expunger._most_recent_conviction is None
+
+    def test_it_does_not_set_2nd_mrc_when_greater_than_10yrs(self):
+        case = CaseFactory.create()
+        mrc_charge = ChargeFactory.create()
+        mrc_charge.disposition = Disposition(self.LESS_THAN_TEN_YEARS_AGO, 'Convicted')
+        second_mrc_charge = ChargeFactory.create()
+        second_mrc_charge.disposition = Disposition(self.TEN_YEARS, 'Convicted')
+        case.charges = [mrc_charge, second_mrc_charge]
+
+        expunger = Expunger([case])
+        expunger.run()
+
+        assert expunger._second_most_recent_conviction is None
+
+    def test_mrc_and_second_mrc(self):
+        case = CaseFactory.create()
+        mrc_charge = ChargeFactory.create()
+        mrc_charge.disposition = Disposition(self.TWO_YEARS_AGO, 'Convicted')
+        second_mrc_charge = ChargeFactory.create()
+        second_mrc_charge.disposition = Disposition(self.LESS_THAN_TEN_YEARS_AGO, 'Convicted')
+        charge = ChargeFactory.create()
+        charge.disposition = Disposition(self.TEN_YEARS, 'Convicted')
+        case.charges = [charge, charge, second_mrc_charge, mrc_charge, charge, charge]
+
+        expunger = Expunger([case])
+        expunger.run()
+
+        assert expunger._most_recent_conviction is mrc_charge
+        assert expunger._second_most_recent_conviction is second_mrc_charge
