ffb8bb262f7bfe6ab32e67f9f571a836a94f6505
Nick Schimek <cfme00@gmail.com>
2019-05-05 20:11:53 -0700

    Create recent conviction method

--- src/backend/tests/models/test_charge.py
+++ src/backend/tests/models/test_charge.py
@@ -1,10 +1,16 @@
 import unittest
 
+from datetime import date
+from dateutil.relativedelta import relativedelta
+from expungeservice.crawler.models.disposition import Disposition
 from tests.factories.charge_factory import ChargeFactory
 from tests.factories.case_factory import CaseFactory
 
 
-class TestCaseWithDisposition(unittest.TestCase):
+class TestChargeClass(unittest.TestCase):
+
+    TEN_YEARS = (date.today() + relativedelta(years=-10)).strftime('%m/%d/%Y')
+    LESS_THAN_TEN_YEARS_AGO = (date.today() + relativedelta(years=-10, days=+1)).strftime('%m/%d/%Y')
 
     def setUp(self):
         self.charge = ChargeFactory.build()
@@ -32,3 +38,21 @@ class TestCaseWithDisposition(unittest.TestCase):
         charge = ChargeFactory.create(case=case)
 
         assert charge.case()() is case
+
+    def test_it_knows_if_it_has_a_recent_conviction_happy_path(self):
+        charge = ChargeFactory.create()
+        charge.disposition = Disposition(self.LESS_THAN_TEN_YEARS_AGO, 'Convicted')
+
+        assert charge.recent_conviction() is True
+
+    def test_it_knows_if_it_has_a_recent_conviction_sad_path(self):
+        charge = ChargeFactory.create()
+        charge.disposition = Disposition(self.TEN_YEARS, 'Convicted')
+
+        assert charge.recent_conviction() is False
+
+    def test_dismissed_charge_is_not_a_recent_conviction(self):
+        charge = ChargeFactory.create()
+        charge.disposition = Disposition(self.LESS_THAN_TEN_YEARS_AGO, 'Dismissed')
+
+        assert charge.recent_conviction() is False
