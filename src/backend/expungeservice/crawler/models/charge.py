9dea881f9a9c9a88569a69a2c00096aab467b2dc
Nick Schimek <cfme00@gmail.com>
2019-05-05 20:11:53 -0700

    Create recent conviction method

--- src/backend/expungeservice/crawler/models/charge.py
+++ src/backend/expungeservice/crawler/models/charge.py
@@ -1,7 +1,9 @@
 import re
 import weakref
-from datetime import datetime
 
+from datetime import datetime
+from datetime import date as date_class
+from dateutil.relativedelta import relativedelta
 from expungeservice.crawler.models.disposition import Disposition
 from expungeservice.crawler.models.expungement_result import ExpungementResult
 
@@ -22,6 +24,10 @@ class Charge:
 
     def acquitted(self):
         return self.disposition.ruling[0:9] != 'Convicted'
+
+    def recent_conviction(self):
+        ten_years_ago = (date_class.today() + relativedelta(years=-10))
+        return not self.acquitted() and self.disposition.date > ten_years_ago
 
     @staticmethod
     def __strip_non_alphanumeric_chars(statute):
