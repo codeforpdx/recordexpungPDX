f10a480ba330f76f36d4414f91f575398842c5b2
Nick Schimek <cfme00@gmail.com>
2019-05-05 20:34:53 -0700

    Refactor most recent acquittal methods
    Moved most recent acquittal to charge class and unit tested it.

--- src/backend/tests/models/test_charge.py
+++ src/backend/tests/models/test_charge.py
@@ -9,8 +9,10 @@ from tests.factories.case_factory import CaseFactory
 
 class TestChargeClass(unittest.TestCase):
 
-    TEN_YEARS = (date.today() + relativedelta(years=-10)).strftime('%m/%d/%Y')
+    TEN_YEARS_AGO = (date.today() + relativedelta(years=-10)).strftime('%m/%d/%Y')
     LESS_THAN_TEN_YEARS_AGO = (date.today() + relativedelta(years=-10, days=+1)).strftime('%m/%d/%Y')
+    LESS_THAN_THREE_YEARS_AGO = (date.today() + relativedelta(years=-3, days=+1)).strftime('%m/%d/%Y')
+    THREE_YEARS_AGO = (date.today() + relativedelta(years=-3)).strftime('%m/%d/%Y')
 
     def setUp(self):
         self.charge = ChargeFactory.build()
@@ -47,7 +49,7 @@ class TestChargeClass(unittest.TestCase):
 
     def test_it_knows_if_it_has_a_recent_conviction_sad_path(self):
         charge = ChargeFactory.create()
-        charge.disposition = Disposition(self.TEN_YEARS, 'Convicted')
+        charge.disposition = Disposition(self.TEN_YEARS_AGO, 'Convicted')
 
         assert charge.recent_conviction() is False
 
@@ -56,3 +58,21 @@ class TestChargeClass(unittest.TestCase):
         charge.disposition = Disposition(self.LESS_THAN_TEN_YEARS_AGO, 'Dismissed')
 
         assert charge.recent_conviction() is False
+
+    def test_most_recent_acquittal_happy_path(self):
+        charge = ChargeFactory.create(date=self.LESS_THAN_THREE_YEARS_AGO)
+        charge.disposition = Disposition(ruling='Dismissed')
+
+        assert charge.recent_acquittal() is True
+
+    def test_most_recent_acquittal_sad_path(self):
+        charge = ChargeFactory.create(date=self.THREE_YEARS_AGO)
+        charge.disposition = Disposition(ruling='Dismissed')
+
+        assert charge.recent_acquittal() is False
+
+    def test_convicted_charge_is_not_a_recent_acquittal(self):
+        charge = ChargeFactory.create(date=self.LESS_THAN_THREE_YEARS_AGO)
+        charge.disposition = Disposition(ruling='Convicted')
+
+        assert charge.recent_acquittal() is False
