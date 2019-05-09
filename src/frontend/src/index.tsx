855c1add8a70ba40e8d24b28a65dc0fe280a9f10
Max Wallace <maxfield.wallace@gmail.com>
2019-05-08 20:46:49 -0700

    Remove redundant Icon Component

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -18,6 +18,9 @@ ReactDOM.render(
   rootElement
 )
 
+// Service Workers are intentionally turned off; they were causing
+// issues when testing URLs.
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
