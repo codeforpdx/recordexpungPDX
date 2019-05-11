ab9e197f32a1e3941e7065255d36fea32bb72262
Chris Breaux <christopherjbreaux@gmail.com>
2019-05-11 12:14:09 -0700

    add wrapper

--- src/frontend/src/components/RecordSearch/index.tsx
+++ src/frontend/src/components/RecordSearch/index.tsx
@@ -11,37 +11,39 @@ import './styles.scss';
 class RecordSearch extends React.Component {
   public render() {
     return (
-      <section className="cf mt4 mb3 pa3 pa4-l bg-white shadow br3">
-        <h1 className="mb4 f4 fw6">Record Search</h1>
-        <form>
-          <div className="flex flex-wrap items-end">
-            <div className="w-100 w-30-ns mb3 pr2-ns">
-              <label htmlFor="first-name" className="db mb1 fw6">
-                First Name
-              </label>
-              <input id="first-name" type="text" className="w-100 pa3 br2 b--black-20" required aria-invalid="false"/>
+      <main className='mw8 center ph2'>
+        <section className="cf mt4 mb3 pa3 pa4-l bg-white shadow br3">
+          <h1 className="mb4 f4 fw6">Record Search</h1>
+          <form>
+            <div className="flex flex-wrap items-end">
+              <div className="w-100 w-30-ns mb3 pr2-ns">
+                <label htmlFor="first-name" className="db mb1 fw6">
+                  First Name
+                </label>
+                <input id="first-name" type="text" className="w-100 pa3 br2 b--black-20" required aria-invalid="false"/>
+              </div>
+              <div className="w-100 w-30-ns mb3 pr2-ns">
+                <label htmlFor="last-name" className="db mb1 fw6">
+                  Last Name
+                </label>
+                <input id="last-name" type="text" className="w-100 pa3 br2 b--black-20" required aria-invalid="false"/>
+              </div>
+              <div className="w-100 w-30-ns mb3 pr2-ns">
+                <label htmlFor="date-of-birth" className="db mb1 fw6">
+                  Date of Birth <span className="fw2 f6">MM/DD/YYYY</span>
+                </label>
+                <input id="date-of-birth" type="text" className="w-100 pa3 br2 b--black-20" required aria-describedby="dob_msg" aria-invalid="false"/>
+              </div>
+              <div className="w-100 w-10-ns mb3">
+                <button className="br2 bg-blue white bg-animate hover-bg-dark-blue db w-100 tc pv3 btn--search" type="submit">
+                  <span className="visually-hidden">Search Records</span>
+                  <i aria-hidden="true" className="fas fa-search"></i>
+                </button>
+              </div>
             </div>
-            <div className="w-100 w-30-ns mb3 pr2-ns">
-              <label htmlFor="last-name" className="db mb1 fw6">
-                Last Name
-              </label>
-              <input id="last-name" type="text" className="w-100 pa3 br2 b--black-20" required aria-invalid="false"/>
-            </div>
-            <div className="w-100 w-30-ns mb3 pr2-ns">
-              <label htmlFor="date-of-birth" className="db mb1 fw6">
-                Date of Birth <span className="fw2 f6">MM/DD/YYYY</span>
-              </label>
-              <input id="date-of-birth" type="text" className="w-100 pa3 br2 b--black-20" required aria-describedby="dob_msg" aria-invalid="false"/>
-            </div>
-            <div className="w-100 w-10-ns mb3">
-              <button className="br2 bg-blue white bg-animate hover-bg-dark-blue db w-100 tc pv3 btn--search" type="submit">
-                <span className="visually-hidden">Search Records</span>
-                <i aria-hidden="true" className="fas fa-search"></i>
-              </button>
-            </div>
-          </div>
-        </form>
-      </section>
+          </form>
+        </section>
+      </main>
     );
   }
 }
