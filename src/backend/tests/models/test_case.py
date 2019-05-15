8a4e5fb94f8dc33751a8956d148d90e1da9d372b
Nick Schimek <cfme00@gmail.com>
2019-05-15 15:57:04 -0700

    Add test for checking inactive cases are closed

--- src/backend/tests/models/test_case.py
+++ src/backend/tests/models/test_case.py
@@ -1,6 +1,7 @@
 import unittest
 
 from expungeservice.models.case import Case
+from tests.factories.case_factory import CaseFactory
 
 
 class TestCaseBalanceDue(unittest.TestCase):
@@ -21,3 +22,21 @@ class TestCaseBalanceDue(unittest.TestCase):
 
         self.case.set_balance_due("0")
         assert self.case.get_balance_due() == 0  
+
+
+class TestCaseClosedMethod(unittest.TestCase):
+
+    def setUp(self):
+        self.case = CaseFactory.create()
+
+    def test_it_returns_true_for_a_closed_case(self):
+        self.case.current_status = 'Closed'
+        assert self.case.closed() is True
+
+    def test_it_returns_false_for_an_open_case(self):
+        self.case.current_status = 'Open'
+        assert self.case.closed() is False
+
+    def test_it_returns_true_for_an_inactive_case(self):
+        self.case.current_status = 'Inactive'
+        assert self.case.closed() is True
