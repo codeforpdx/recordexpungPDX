5a6b195642df96e47ff798fd746fbc6580d0204c
Jordan Witte <wittejm@gmail.com>
2019-06-01 20:21:18 -0700

    sql function code in its own file

--- config/postgres/initdb/scripts/create-tables.sql
+++ config/postgres/initdb/scripts/create-tables.sql
@@ -60,22 +60,3 @@ CREATE TABLE analyses (
     user_id        UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
     PRIMARY KEY (client_id, case_id)
 );
-
-
-CREATE FUNCTION public.CREATE_USER( email citext, admin boolean, hashed_password text) 
-RETURNS TABLE (USER_ID VARCHAR, EMAIL CITEXT, ADMIN BOOLEAN, AUTH_ID VARCHAR, HASHED_PASSWORD TEXT)
-as $$
-WITH USER_INSERT_RESULT AS 
-            (
-            INSERT INTO USERS (user_id, email, admin)
-            VALUES ( uuid_generate_v4(), $1, $2)
-            RETURNING user_id)
-            INSERT INTO AUTH (auth_id, hashed_password, user_id)
-            SELECT uuid_generate_v4(), $3, user_id FROM USER_INSERT_RESULT;
-            
-SELECT USERS.USER_ID::varchar, EMAIL, ADMIN, AUTH_ID::varchar, HASHED_PASSWORD 
-FROM USERS JOIN AUTH ON USERS.USER_ID = AUTH.USER_ID 
-WHERE EMAIL=$1;
-$$
-LANGUAGE SQL;
-
