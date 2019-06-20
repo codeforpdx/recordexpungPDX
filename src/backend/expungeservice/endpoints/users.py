c03b825ccbab0f93fa03b8b4d632b5a00f5ca5a9
Jordan Witte <wittejm@gmail.com>
2019-06-20 15:18:43 -0700

    users endpoint and auth use database

--- src/backend/expungeservice/endpoints/users.py
+++ src/backend/expungeservice/endpoints/users.py
@@ -2,24 +2,50 @@ from flask.views import MethodView
 from flask import request, abort, jsonify
 from werkzeug.security import generate_password_hash
 
-# TODO temp hack - replace with table from DB
-users = {}
+from flask import g
+from expungeservice.database.user import create_user, get_user_by_email
+from expungeservice.endpoints.auth import auth_required
+from psycopg2.errors import UniqueViolation
+
 
 class Users(MethodView):
+    @auth_required
     def post(self):
         data = request.get_json()
 
         if data == None:
             abort(400)
 
-        # TODO temp hack - replace with table from DB
-        if data['email'] in users:
-            return 'User already exists! Please login', 202
+        """
+        - the @auth_required decorator identifies the logged in user. If no auth, return 401 UNAUTHORIZED.
+        - Verify the logged in user is admin. if not, return 403 FORBIDDEN.
+        - Verify the password meets the 8-character length requirement. If not, return 422
+        - Attempt to insert the user into the database. If failure due to duplicate entry, return 422
+        - If success, return the new user's email, admin flag, and creation timestamp.
+        """
 
-        users[data['email']] = generate_password_hash(data['password'])
+        authorized_user_db_result = get_user_by_email(g.database, g.auth_hdr_payload['sub'])
+        if not authorized_user_db_result['admin']:
+            return 'Logged in user not admin', 403
+
+        if len(data['password']) <8:
+            return 'New password is less than 8 characters long!', 422
+
+        password_hash = generate_password_hash(data['password'])
+
+        try:
+            create_user_result = create_user(g.database,
+                                             email = data['email'],
+                                             password_hash = password_hash,
+                                             admin = data['admin'])
+        except UniqueViolation:
+            return 'User with that email address already exists!', 422
 
         response_data = {
-            'email': data['email']
+            'email': create_user_result['email'],
+            'admin': create_user_result['admin'],
+            'timestamp': create_user_result['date_created'],
+
         }
 
         return jsonify(response_data), 201
--- src/backend/tests/endpoints/test_auth.py
+++ src/backend/tests/endpoints/test_auth.py
@@ -2,89 +2,90 @@ import pytest
 import os
 import time
 import datetime
+import unittest
 
-from flask import jsonify, current_app
+from flask import jsonify, current_app, g
+from expungeservice.database import user
+from werkzeug.security import generate_password_hash
 
 import expungeservice
 
-@pytest.fixture(scope='module')
-def app():
-    return expungeservice.create_app('development')
+class TestAuth(unittest.TestCase):
 
-@pytest.fixture(scope='module')
-def client(app):
-    return app.test_client()
+    email = 'pytest_user@auth_test.com'
+    password = 'pytest_password'
+    hashed_password = generate_password_hash(password)
 
-def test_hello(client):
-    response = client.get('/hello')
-    assert(response.data == b'Hello, world!')
+    def setUp(self):
 
-email = 'test_user@test.com'
-password = 'test_password'
+        self.app = expungeservice.create_app('development')
+        self.client = self.app.test_client()
 
-def create_user(client, email, password):
-    return client.post('api/v0.1/users', json={
-        'email': email,
-        'password': password,
-    })
+        with self.app.app_context():
+            expungeservice.request.before()
 
-def get_auth_token(client, email, password):
-    return client.get('/api/v0.1/auth_token', json={
-        'email': email,
-        'password': password,
-    })
+            self.db_cleanup()
+            user.create_user(g.database, self.email, self.hashed_password, False)
+            expungeservice.request.teardown(None)
 
-def test_create_user(client):
-    response = create_user(client, email, password)
-    assert(response.status_code == 201)
+    def tearDown(self):
+        with self.app.app_context():
+            expungeservice.request.before()
 
-    data = response.get_json()
-    assert(data['email'] == email)
+            self.db_cleanup()
+            expungeservice.request.teardown(None)
 
-def test_create_user_collision(client):
-    response = create_user(client, email, password)
-    assert(response.status_code == 202)
+    def db_cleanup(self):
 
-def test_auth_token_valid_credentials(client):
-    response = get_auth_token(client, 'test_user@test.com', 'test_password')
+        cleanup_query = """DELETE FROM users where email like %(pattern)s;"""
+        g.database.cursor.execute(cleanup_query, {"pattern":"%pytest%"})
+        g.database.connection.commit()
 
-    assert(response.status_code == 200)
-    assert(response.headers.get('Content-type') == 'application/json')
-    data = response.get_json()
-    assert('auth_token' in data)
-    assert(len(data['auth_token']))
 
-def test_auth_token_invalid_username(client):
-    response = get_auth_token(client, 'wrong_user@test.com', 'test_password')
-    assert(response.status_code == 401)
+    def get_auth_token(self, email, password):
+        return self.client.get('/api/v0.1/auth_token', json={
+            'email': email,
+            'password': password,
+        })
 
-def test_login_invalid_pasword(client):
-    response = get_auth_token(client, 'test_user@test.com', 'wrong_password')
-    assert(response.status_code == 401)
+    def test_auth_token_valid_credentials(self):
+        response = self.get_auth_token(self.email, self.password)
 
-def test_access_valid_auth_token(client):
-    response = get_auth_token(client, 'test_user@test.com', 'test_password')
-    response = client.get('/api/v0.1/test/protected', headers={
-        'Authorization': 'Bearer {}'.format(response.get_json()['auth_token'])
-    })
-    assert(response.status_code == 200)
+        assert(response.status_code == 200)
+        assert(response.headers.get('Content-type') == 'application/json')
+        data = response.get_json()
+        assert('auth_token' in data)
+        assert(len(data['auth_token']))
 
-def test_access_invalid_auth_token(client):
-    response = get_auth_token(client, 'test_user@test.com', 'test_password')
-    response = client.get('/api/v0.1/test/protected', headers={
-        'Authorization': 'Bearer {}'.format('Invalid auth token')
-    })
-    assert(response.status_code == 401)
+    def test_auth_token_invalid_username(self):
+        response = self.get_auth_token('wrong_user@test.com', 'test_password')
+        assert(response.status_code == 401)
 
-def test_access_expired_auth_token():
-    app = expungeservice.create_app('development')
-    app.config['JWT_EXPIRY_TIMER'] = datetime.timedelta(seconds=0)
+    def test_login_invalid_pasword(self):
+        response = self.get_auth_token(self.email, 'wrong_password')
+        assert(response.status_code == 401)
 
-    client = app.test_client()
+    def test_access_valid_auth_token(self):
+        response = self.get_auth_token(self.email, self.password)
+        response = self.client.get('/api/v0.1/test/protected', headers={
+            'Authorization': 'Bearer {}'.format(response.get_json()['auth_token'])
+        })
+        assert(response.status_code == 200)
 
-    response = get_auth_token(client, 'test_user@test.com', 'test_password')
-    time.sleep(1)
-    response = client.get('/api/v0.1/test/protected', headers={
-        'Authorization': 'Bearer {}'.format(response.get_json()['auth_token'])
-    })
-    assert(response.status_code == 401)
+    def test_access_invalid_auth_token(self):
+        response = self.get_auth_token(self.email, self.password)
+        response = self.client.get('/api/v0.1/test/protected', headers={
+            'Authorization': 'Bearer {}'.format('Invalid auth token')
+        })
+        assert(response.status_code == 401)
+
+    def test_access_expired_auth_token(self):
+        self.app.config['JWT_EXPIRY_TIMER'] = datetime.timedelta(seconds=0)
+
+
+        response = self.get_auth_token(self.email, self.password)
+        time.sleep(1)
+        response = self.client.get('/api/v0.1/test/protected', headers={
+            'Authorization': 'Bearer {}'.format(response.get_json()['auth_token'])
+        })
+        assert(response.status_code == 401)
