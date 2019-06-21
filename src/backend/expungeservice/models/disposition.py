84725a56b59ec34034133f4f78920439b6e8ee2f
Nick Schimek <cfme00@gmail.com>
2019-06-20 20:42:48 -0700

    Set ruling as string

--- src/backend/expungeservice/models/disposition.py
+++ src/backend/expungeservice/models/disposition.py
@@ -3,11 +3,11 @@ from datetime import datetime
 
 class Disposition:
 
-    def __init__(self, date=None, ruling=None):
+    def __init__(self, date=None, ruling=''):
         self.date = self.__set_date(date)
         self.ruling = ruling
 
     def __set_date(self, date):
         if date:
             return datetime.date(datetime.strptime(date, '%m/%d/%Y'))
-        return None
+        return ''
