d9ec2d71a7c2093dc7abcd567b961c2af84cb105
Jordan Witte <wittejm@gmail.com>
2019-07-03 20:31:13 -0700

    Crawler returns a Record object
    * Crawler returns Record obj; Record obj uses properties, record file renamed
    * updated tests to refer to the returned Record object

--- src/backend/tests/crawler/test_crawler.py
+++ src/backend/tests/crawler/test_crawler.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from tests.factories.crawler_factory import CrawlerFactory
 from tests.fixtures.case_details import CaseDetails
 from tests.fixtures.john_doe import JohnDoe
+from expungeservice.models.record import Record
 
 
 class TestCrawler(unittest.TestCase):
@@ -12,59 +13,60 @@ class TestCrawler(unittest.TestCase):
         self.crawler = CrawlerFactory.setup()
 
     def test_search_function(self):
-        CrawlerFactory.create(self.crawler, JohnDoe.RECORD, {'X0001': CaseDetails.CASE_X1,
+        record = CrawlerFactory.create(self.crawler, JohnDoe.RECORD, {'X0001': CaseDetails.CASE_X1,
                                                              'X0002': CaseDetails.CASE_WITHOUT_FINANCIAL_SECTION,
                                                              'X0003': CaseDetails.CASE_WITHOUT_DISPOS})
 
-        assert len(self.crawler.result.cases) == 3
+        assert record.__class__ == Record
+        assert len(record.cases) == 3
 
-        assert len(self.crawler.result.cases[0].charges) == 3
-        assert len(self.crawler.result.cases[1].charges) == 1
-        assert len(self.crawler.result.cases[2].charges) == 3
+        assert len(record.cases[0].charges) == 3
+        assert len(record.cases[1].charges) == 1
+        assert len(record.cases[2].charges) == 3
 
-        assert self.crawler.result.cases[0].charges[0].disposition.ruling == 'Convicted - Failure to Appear'
-        assert self.crawler.result.cases[0].charges[0].disposition.date == datetime.date(datetime.strptime('06/12/2017', '%m/%d/%Y'))
-        assert self.crawler.result.cases[0].charges[1].disposition.ruling == 'Dismissed'
-        assert self.crawler.result.cases[0].charges[1].disposition.date == datetime.date(datetime.strptime('06/12/2017', '%m/%d/%Y'))
-        assert self.crawler.result.cases[0].charges[2].disposition.ruling == 'Dismissed'
-        assert self.crawler.result.cases[0].charges[2].disposition.date == datetime.date(datetime.strptime('06/12/2017', '%m/%d/%Y'))
+        assert record.cases[0].charges[0].disposition.ruling == 'Convicted - Failure to Appear'
+        assert record.cases[0].charges[0].disposition.date == datetime.date(datetime.strptime('06/12/2017', '%m/%d/%Y'))
+        assert record.cases[0].charges[1].disposition.ruling == 'Dismissed'
+        assert record.cases[0].charges[1].disposition.date == datetime.date(datetime.strptime('06/12/2017', '%m/%d/%Y'))
+        assert record.cases[0].charges[2].disposition.ruling == 'Dismissed'
+        assert record.cases[0].charges[2].disposition.date == datetime.date(datetime.strptime('06/12/2017', '%m/%d/%Y'))
 
-        assert self.crawler.result.cases[1].charges[0].disposition.ruling == 'Dismissed'
-        assert self.crawler.result.cases[1].charges[0].disposition.date == datetime.date(datetime.strptime('04/30/1992', '%m/%d/%Y'))
+        assert record.cases[1].charges[0].disposition.ruling == 'Dismissed'
+        assert record.cases[1].charges[0].disposition.date == datetime.date(datetime.strptime('04/30/1992', '%m/%d/%Y'))
 
-        assert self.crawler.result.cases[2].charges[0].disposition.ruling is None
-        assert self.crawler.result.cases[2].charges[0].disposition.date is None
-        assert self.crawler.result.cases[2].charges[1].disposition.ruling is None
-        assert self.crawler.result.cases[2].charges[1].disposition.date is None
-        assert self.crawler.result.cases[2].charges[2].disposition.ruling is None
-        assert self.crawler.result.cases[2].charges[2].disposition.date is None
+        assert record.cases[2].charges[0].disposition.ruling is None
+        assert record.cases[2].charges[0].disposition.date is None
+        assert record.cases[2].charges[1].disposition.ruling is None
+        assert record.cases[2].charges[1].disposition.date is None
+        assert record.cases[2].charges[2].disposition.ruling is None
+        assert record.cases[2].charges[2].disposition.date is None
 
     def test_a_blank_search_response(self):
-        CrawlerFactory.create(self.crawler, JohnDoe.BLANK_RECORD, {})
+        record = CrawlerFactory.create(self.crawler, JohnDoe.BLANK_RECORD, {})
 
-        assert len(self.crawler.result.cases) == 0
+        assert len(record.cases) == 0
 
     def test_single_charge_conviction(self):
-        CrawlerFactory.create(self.crawler, JohnDoe.SINGLE_CASE_RECORD, {'CASEJD1': CaseDetails.CASEJD1})
+        record = CrawlerFactory.create(self.crawler, JohnDoe.SINGLE_CASE_RECORD, {'CASEJD1': CaseDetails.CASEJD1})
 
-        assert len(self.crawler.result.cases) == 1
-        assert len(self.crawler.result.cases[0].charges) == 1
+        assert len(record.cases) == 1
+        assert len(record.cases[0].charges) == 1
 
-        assert self.crawler.result.cases[0].charges[0].name == 'Loading Zone'
-        assert self.crawler.result.cases[0].charges[0].statute == '29'
-        assert self.crawler.result.cases[0].charges[0].level == 'Violation Unclassified'
-        assert self.crawler.result.cases[0].charges[0].date == datetime.date(datetime.strptime('09/04/2008', '%m/%d/%Y'))
-        assert self.crawler.result.cases[0].charges[0].disposition.ruling == 'Convicted'
-        assert self.crawler.result.cases[0].charges[0].disposition.date == datetime.date(datetime.strptime('11/18/2008', '%m/%d/%Y'))
+        assert record.cases[0].charges[0].name == 'Loading Zone'
+        assert record.cases[0].charges[0].statute == '29'
+        assert record.cases[0].charges[0].level == 'Violation Unclassified'
+        assert record.cases[0].charges[0].date == datetime.date(datetime.strptime('09/04/2008', '%m/%d/%Y'))
+        assert record.cases[0].charges[0].disposition.ruling == 'Convicted'
+        assert record.cases[0].charges[0].disposition.date == datetime.date(datetime.strptime('11/18/2008', '%m/%d/%Y'))
 
     def test_nonzero_balance_due_on_case(self):
-        CrawlerFactory.create(self.crawler, JohnDoe.RECORD, {'X0001': CaseDetails.CASE_X1,
+        record = CrawlerFactory.create(self.crawler, JohnDoe.RECORD, {'X0001': CaseDetails.CASE_X1,
                                                              'X0002': CaseDetails.CASE_WITHOUT_FINANCIAL_SECTION,
                                                              'X0003': CaseDetails.CASE_WITHOUT_DISPOS})
 
-        assert self.crawler.result.cases[0].get_balance_due() == 1516.80
+        assert record.cases[0].get_balance_due() == 1516.80
 
     def test_zero_balance_due_on_case(self):
-        CrawlerFactory.create(self.crawler, JohnDoe.SINGLE_CASE_RECORD, {'CASEJD1': CaseDetails.CASEJD1})
+        record = CrawlerFactory.create(self.crawler, JohnDoe.SINGLE_CASE_RECORD, {'CASEJD1': CaseDetails.CASEJD1})
 
-        assert self.crawler.result.cases[0].get_balance_due() == 0
+        assert record.cases[0].get_balance_due() == 0
