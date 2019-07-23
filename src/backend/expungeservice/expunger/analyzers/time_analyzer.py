17655e5fd2d5558d64a29ca900640129bcd8bf24
Nick Schimek <cfme00@gmail.com>
2019-07-23 13:51:39 -0700

    Refactored time analyzer

--- src/backend/expungeservice/expunger/analyzers/time_analyzer.py
+++ src/backend/expungeservice/expunger/analyzers/time_analyzer.py
@@ -12,38 +12,32 @@ class TimeAnalyzer:
     @staticmethod
     def evaluate(expunger):
         if expunger.most_recent_conviction:
-            TimeAnalyzer._mark_all_charges_ineligible_using_mrc_date(expunger, 'Time-ineligible under 137.225(7)(b)',
-                                                                     TimeAnalyzer.TEN_YEARS)
+            elig_date = TimeAnalyzer._calc_elig_date(expunger.most_recent_conviction, TimeAnalyzer.TEN_YEARS)
+            TimeAnalyzer._mark_as_time_ineligible(expunger.charges, 'Time-ineligible under 137.225(7)(b)', elig_date)
             TimeAnalyzer._check_mrc_time_eligibility(expunger)
         elif expunger.most_recent_dismissal:
             TimeAnalyzer._mark_all_acquittals_ineligible_using_mrd_date(expunger)
-            TimeAnalyzer._mark_all_mrd_case_related_charges_eligible(expunger)
-            TimeAnalyzer._mark_all_convictions_time_eligible(expunger)
+            TimeAnalyzer._mark_as_time_eligible(expunger.most_recent_dismissal.case()().charges)
+            TimeAnalyzer._mark_as_time_eligible(expunger.convictions)
         else:
-            TimeAnalyzer._mark_all_charges_eligible(expunger)
+            TimeAnalyzer._mark_as_time_eligible(expunger.charges)
 
         TimeAnalyzer._evaluate_class_b_felonies(expunger)
 
     @staticmethod
-    def _mark_all_charges_ineligible_using_mrc_date(expunger, reason, years):
-        eligibility_date = expunger.most_recent_conviction.disposition.date + relativedelta(years=years)
-        for charge in expunger.charges:
-            charge.set_time_ineligible(reason, eligibility_date)
-
-    @staticmethod
     def _check_mrc_time_eligibility(expunger):
         if expunger.second_most_recent_conviction:
-            eligibility_date = expunger.second_most_recent_conviction.disposition.date + relativedelta(
-                years=TimeAnalyzer.TEN_YEARS)
-            expunger.most_recent_conviction.set_time_ineligible('Multiple convictions within last ten years',
-                                                                eligibility_date)
+            elig_date = TimeAnalyzer._calc_elig_date(expunger.second_most_recent_conviction, TimeAnalyzer.TEN_YEARS)
+            expunger.most_recent_conviction.set_time_ineligible('Multiple convictions within last ten years', elig_date)
         elif TimeAnalyzer._most_recent_conviction_is_greater_than_three_years_old(expunger):
             expunger.most_recent_conviction.set_time_eligible()
         else:
-            eligibility_date = expunger.most_recent_conviction.disposition.date + relativedelta(
-                years=TimeAnalyzer.THREE_YEARS)
-            expunger.most_recent_conviction.set_time_ineligible('Most recent conviction is less than three years old',
-                                                                eligibility_date)
+            elig_date = TimeAnalyzer._calc_elig_date(expunger.most_recent_conviction, TimeAnalyzer.THREE_YEARS)
+            expunger.most_recent_conviction.set_time_ineligible('Most recent conviction is less than three years old', elig_date)
+
+    @staticmethod
+    def _calc_elig_date(charge, years):
+        return charge.disposition.date + relativedelta(years=years)
 
     @staticmethod
     def _most_recent_conviction_is_greater_than_three_years_old(expunger):
@@ -57,16 +51,6 @@ class TimeAnalyzer:
             charge.set_time_ineligible('Recommend sequential expungement', eligibility_date)
 
     @staticmethod
-    def _mark_all_mrd_case_related_charges_eligible(expunger):
-        for charge in expunger.most_recent_dismissal.case()().charges:
-            charge.set_time_eligible()
-
-    @staticmethod
-    def _mark_all_convictions_time_eligible(expunger):
-        for charge in expunger.convictions:
-            charge.set_time_eligible()
-
-    @staticmethod
     def _evaluate_class_b_felonies(expunger):
         if expunger.most_recent_charge and expunger.most_recent_charge.disposition.date > TimeAnalyzer.TWENTY_YEARS_AGO:
             for charge in expunger.class_b_felonies:
@@ -75,6 +59,11 @@ class TimeAnalyzer:
                                                years=TimeAnalyzer.TWENTY_YEARS))
 
     @staticmethod
-    def _mark_all_charges_eligible(expunger):
-        for charge in expunger.charges:
+    def _mark_as_time_ineligible(charges, reason, eligibility_date):
+        for charge in charges:
+            charge.set_time_ineligible(reason, eligibility_date)
+
+    @staticmethod
+    def _mark_as_time_eligible(charges):
+        for charge in charges:
             charge.set_time_eligible()
