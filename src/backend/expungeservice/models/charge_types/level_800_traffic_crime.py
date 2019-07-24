0670530f1eddc087fd7d3a0efccb4618ba7a5b20
Nick Schimek <cfme00@gmail.com>
2019-07-24 08:31:11 -0700

    Skip time analysis for vehicle violations (#286)
    * Remove motor vehicle violation method
    
    Currently using this method to exclude motor vehicle violations from
    being set to a most recent charge, however motor vehicle violations
    should be excluded from the time analysis altogether.
    
    Removing the motor vehilce violation method, because the skip analysis
    method can be used for this
    
    * Refactor: Create parking ticket class
    
    * Skip analysis for parking tickets
    
    Parking tickets are not eligible
    
    * Level 800 violations skip time analysis
    
    Level 800 charges that are violations do not affect time analysis.
    
    * Update case fixture charge level
    
    The charge level was changed so that it does not skip time analysis.

--- src/backend/expungeservice/models/charge_types/level_800_traffic_crime.py
+++ src/backend/expungeservice/models/charge_types/level_800_traffic_crime.py
@@ -12,8 +12,11 @@ class Level800TrafficCrime(BaseCharge):
             self.expungement_result.set_type_eligibility(False)
             self.expungement_result.set_reason('Ineligible under 137.225(5)')
 
-    def motor_vehicle_violation(self):
-        return True
+    def skip_analysis(self):
+        if self.expungement_result.type_eligibility is True:
+            return False
+        else:
+            return True
 
     def _expungeable(self, **kwargs):
         return 'violation' not in kwargs['level'].lower()
--- src/backend/expungeservice/models/charge_types/parking_ticket.py
+++ src/backend/expungeservice/models/charge_types/parking_ticket.py
@@ -12,5 +12,5 @@ class ParkingTicket(BaseCharge):
             self.expungement_result.set_type_eligibility(False)
             self.expungement_result.set_reason('Ineligible under 137.225(5)')
 
-    def motor_vehicle_violation(self):
+    def skip_analysis(self):
         return True
