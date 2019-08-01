911cc9a16e41462458bdbac4cb737fa41bbd5ff2
Benjamin Jackson <gingerben93@gmail.com>
2019-07-31 23:50:19 -0400

    new tests for record object

--- src/backend/tests/models/test_record.py
+++ src/backend/tests/models/test_record.py
@@ -28,6 +28,56 @@ class TestRecordObject(unittest.TestCase):
         assert recordTest.charges()[0]['name'] == ChargeFactory.build()['name']
 
 
+class TestChargeMethod(unittest.TestCase):
+    def setUp(self):
+        self.case_zero = CaseFactory.create()
+        self.charge_zero = ChargeFactory.create(case=self.case_zero)
+        self.case_zero.charges = [self.charge_zero]
+        
+        self.case_one = CaseFactory.create()
+        self.charge_one = ChargeFactory.create(case=self.case_one)
+        self.charge_two = ChargeFactory.create(case=self.case_one)
+        self.case_one.charges = [self.charge_one, self.charge_two]
+        
+        self.record = RecordFactory.create([self.case_zero, self.case_one])
+
+    def test_num_cases(self):
+        
+        assert len(self.record.charges()) == 3
+        
+    def test_charges_index_0(self):
+
+        assert self.record.charges()[0] == self.charge_zero
+
+    def test_charges_index_1(self):
+        record = RecordFactory.create([self.case_zero, self.case_one])
+
+        assert self.record.charges()[1] == self.charge_one
+
+    def test_charges_index_2(self):
+        record = RecordFactory.create([self.case_zero, self.case_one])
+
+        assert self.record.charges()[2] == self.charge_two
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
