ecc0ea046b6d92f93190f9181ed947335dccc19d
ollinieminen <otn987@gmail.com>
2019-08-06 08:11:58 -0700

    get-records service works.  Still needs error handling

--- src/frontend/src/service/get-records.ts
+++ src/frontend/src/service/get-records.ts
@@ -1,5 +1,10 @@
 import apiService, { Request } from './api-service';
 
-const request = {
-  url: ''
+const request: Request = {
+  url: 'http://localhost:5000/api/search',
+  method: 'post'
 };
+
+export default function loadSearchRecords() {
+  return apiService(request);
+}
