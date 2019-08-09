875e97d4fa1e2f002d0ededf3d8f79883319e72c
cjbreaux <46476298+cjbreaux@users.noreply.github.com>
2019-08-09 13:56:57 -0700

    Main login integration (Issues #75 & #82) (#285)
    Add main login integration

--- src/frontend/src/components/App/index.tsx
+++ src/frontend/src/components/App/index.tsx
@@ -2,23 +2,33 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { get } from 'lodash';
 import { AppState } from '../../redux/store';
+import { Router, Route } from 'react-router-dom';
 
+import history from '../History';
 import Footer from '../Footer';
 import LogIn from '../LogIn';
 import LoggedIn from '../LoggedIn';
 import RecordSearch from '../RecordSearch';
+import OeciLogin from '../Oeci';
+import ForgotPassword from '../ForgotPassword';
+import PasswordReset from '../PasswordReset';
 
 class App extends React.Component {
   public render() {
     return get(this, 'props.system.loggedIn') ? (
-      <LoggedIn>
-        <RecordSearch />
-      </LoggedIn>
+      <Router history={history}>
+        <LoggedIn>
+          <Route path="/oeci" component={OeciLogin} />
+          <Route path="/record-search" component={RecordSearch} />
+        </LoggedIn>
+      </Router>
     ) : (
-      <div>
-        <LogIn />
-        <Footer />
-      </div>
+      <Router history={history}>
+        <Route exact path="/" component={LogIn} />
+        <Route exact path="/" component={Footer} />
+        <Route path="/forgot-password" component={ForgotPassword} />
+        <Route path="/password-reset" component={PasswordReset} />
+      </Router>
     );
   }
 }
