8a29f4aca62528ad9bf20cd96e3ace3819971ff2
Nick Schimek <cfme00@gmail.com>
2019-08-18 21:19:43 -0700

    Update version

--- cli.py
+++ cli.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 
 ##############################################
-VERSION = 'v0.3.1'
+VERSION = 'v0.3.2'
 PRODUCTION = True
 ##############################################
 
