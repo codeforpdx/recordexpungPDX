f6d1d621bbe97c9cebc00c06838ade01e65b263a
ollinieminen <otn987@gmail.com>
2019-09-20 08:23:51 -0700

    update method for get-records to get from post

--- src/frontend/src/service/get-records.ts
+++ src/frontend/src/service/get-records.ts
@@ -2,7 +2,7 @@ import apiService, { Request } from './api-service';
 
 const request: Request = {
   url: 'http://localhost:5000/api/search',
-  method: 'post'
+  method: 'get'
 };
 export default function getSearchRecords() {
   return apiService(request);
