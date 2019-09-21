f2c5c45cf65dcbe9f8e4806e611df550179963bc
Jordan Witte <wittejm@gmail.com>
2019-09-20 17:19:41 -0700

    working on a mocker

--- src/backend/expungeservice/endpoints/oeci_login.py
+++ src/backend/expungeservice/endpoints/oeci_login.py
@@ -1,18 +1,23 @@
 import flask
 from flask.views import MethodView
-from flask import request, make_response, abort, jsonify
+from flask import request, make_response, abort, jsonify, g
 from werkzeug.security import generate_password_hash
+import time
 
-from flask import g
+print("====\n====\n====\n====\n====\n====\nOECILOGIN ENDPOINT IMPORT STARTING====\n====\n====\n====\n====\n====\n")
+
+from expungeservice.crawler.crawler import Crawler
 from expungeservice.database import user
 from expungeservice.endpoints.auth import user_auth_required
 from expungeservice.request import check_data_fields
 from expungeservice.request.error import error
 
+print("====\n====\n====\n====\n====\n====\nOECILOGIN ENDPOINT IMPORT HAPPENED====\n====\n====\n====\n====\n====\n")
+input()
 
 class OeciLogin(MethodView):
 
-    #@user_auth_required
+    @user_auth_required
     def post(self):
         """
 
@@ -23,26 +28,37 @@ class OeciLogin(MethodView):
         if data is None:
             error(400, "No json data in request body")
 
-        print("data received by OeciLogin.post():", data)
         check_data_fields(data, ['oeci_username', 'oeci_password'])
 
-        """
-        TODO: attempt login to OECI and verify credentials are correct
-        """
+        print("in oeci login endpoint, json data is:\n")
+        print(data)
+        c = Crawler()
+        print("\ncrawler object is :\n", c)
+
+        login_result = c.login(data["oeci_username"], data["oeci_password"])
+
+        print("crawler.login in endpoint result:", login_result)
+        input("...")
+
+        if not login_result:
+            error(401, "Invalid OECI login credentials")
 
         """
         TODO: encrypt and sign a new JWT containing the oeci credentials
         to store in the returned cookie
         """
 
-        """
-        attach a cookie  to the response object, called "oeci_token"
-        """
         response = flask.make_response()
+
         response.set_cookie(
             "oeci_token",
-            value='I\'m a fake oeci token! I don\'t expire! I\'m not secure!')
+            secure = True,
+            httponly = True,
+            samesite = 'strict',
+            expires =  time.time() + 15 * 60,
+            value="I'm a fake oeci token!")
 
+        app = flask.Flask(__name__)
 
         return response, 201
 
--- src/backend/tests/endpoints/test_oeci_login.py
+++ src/backend/tests/endpoints/test_oeci_login.py
@@ -1,17 +1,28 @@
+
+
+import unittest.mock
+from unittest.mock import patch
 import pytest
 import os
 import time
 import datetime
 import unittest
-
-from flask import jsonify, current_app, g, request
-from expungeservice.database import user
 from werkzeug.security import generate_password_hash
+from flask import jsonify, current_app, g, request
 
-import expungeservice
+print('\n============\nTEST CASE FILE STARTING\n==============')
+
+class MockCrawler:
+    def login(self, u, p):
+        print("mock crawler login")
+        if u == "correct" and  p =="correct":
+            return True
+        else:
+            return False
+
+class TestOeciLogin(unittest.TestCase):
 
 
-class TestUsers(unittest.TestCase):
 
     email = 'pytest_user@auth_test.com'
     name = 'Endpoint Test'
@@ -20,7 +31,27 @@ class TestUsers(unittest.TestCase):
 
     hashed_password = generate_password_hash(password)
 
-    def setUp(self):
+    @unittest.mock.patch("expungeservice.crawler.crawler.Crawler")
+    def setUp(self, mock_crawler):
+
+        print("startings Mocked Setup")
+
+        from expungeservice.crawler.request import URL
+        from tests.fixtures.post_login_page import PostLoginPage
+
+
+        from expungeservice.database import user
+        import expungeservice
+
+        from expungeservice.endpoints import oeci_login
+
+
+        mock_crawler().return_value.login.return_value=False
+        mock_crawler.return_value.login.return_value=False
+
+        #crawler = expungeservice.crawler.crawler.Crawler()
+        #print("in setup crawler:", crawler)
+
 
         self.app = expungeservice.create_app('development')
         self.client = self.app.test_client()
@@ -34,6 +65,9 @@ class TestUsers(unittest.TestCase):
             expungeservice.request.teardown(None)
 
     def tearDown(self):
+        from expungeservice.database import user
+        import expungeservice
+
         with self.app.app_context():
             expungeservice.request.before()
 
@@ -52,7 +86,13 @@ class TestUsers(unittest.TestCase):
             'password': password,
         })
 
-    def test_oeci_login_success(self):
+    @unittest.mock.patch("expungeservice.crawler.crawler.Crawler")
+    def test_oeci_login_success(self, mock_crawler):
+        print("==================\ntest_oeci_login_success\n====================")
+
+        mock_crawler.return_value.login.return_value = True
+        mock_crawler().return_value.login.return_value = True
+
 
         generate_auth_response = self.generate_auth_token(
             self.email, self.password)
@@ -60,10 +100,38 @@ class TestUsers(unittest.TestCase):
         response = self.client.post('/api/oeci_login', headers={
             'Authorization': 'Bearer {}'.format(
                 generate_auth_response.get_json()['auth_token'])},
-                json={'oeci_username': "oeci_username",
-                      'oeci_password': "oeci_password",
+                json={'oeci_username': "correct",
+                      'oeci_password': "correct",
                       })
 
         assert(response.status_code == 201)
+
         assert self.client.cookie_jar._cookies[
             'localhost.local']['/']['oeci_token'].value
+
+    @unittest.mock.patch("expungeservice.crawler.crawler.Crawler")
+    def test_oeci_login_invalid_credentials(self, mock_crawler):
+        print("==================\ntest_oeci_login_invalid_credentials\n====================")
+        mock_crawler().return_value = MockCrawler()
+
+        mock_crawler.return_value.login.return_value = False
+        print("mock_crawler.return_value.login" , mock_crawler.return_value.login)
+        #mock_crawler.login = lambda u,p : (u == "username" and p == "password")
+        #mock_crawler.login.return_value = True
+        #mock_crawler.return_value.login.return_value = False
+
+        #print("mock_crawler in failure:", mock_crawler)
+        #input("...")
+
+        generate_auth_response = self.generate_auth_token(
+            self.email, self.password)
+
+
+        response = self.client.post('/api/oeci_login', headers={
+            'Authorization': 'Bearer {}'.format(
+                generate_auth_response.get_json()['auth_token'])},
+                json={'oeci_username': "BadUsername",
+                      'oeci_password': "BadPassword",
+                      })
+
+        assert(response.status_code == 401)
