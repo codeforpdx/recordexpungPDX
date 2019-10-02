f25f0958671d11cb84a91fa42269d334f1fd91fc
Nick Schimek <cfme00@gmail.com>
2019-10-01 19:38:13 -0700

    Update parking violation rule

--- src/backend/expungeservice/models/charge_types/parking_ticket.py
+++ src/backend/expungeservice/models/charge_types/parking_ticket.py
@@ -5,12 +5,8 @@ class ParkingTicket(BaseCharge):
 
     def __init__(self, **kwargs):
         super(ParkingTicket, self).__init__(**kwargs)
-        if self.acquitted():
-            self.expungement_result.set_type_eligibility(True)
-            self.expungement_result.set_reason('Eligible under 137.225(1)(b)')
-        else:
-            self.expungement_result.set_type_eligibility(False)
-            self.expungement_result.set_reason('Ineligible under 137.225(5)')
+        self.expungement_result.set_type_eligibility(False)
+        self.expungement_result.set_reason('Ineligible under 137.225(5)')
 
     def skip_analysis(self):
         return True
