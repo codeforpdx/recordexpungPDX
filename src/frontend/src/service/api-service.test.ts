addd03b78ac83ce649b8c5b48d3ac5144287b779
Olli Nieminen <44886153+SteelFace87@users.noreply.github.com>
2019-10-04 10:04:41 -0700

    Update/api response (#439)
    * update API response

--- src/frontend/src/service/api-service.test.ts
+++ src/frontend/src/service/api-service.test.ts
@@ -1,6 +1,3 @@
-// NOTE, the API response for records has changed since this file was made.  This test needs to be updated.
-// For reference to what the API response lookslike now, visit https://github.com/codeforpdx/recordexpungPDX/commit/8bed5d309f9f43a433ecdda6e768f01b1a2a7481?diff=unified
-
 import apiService, { Request } from './api-service';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
@@ -55,40 +52,95 @@ describe('API SERVICE TEST', () => {
     };
     apiService(mockStore.dispatch, request).then((response: any) => {
       expect(response).toEqual({
-        data: [
-          {
-            name: 'WOODS, LAVELLE D',
-            birth_year: 1970,
-            case_number: 'ZA0081909',
-            citation_number: 'ZA0081909',
-            location: 'Multnomah',
-            date: '07/02/2013',
-            violation_type: 'Offense Violation',
-            current_status: 'Inactive',
-            balance_due: '0',
-            charges: [
+        data: {
+          record: {
+            total_balance_due: 199.99,
+            cases: [
               {
-                name: 'Failure to Properly Use Safety Belts - MV Operator',
-                statute: '8112101A',
-                level: 'Violation Class D',
-                date: '06/11/2013',
-                disposition: {
-                  date: '07/16/2013',
-                  ruling: 'Convicted'
-                },
-                expungement_result: {
-                  type_eligibility: 'True',
-                  type_eligibility_reason: 'some string',
-                  time_eligibility: 'True',
-                  time_eligibility_reason: 'some string',
-                  date_of_eligibility: 'date'
-                }
+                name: 'Doe, John',
+                birth_year: 1970,
+                case_number: 'HA05555555',
+                citation_number: 'HA08643443',
+                location: 'Multnomah',
+                date: '09/05/2008',
+                violation_type: 'Municipal Parking',
+                current_status: 'Closed',
+                charges: [
+                  {
+                    name: 'Loading Zone',
+                    statute: '29',
+                    level: 'Violation Unclassified',
+                    date: '09/04/2008',
+                    disposition: {
+                      date: '11/18/2008',
+                      ruling: 'Convicted'
+                    },
+                    expungement_result: {
+                      type_eligibility: false,
+                      type_eligibility_reason: 'Ineligible under 137.225(5)',
+                      time_eligibility: null,
+                      time_eligibility_reason: '',
+                      date_of_eligibility: null
+                    }
+                  }
+                ],
+                balance_due: 199.99,
+                case_detail_link:
+                  'https://publicaccess.courts.oregon.gov/PublicAccessLogin/CaseDetail.aspx?CaseID=11918348'
+              },
+              {
+                name: 'Doe, John',
+                birth_year: 1970,
+                case_number: '18VI55555',
+                citation_number: 'RU420001442',
+                location: 'Multnomah',
+                date: '04/02/2018',
+                violation_type: 'Offense Violation',
+                current_status: 'Closed',
+                charges: [
+                  {
+                    name: 'Violating a Speed Limit',
+                    statute: '811111',
+                    level: 'Violation Class A',
+                    date: '03/06/2018',
+                    disposition: {
+                      date: '04/02/2018',
+                      ruling: 'Dismissed'
+                    },
+                    expungement_result: {
+                      type_eligibility: true,
+                      type_eligibility_reason: 'Eligible under 137.225(1)(b)',
+                      time_eligibility: null,
+                      time_eligibility_reason: '',
+                      date_of_eligibility: null
+                    }
+                  },
+                  {
+                    name: 'Violation Driving While Suspended or Revoked',
+                    statute: '811175',
+                    level: 'Violation Class A',
+                    date: '03/06/2018',
+                    disposition: {
+                      date: '04/02/2018',
+                      ruling: 'Dismissed'
+                    },
+                    expungement_result: {
+                      type_eligibility: true,
+                      type_eligibility_reason: 'Eligible under 137.225(1)(b)',
+                      time_eligibility: null,
+                      time_eligibility_reason: '',
+                      date_of_eligibility: null
+                    }
+                  }
+                ],
+                balance_due: 0.0,
+                case_detail_link:
+                  'https://publicaccess.courts.oregon.gov/PublicAccessLogin/CaseDetail.aspx?CaseID=55555555'
               }
-            ],
-            case_detail_link:
-              'https://publicaccess.courts.oregon.gov/PublicAccessLogin/CaseDetail.aspx?CaseID=9036658'
+            ]
           }
-        ]
+        },
+        errors: []
       });
       done();
     });
