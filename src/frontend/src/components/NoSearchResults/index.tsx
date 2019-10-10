5b620674b4ef06480419c023c8f365e6f0ad404a
Olli Nieminen <44886153+SteelFace87@users.noreply.github.com>
2019-10-09 20:33:40 -0700

    Feature/status (#449)
    Get search status working

---
+++ src/frontend/src/components/NoSearchResults/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function NoSearchResults() {
+  return (
+    <p className="bg-light-gray mv4 pa4 br3 fw6">No search results found.</p>
+  );
+}
---
+++ src/frontend/src/components/Spinner/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function() {
+  return (
+    <p className="bg-white mv4 pa4 br3 fw6">
+      <span className="spinner mr2"></span>Loading your search results...
+    </p>
+  );
+}
