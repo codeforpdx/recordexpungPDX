79b4f347826bc6667af3d161a59bef301667e6cd
Kent Shikama <kent@kentshikama.com>
2019-11-12 15:06:13 -0800

    Replace custom Request type with default AxiosRequestConfig
    AxiosRequestConfig, which is already defined in the axios library, is a superset of our custom Request type.
    
    ```
    export interface AxiosRequestConfig {
      url?: string;
      method?: Method;
      baseURL?: string;
      transformRequest?: AxiosTransformer | AxiosTransformer[];
      transformResponse?: AxiosTransformer | AxiosTransformer[];
      headers?: any;
      params?: any;
      paramsSerializer?: (params: any) => string;
      data?: any;
      timeout?: number;
      withCredentials?: boolean;
      adapter?: AxiosAdapter;
      auth?: AxiosBasicCredentials;
      responseType?: ResponseType;
      xsrfCookieName?: string;
      xsrfHeaderName?: string;
      onUploadProgress?: (progressEvent: any) => void;
      onDownloadProgress?: (progressEvent: any) => void;
      maxContentLength?: number;
      validateStatus?: (status: number) => boolean;
      maxRedirects?: number;
      socketPath?: string | null;
      httpAgent?: any;
      httpsAgent?: any;
      proxy?: AxiosProxyConfig | false;
      cancelToken?: CancelToken;
    }
    ```

--- src/frontend/src/service/api-service.ts
+++ src/frontend/src/service/api-service.ts
@@ -1,23 +1,10 @@
-import axios, { AxiosPromise } from 'axios';
+import axios, { AxiosPromise, AxiosRequestConfig } from 'axios';
 import { removeCookie } from './cookie-service';
 import { LOG_OUT } from '../redux/system/types';
 
-type Method = 'post' | 'delete' | 'get' | 'head' | 'delete' | 'options' | 'put';
-
-// If 'authenticated' is true then the RequestMiddleware will add
-// our authentication token (a JWT) as the authorization header for
-// the request.
-export type Request = {
-  url: string;
-  data?: object;
-  method: Method;
-  headers?: object;
-  withCredentials?: boolean;
-};
-
 export default function apiService(
   dispatch: Function,
-  request: Request
+  request: AxiosRequestConfig
 ): AxiosPromise {
   return axios.request(request).catch(error => {
     if (error.response && error.response.status === 401) {
