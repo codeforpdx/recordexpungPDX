fb7f0427705d66c026a1d9a51a459bca4ee7a86d
ollinieminen <otn987@gmail.com>
2019-11-17 18:32:54 -0800

    chore(Charges):  remove console.log

--- src/frontend/src/components/Charges/index.tsx
+++ src/frontend/src/components/Charges/index.tsx
@@ -8,9 +8,7 @@ interface Props {
 
 export default class Charges extends React.Component<Props> {
   render() {
-    console.log('mapping', this.props.charges);
     const charges = this.props.charges.map((charge: ChargeType, i) => {
-      console.log('charge', charge);
       return (
         <li key={i}>
           <Charge charge={charge} />
