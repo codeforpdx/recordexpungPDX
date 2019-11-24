68fd651f38b2aa9766c81783d36272a621904e08
Jordan Witte <wittejm@gmail.com>
2019-11-23 16:15:31 -0800

    simplified save_result test case

--- src/backend/tests/stats/test_stats_save_result.py
+++ src/backend/tests/stats/test_stats_save_result.py
@@ -2,28 +2,8 @@ from flask import g, request
 
 
 import expungeservice
-from expungeservice.database.db_util import rollback_errors
-from expungeservice import stats
 from tests.factories.crawler_factory import CrawlerFactory
-from expungeservice.database import get_database
 from tests.endpoints.endpoint_util import EndpointShared
-from flask.views import MethodView
-
-
-class TestStatsView(MethodView):
-    """
-    To get the direct call to stats.save_result() to work,
-    this endpoint wrapper is needed so that the flask.current_user
-    object gets populated with user_id
-    """
-
-    def post(self):
-
-        request_data = request.get_json()
-        record = CrawlerFactory.create(CrawlerFactory.setup())
-        stats.save_result(request_data, record)
-
-        return "TestStatsViewResponse"
 
 
 class TestStats(EndpointShared):
@@ -31,23 +11,25 @@ class TestStats(EndpointShared):
 
     def test_save_result(self):
 
-        self.app.add_url_rule(
-            "/api/test/save_result", view_func=TestStatsView.as_view(
-                "save_result"))
+        with self.client:
+            user_id = self.user_data["user1"]["user_id"]
 
-        user_id = self.user_data["user1"]["user_id"]
+            request_data = {
+                "first_name":"John",
+                "last_name":"Doe",
+                "middle_name":"Test",
+                "birth_date":"01/01/1980",
+            }
 
-        request_data = {
-            "first_name":"John",
-            "last_name":"Doe",
-            "middle_name":"Test",
-            "birth_date":"01/01/1980",
-        }
+            record = CrawlerFactory.create(CrawlerFactory.setup())
 
-        self.login(self.user_data["user1"]["email"], self.user_data["user1"]["password"])
+            self.login(self.user_data["user1"]["email"], self.user_data["user1"]["password"])
 
-        response = self.client.post('/api/test/save_result', json = request_data)
+            expungeservice.request.before()
+
+            expungeservice.stats.save_result(request_data, record)
+            g.database.connection.commit()
 
         self.check_search_result_saved(
-                    self.user_data["user1"]["user_id"], request_data,
-                    num_eligible_charges=6, num_charges=9)
+                self.user_data["user1"]["user_id"], request_data,
+                num_eligible_charges=6, num_charges=9)
