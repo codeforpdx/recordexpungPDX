1927e6c2677b1af02c66e7fed24f5802846da574
ForrestLonganecker <42503418+ForrestLonganecker@users.noreply.github.com>
2019-11-30 08:53:29 -0800

    Redirect to OECI login from search page (#587)
    * added check for OECI token on componentDidMount() in AllRecords component, fixed typos
    
    * added check for this.props.records.cases.length to show "no search results found" on subsequent searches
    
    * removed outdated comment

--- src/frontend/src/containers/AllRecords.tsx
+++ src/frontend/src/containers/AllRecords.tsx
@@ -9,6 +9,7 @@ import {
 import RecordSearch from '../components/RecordSearch';
 import SearchResults from '../components/SearchResults';
 import AllStatus from './AllStatus';
+import { checkOeciRedirect } from '../service/cookie-service';
 
 type Props = {
   fetchRecords: Function;
@@ -17,18 +18,22 @@ type Props = {
 };
 
 class AllRecords extends Component<Props> {
+  componentWillMount() {
+    checkOeciRedirect();
+  }
+
   componentWillUnmount() {
     this.props.clearRecords();
   }
 
   render() {
-    // Fetch has now been passed down to RecordSearch componant and is triggered when submit button is pressed.
-    // Currentlly no matter what the user puts in the search fields, it still triggers a mock fetch with pre determined API response
     // Once fetch adds records to Redux, a SearchResults container is rendered with updated records
     return (
       <main className="mw8 center ph2">
         <RecordSearch fetchRecords={this.props.fetchRecords} />
-        {this.props.records ? (
+        {this.props.records &&
+        this.props.records.cases &&
+        this.props.records.cases.length > 0 ? (
           <SearchResults records={this.props.records} />
         ) : (
           <AllStatus />
