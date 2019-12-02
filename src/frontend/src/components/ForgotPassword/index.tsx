a9d8d197dce8b4bfc0b5d84d67b62ced95077069
Hunter Marcks <hmarcks@gmail.com>
2019-12-01 22:58:35 -0600

    resizes reset password view logo

--- src/frontend/src/components/ForgotPassword/index.tsx
+++ src/frontend/src/components/ForgotPassword/index.tsx
@@ -49,7 +49,9 @@ class ForgotPassword extends React.Component<Props, State> {
     return (
       <main className="mw8 center ph2">
         <section className="mw6 center cf mt4 mb3 pa4 pa5-ns pt4-ns bg-white shadow br3">
-          <Logo />
+          <div className="logo--login">
+            <Logo />
+          </div>
           <form
             onSubmit={this.handleSubmit}
             noValidate
