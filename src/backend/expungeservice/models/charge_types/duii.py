5fc02a8ea83537d24046104229065c636fba865a
Jordan Witte <wittejm@gmail.com>
2019-12-07 11:11:36 -0800

    Add New Duii charge class (#607)
    * Add New Duii charge class
    
    duii is classified in _traffic_crime along with Level800TrafficCrime
    
    fixes https://github.com/codeforpdx/recordexpungPDX/issues/592

---
+++ src/backend/expungeservice/models/charge_types/duii.py
@@ -0,0 +1,25 @@
+from expungeservice.models.charge_types.charge import Charge
+from expungeservice.models.expungement_result import TypeEligibility, EligibilityStatus
+
+
+class Duii(Charge):
+
+    def _default_type_eligibility(self):
+
+        if not self.disposition:
+            return TypeEligibility(EligibilityStatus.NEEDS_MORE_ANALYSIS, reason='Further Analysis Needed - No disposition')
+
+        elif 'diverted' in self.disposition.ruling.lower():
+            return TypeEligibility(EligibilityStatus.INELIGIBLE, reason='Ineligible under 137.225(8)(b)')
+
+        elif self.acquitted():
+            return TypeEligibility(EligibilityStatus.NEEDS_MORE_ANALYSIS, reason='Further Analysis Needed')
+
+        else:
+            """
+            TODO: other dispositions than "convicted" are possible and are not handled here.
+            See issue: https://github.com/codeforpdx/recordexpungPDX/issues/601
+
+            TODO: reason can be expanded to "137.225(7)(a) - Traffic offenses are ineligible"
+            """
+            return TypeEligibility(EligibilityStatus.INELIGIBLE, reason='Ineligible under 137.225(7)(a)')
