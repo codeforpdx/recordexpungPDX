22a9aaf8931043904dffadb11f3fa4b596fe919a
Kent Shikama <kent@kentshikama.com>
2019-12-06 22:34:58 -0800

    Add a search strategy for Record generation and serializer tests
    Thanks to all the models being data classes we can have hypothesis infer types and automatically generate mock data for us. For the serializer test we are just checking that the server will not throw a 500 and thus there are no asserts.

---
+++ src/backend/expungeservice/models/generator/__init__.py
@@ -0,0 +1,36 @@
+from _weakref import ref
+
+from hypothesis._strategies import none, composite
+
+from expungeservice.models.case import Case
+from expungeservice.models.record import Record
+from hypothesis.strategies import builds, just, lists, one_of
+
+from expungeservice.models.charge_types.juvenile_charge import JuvenileCharge
+from expungeservice.models.charge_types.felony_class_a import FelonyClassA
+from expungeservice.models.charge_types.felony_class_b import FelonyClassB
+from expungeservice.models.charge_types.felony_class_c import FelonyClassC
+from expungeservice.models.charge_types.level_800_traffic_crime import Level800TrafficCrime
+from expungeservice.models.charge_types.list_b import ListB
+from expungeservice.models.charge_types.marijuana_ineligible import MarijuanaIneligible
+from expungeservice.models.charge_types.misdemeanor import Misdemeanor
+from expungeservice.models.charge_types.non_traffic_violation import NonTrafficViolation
+from expungeservice.models.charge_types.parking_ticket import ParkingTicket
+from expungeservice.models.charge_types.person_crime import PersonCrime
+from expungeservice.models.charge_types.schedule_1_p_c_s import Schedule1PCS
+from expungeservice.models.charge_types.unclassified_charge import UnclassifiedCharge
+
+charge_types = [JuvenileCharge, FelonyClassA, FelonyClassB, FelonyClassC, Level800TrafficCrime, ListB, MarijuanaIneligible, Misdemeanor, NonTrafficViolation, ParkingTicket, PersonCrime, Schedule1PCS, UnclassifiedCharge]
+
+@composite
+def _build_case_strategy(draw, min_charges_size=0):
+    case = draw(builds(Case, charges=none()))
+    charge_strategy_choices = list(map(lambda charge_type: builds(charge_type, _case=just(ref(case))), charge_types))
+    charge_strategy = one_of(charge_strategy_choices)
+    charges = draw(lists(charge_strategy, min_charges_size))
+    case.charges = charges
+    return case
+
+def build_record_strategy(min_cases_size=0, min_charges_size=0):
+    case_strategy = _build_case_strategy(min_charges_size)
+    return builds(Record, cases=lists(case_strategy, min_cases_size))
---
+++ src/backend/tests/serializer/test_serializer_generative.py
@@ -0,0 +1,13 @@
+import json
+
+from hypothesis import given
+
+from expungeservice.expunger.expunger import Expunger
+from expungeservice.models.generator import build_record_strategy
+from expungeservice.serializer import ExpungeModelEncoder
+
+@given(build_record_strategy())
+def test_round_trip_various_records(record):
+    expunger = Expunger(record)
+    expunger.run()
+    json.loads(json.dumps(record, cls=ExpungeModelEncoder))
