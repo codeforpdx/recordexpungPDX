a25bc6f4caf0b8f0b24d0e7deedce19c76179e08
Nick Schimek <cfme00@gmail.com>
2019-12-13 21:35:50 -0800

    Skip <th> tags and whitespace in <td> tags
    Fixes #647

--- src/backend/expungeservice/crawler/parsers/case_parser/charge_table_data.py
+++ src/backend/expungeservice/crawler/parsers/case_parser/charge_table_data.py
@@ -1,7 +1,14 @@
 class ChargeTableData:
 
+    def __init__(self):
+        self.data_tag = False
+
     def store_data(self, case_parser, data):
-        case_parser.charge_table_data.append(data) 
+        if self.data_tag and data != '\xa0':
+            case_parser.charge_table_data.append(data)
 
     def check_tag(self, tag):
-        pass
+        if tag == 'td':
+            self.data_tag = True
+        else:
+            self.data_tag = False
