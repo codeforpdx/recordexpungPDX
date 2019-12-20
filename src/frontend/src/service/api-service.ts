bbb4a236ead308dd8b7efb5cca3a285c71b60ab1
Jordan Witte <wittejm@gmail.com>
2019-12-19 21:53:52 -0800

    check 401 error message match before app logout (#664)
    A 401 error gets thrown for both an app auth failure and an OECI auth failure.
    An api-service logs out the frontend app when it receives a 401 response, but the app should only log out if the error is coming from from an app auth failure.

--- src/frontend/src/service/api-service.ts
+++ src/frontend/src/service/api-service.ts
@@ -7,7 +7,8 @@ export default function apiService<T>(
   request: AxiosRequestConfig
 ): AxiosPromise {
   return axios.request<T>(request).catch(error => {
-    if (error.response && error.response.status === 401) {
+    if (error.response && error.response.status === 401 && error.response.message === "Invalid username or password") {
+      // To avoid catching the 401 error due to an OECI login failure
       removeCookie();
       dispatch({ type: LOG_OUT });
     }
