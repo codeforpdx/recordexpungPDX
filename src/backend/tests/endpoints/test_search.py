de82d7d202e28402527ef0018bf89cbd0cbea8da
Kent Shikama <kent@kentshikama.com>
2019-12-29 18:34:14 -0800

    Turn EndpointShared into a fixture
    This reduces the `self.service.<whatever>` boilerplate back down to `service.<whatever>`.

--- src/backend/tests/endpoints/test_search.py
+++ src/backend/tests/endpoints/test_search.py
@@ -8,157 +8,157 @@ from tests.factories.crawler_factory import CrawlerFactory
 from expungeservice.serializer import ExpungeModelEncoder
 
 
-class TestSearch:
+@pytest.fixture
+def service():
+    return EndpointShared()
 
-    @pytest.fixture(autouse=True)
-    def setup_and_teardown(self):
-        self.service = EndpointShared()
-        self.service.setup()
+@pytest.fixture(autouse=True)
+def setup_and_teardown(service):
+    service.setup()
 
-        self.crawler = CrawlerFactory.setup()
-        self.crawler.logged_in = True
-        self.mock_record = {
-            "john_doe": CrawlerFactory.create(self.crawler)
-        }
-        self.search_request_data = {
-            "first_name": "John",
-            "last_name": "Doe",
-            "middle_name": "",
-            "birth_date": "02/02/1990"}
-        yield
-        self.service.teardown()
+    service.crawler = CrawlerFactory.setup()
+    service.crawler.logged_in = True
+    service.mock_record = {
+        "john_doe": CrawlerFactory.create(service.crawler)
+    }
+    service.search_request_data = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "middle_name": "",
+        "birth_date": "02/02/1990"}
+    yield
+    service.teardown()
 
+def mock_login(return_value):
+    return lambda s, username, password, close_session=False: return_value
 
-    def mock_login(self, return_value):
-        return lambda s, username, password, close_session=False: return_value
+def mock_search(service, mocked_record_name):
+    return lambda s, first_name, last_name, middle_name, birth_date: service.mock_record[mocked_record_name]
 
-    def mock_search(self, mocked_record_name):
-        return lambda s, first_name, last_name, middle_name, birth_date: self.mock_record[mocked_record_name]
+def mock_save_result_fail(user_id, request_data, record):
+    raise Exception("Exception to test failing save_result")
 
-    def mock_save_result_fail(self, user_id, request_data, record):
-        raise Exception("Exception to test failing save_result")
+def test_search(service, monkeypatch):
+    service.login(service.user_data["user1"]["email"], service.user_data["user1"]["password"])
 
-    def test_search(self, monkeypatch):
-        self.service.login(self.service.user_data["user1"]["email"], self.service.user_data["user1"]["password"])
+    monkeypatch.setattr(search.Crawler, "login", mock_login(True))
+    monkeypatch.setattr(search.Crawler, "search", mock_search(service, "john_doe"))
 
-        monkeypatch.setattr(search.Crawler, "login", self.mock_login(True))
-        monkeypatch.setattr(search.Crawler, "search", self.mock_search("john_doe"))
+    """
+    A separate test, below, verifies that the save-result step
+    also works. Here, we mock the function to reduce the scope of the test.
+    """
+    monkeypatch.setattr(search, "save_result", lambda user_id, request_data, record : None)
 
-        """
-        A separate test, below, verifies that the save-result step
-        also works. Here, we mock the function to reduce the scope of the test.
-        """
-        monkeypatch.setattr(search, "save_result", lambda user_id, request_data, record : None)
+    """
+    as a more unit-y unit test, we could make the encrypted cookie
+    ""manually" in the test code ...
+    But attaching a cookie client-side isn't really a thing. So, use the oeci endpoint.
+    """
 
-        """
-        as a more unit-y unit test, we could make the encrypted cookie
-        ""manually" in the test code ...
-        But attaching a cookie client-side isn't really a thing. So, use the oeci endpoint.
-        """
+    service.client.post(
+        "/api/oeci_login",
+        json={"oeci_username": "correctusername",
+              "oeci_password": "correctpassword"})
 
-        self.service.client.post(
-            "/api/oeci_login",
-            json={"oeci_username": "correctusername",
-                  "oeci_password": "correctpassword"})
-
-        assert self.service.client.cookie_jar._cookies[
-            "localhost.local"]["/"]["oeci_token"]
-
-
-        response = self.service.client.post("/api/search",
-            json=self.search_request_data)
-
-        assert(response.status_code == 200)
-        data = response.get_json()["data"]
-
-        """
-        Check that the resulting "record" field in the response matches what we gave to the
-        mock search function.
-        (use this json encode-decode approach because it turns a Record into a dict.)
-        """
-        assert data["record"] == json.loads(json.dumps(
-            self.mock_record["john_doe"], cls = ExpungeModelEncoder))
+    assert service.client.cookie_jar._cookies[
+        "localhost.local"]["/"]["oeci_token"]
 
 
-    def test_search_fails_without_oeci_token(self):
-        self.service.login(self.service.user_data["user1"]["email"], self.service.user_data["user1"]["password"])
+    response = service.client.post("/api/search",
+        json=service.search_request_data)
 
-        response = self.service.client.post("/api/search",
-            json=self.search_request_data)
+    assert(response.status_code == 200)
+    data = response.get_json()["data"]
 
-        assert(response.status_code == 401)
-
-
-    def test_search_creates_save_results(self, monkeypatch):
-        """
-        This is the same test as above except it includes the save-results step. Less unit-y,
-        more of a vertical test.
-        """
-
-        self.service.login(self.service.user_data["user1"]["email"], self.service.user_data["user1"]["password"])
-
-        monkeypatch.setattr(search.Crawler, "login", self.mock_login(True))
-        monkeypatch.setattr(search.Crawler, "search", self.mock_search("john_doe"))
-
-        self.service.client.post(
-            "/api/oeci_login",
-            json={"oeci_username": "correctusername",
-                  "oeci_password": "correctpassword"})
-
-        assert self.service.client.cookie_jar._cookies[
-            "localhost.local"]["/"]["oeci_token"]
-
-        response = self.service.client.post("/api/search",
-            json=self.search_request_data)
-
-        assert(response.status_code == 200)
-        data = response.get_json()["data"]
-
-        """
-        Check that the resulting "record" field in the response matches
-        what we gave to the mock search function.
-
-        (use this json encode-decode approach because it turns a Record into a dict.)
-        """
-        assert data["record"] == json.loads(json.dumps(
-            self.mock_record["john_doe"], cls = ExpungeModelEncoder))
+    """
+    Check that the resulting "record" field in the response matches what we gave to the
+    mock search function.
+    (use this json encode-decode approach because it turns a Record into a dict.)
+    """
+    assert data["record"] == json.loads(json.dumps(
+        service.mock_record["john_doe"], cls = ExpungeModelEncoder))
 
 
-        self.service.check_search_result_saved(
-            self.service.user_data["user1"]["user_id"], self.search_request_data,
-            num_eligible_charges = 6, num_charges = 9)
+def test_search_fails_without_oeci_token(service):
+    service.login(service.user_data["user1"]["email"], service.user_data["user1"]["password"])
+
+    response = service.client.post("/api/search",
+        json=service.search_request_data)
+
+    assert(response.status_code == 401)
 
 
-    def test_search_with_failing_save_results(self, monkeypatch):
-        """
-        The search endpoint should succeed even if something goes wrong with the save-result step.
+def test_search_creates_save_results(service, monkeypatch):
+    """
+    This is the same test as above except it includes the save-results step. Less unit-y,
+    more of a vertical test.
+    """
 
-        """
+    service.login(service.user_data["user1"]["email"], service.user_data["user1"]["password"])
 
-        self.service.login(self.service.user_data["user1"]["email"], self.service.user_data["user1"]["password"])
-        monkeypatch.setattr(search.Crawler, "login", self.mock_login(True))
-        monkeypatch.setattr(search.Crawler, "search", self.mock_search("john_doe"))
-        monkeypatch.setattr(search, "save_result", self.mock_save_result_fail)
+    monkeypatch.setattr(search.Crawler, "login", mock_login(True))
+    monkeypatch.setattr(search.Crawler, "search", mock_search(service, "john_doe"))
 
-        self.service.client.post(
-            "/api/oeci_login",
-            json={"oeci_username": "correctusername",
-                  "oeci_password": "correctpassword"})
+    service.client.post(
+        "/api/oeci_login",
+        json={"oeci_username": "correctusername",
+              "oeci_password": "correctpassword"})
 
-        assert self.service.client.cookie_jar._cookies[
-            "localhost.local"]["/"]["oeci_token"]
+    assert service.client.cookie_jar._cookies[
+        "localhost.local"]["/"]["oeci_token"]
+
+    response = service.client.post("/api/search",
+        json=service.search_request_data)
+
+    assert(response.status_code == 200)
+    data = response.get_json()["data"]
+
+    """
+    Check that the resulting "record" field in the response matches
+    what we gave to the mock search function.
+
+    (use this json encode-decode approach because it turns a Record into a dict.)
+    """
+    assert data["record"] == json.loads(json.dumps(
+        service.mock_record["john_doe"], cls = ExpungeModelEncoder))
 
 
-        response = self.service.client.post("/api/search",
-            json=self.search_request_data)
+    service.check_search_result_saved(
+        service.user_data["user1"]["user_id"], service.search_request_data,
+        num_eligible_charges = 6, num_charges = 9)
 
-        assert(response.status_code == 200)
-        data = response.get_json()["data"]
 
-        """
-        Check that the resulting "record" field in the response matches what we gave to the
-        mock search function.
-        (use this json encode-decode approach because it turns a Record into a dict.)
-        """
-        assert data["record"] == json.loads(json.dumps(
-            self.mock_record["john_doe"], cls = ExpungeModelEncoder))
+def test_search_with_failing_save_results(service, monkeypatch):
+    """
+    The search endpoint should succeed even if something goes wrong with the save-result step.
+
+    """
+
+    service.login(service.user_data["user1"]["email"], service.user_data["user1"]["password"])
+    monkeypatch.setattr(search.Crawler, "login", mock_login(True))
+    monkeypatch.setattr(search.Crawler, "search", mock_search(service, "john_doe"))
+    monkeypatch.setattr(search, "save_result", mock_save_result_fail)
+
+    service.client.post(
+        "/api/oeci_login",
+        json={"oeci_username": "correctusername",
+              "oeci_password": "correctpassword"})
+
+    assert service.client.cookie_jar._cookies[
+        "localhost.local"]["/"]["oeci_token"]
+
+
+    response = service.client.post("/api/search",
+        json=service.search_request_data)
+
+    assert(response.status_code == 200)
+    data = response.get_json()["data"]
+
+    """
+    Check that the resulting "record" field in the response matches what we gave to the
+    mock search function.
+    (use this json encode-decode approach because it turns a Record into a dict.)
+    """
+    assert data["record"] == json.loads(json.dumps(
+        service.mock_record["john_doe"], cls = ExpungeModelEncoder))
