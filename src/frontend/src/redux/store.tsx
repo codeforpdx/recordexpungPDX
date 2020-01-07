25bae31d10f7e62b2ede85becee98c0786b28ebb
Jordan Witte <wittejm@gmail.com>
2020-01-06 21:21:25 -0800

    replace redux toolkit package dependency (#691)

--- src/frontend/src/redux/store.tsx
+++ src/frontend/src/redux/store.tsx
@@ -1,7 +1,7 @@
 // See the following guides for an explanation:
 // https://redux-starter-kit.js.org/usage/usage-guide
 // https://redux.js.org/recipes/usage-with-typescript
-import { configureStore, getDefaultMiddleware } from 'redux-starter-kit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 
 // Reducers:
