6b6d2ad013a3f7056798d82ebb34dde38f544924
Jordan Witte <wittejm@gmail.com>
2020-01-10 16:00:14 -0800

    missing disposition is not an error if the case is open (#715)
    also minor clarifications to other record error messages

--- src/backend/tests/functional_tests/test_crawler_expunger.py
+++ src/backend/tests/functional_tests/test_crawler_expunger.py
@@ -68,10 +68,11 @@ def record_without_dispos(crawler):
 
 def test_case_without_dispos(record_without_dispos):
     expunger = Expunger(record_without_dispos)
+    assert record_without_dispos.cases[0].closed()
     assert expunger.run()
     assert record_without_dispos.errors[0] == (
         f"""Case {record_without_dispos.cases[0].case_number} has a charge with a missing disposition.
-This is likely an error in the OECI database. Time analysis is ignoring this charge and may be inaccurate for other charges."""
+This might be an error in the OECI database. Time analysis is ignoring this charge and may be inaccurate for other charges."""
     )
 
 
@@ -85,7 +86,7 @@ def test_case_with_unrecognized_dispo(record_with_unrecognized_dispo):
     assert expunger.run()
     assert record_with_unrecognized_dispo.errors[0] == (
         f"""Case {record_with_unrecognized_dispo.cases[0].case_number}: Something unrecognized has a charge with an unrecognized disposition.
-This is likely an error in the OECI database. Time analysis is ignoring this charge and may be inaccurate for other charges."""
+This might be an error in the OECI database. Time analysis is ignoring this charge and may be inaccurate for other charges."""
     )
 
 
@@ -105,7 +106,7 @@ def test_case_with_mulitple_disposition_errors(record_with_multiple_disposition_
     expunger = Expunger(record_with_multiple_disposition_errors)
     assert expunger.run()
     unrecognized_error_message = f"""The following cases have charges with an unrecognized disposition.
-This is likely an error in the OECI database. Time analysis is ignoring these charges and may be inaccurate for other charges.
+This might be an error in the OECI database. Time analysis is ignoring these charges and may be inaccurate for other charges.
 Case numbers: """
     cases_order_1 = "X0001: Something unrecognized, X0002: Something unrecognized"
     cases_order_2 = "X0002: Something unrecognized, X0001: Something unrecognized"
@@ -114,7 +115,7 @@ Case numbers: """
         or unrecognized_error_message + cases_order_2 in record_with_multiple_disposition_errors.errors
     )
     missing_error_message = f"""Case X0003 has a charge with a missing disposition.
-This is likely an error in the OECI database. Time analysis is ignoring this charge and may be inaccurate for other charges."""
+This might be an error in the OECI database. Time analysis is ignoring this charge and may be inaccurate for other charges."""
     assert missing_error_message in record_with_multiple_disposition_errors.errors
 
 
