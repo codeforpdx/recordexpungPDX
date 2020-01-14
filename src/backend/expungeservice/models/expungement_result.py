1f65350487e796473abd7933d1e2285b6028a4b6
Jordan Witte <wittejm@gmail.com>
2020-01-14 11:29:00 -0800

    new charge_eligibility value computed from type and time eligibility

--- src/backend/expungeservice/models/expungement_result.py
+++ src/backend/expungeservice/models/expungement_result.py
@@ -10,6 +10,15 @@ class EligibilityStatus(str, Enum):
     INELIGIBLE = "Ineligible"
 
 
+class ChargeEligibilityStatus(str, Enum):
+    UNKNOWN = "Unknown"
+    ELIGIBLE_NOW = "Eligible now"
+    POSSIBLY_ELIGIBILE = "Possibly eligible"
+    WILL_BE_ELIGIBLE = "Will be eligible"
+    POSSIBLY_WILL_BE_ELIGIBLE = "Possibly will be eligible"
+    INELIGIBLE = "Ineligible"
+
+
 @dataclass
 class TypeEligibility:
     status: EligibilityStatus
@@ -24,9 +33,60 @@ class TimeEligibility:
 
 
 @dataclass
+class ChargeEligibility:
+    status: ChargeEligibilityStatus
+    label: str
+    date_will_be_eligible: Optional[date]
+
+
+@dataclass
 class ExpungementResult:
     type_eligibility: TypeEligibility
     time_eligibility: Optional[TimeEligibility]
 
     def set_type_eligibility(self, type_eligibility):
         self.type_eligibility = type_eligibility
+
+    @property
+    def charge_eligibility(self):
+        if self.type_eligibility.status == EligibilityStatus.ELIGIBLE:
+            if self.time_eligibility and self.time_eligibility.status == EligibilityStatus.ELIGIBLE:
+                return ChargeEligibility(ChargeEligibilityStatus.ELIGIBLE_NOW, "Eligible", None)
+
+            elif self.time_eligibility and self.time_eligibility.status == EligibilityStatus.INELIGIBLE:
+                if self.time_eligibility.date_will_be_eligible:
+                    return ChargeEligibility(
+                        ChargeEligibilityStatus.WILL_BE_ELIGIBLE,
+                        f"Eligible {self.time_eligibility.date_will_be_eligible.strftime('%b %-d, %Y')}",
+                        self.time_eligibility.date_will_be_eligible,
+                    )
+                else:
+                    # Currently, no charge types that are type-eligible can be disqualified due to a time-ineligibility date of None, meaning "never"
+                    # So this else block has no applicable cases and never runs. But it will apply if a new charge type that qualifies gets added.
+                    return ChargeEligibility(ChargeEligibilityStatus.INELIGIBLE, "Ineligible", None)
+            else:
+                return ChargeEligibility(
+                    ChargeEligibilityStatus.UNKNOWN, "Type-eligible but time analysis is missing", None
+                )
+
+        elif self.type_eligibility.status == EligibilityStatus.NEEDS_MORE_ANALYSIS:
+            if self.time_eligibility and self.time_eligibility.status == EligibilityStatus.ELIGIBLE:
+                return ChargeEligibility(ChargeEligibilityStatus.POSSIBLY_ELIGIBILE, "Possibly Eligible (review)", None)
+
+            elif self.time_eligibility and self.time_eligibility.status == EligibilityStatus.INELIGIBLE:
+                if self.time_eligibility.date_will_be_eligible:
+                    return ChargeEligibility(
+                        ChargeEligibilityStatus.POSSIBLY_WILL_BE_ELIGIBLE,
+                        f"Possibly Eligible {self.time_eligibility.date_will_be_eligible.strftime('%b %-d, %Y')} (review)",
+                        self.time_eligibility.date_will_be_eligible,
+                    )
+                else:
+                    # Currently, this occurs with Class B Felonies only, which can be time ineligible with a date of None, meaning "never"
+                    return ChargeEligibility(ChargeEligibilityStatus.INELIGIBLE, "Ineligible", None)
+            else:
+                return ChargeEligibility(
+                    ChargeEligibilityStatus.UNKNOWN, "Possibly eligible but time analysis is missing", None
+                )
+
+        elif self.type_eligibility.status == EligibilityStatus.INELIGIBLE:
+            return ChargeEligibility(ChargeEligibilityStatus.INELIGIBLE, "Ineligible", None)
--- src/frontend/src/components/Eligibility/index.tsx
+++ src/frontend/src/components/Eligibility/index.tsx
@@ -6,72 +6,33 @@ interface Props {
 }
 
 export default class Eligibility extends React.Component<Props> {
+
   render() {
     const {
-      type_eligibility,
-      time_eligibility
+      charge_eligibility
     } = this.props.expungement_result;
 
-    const eligibleNow = (
-      <h2 className="fw6 green bg-washed-green pv2 ph3 ma2 mb3 dib br3">
-        Eligible now
-      </h2>
-    );
-
-    const eligibleOn = (date: string) => (
-      <h2 className="fw6 dark-blue bg-washed-blue pv2 ph3 ma2 mb3 dib br3">
-        Eligible {date}
-      </h2>
-    );
-
-    const eligibleWithReview = (date: string) => (
-      <h2 className="fw6 purple bg-washed-purple pv2 ph3 ma2 mb3 dib br3">
-        Possibly Eligible {date} (review)
-      </h2>
-    );
-
-    const ineligible = (
-      <h2 className="fw6 red bg-washed-red pv2 ph3 ma2 mb3 dib br3">
-        Ineligible
-      </h2>
-    );
-
-    const handleWhenTypeEligibile = () => {
-      if (time_eligibility) {
-        if (time_eligibility.status === 'Eligible') {
-          return eligibleNow;
-        } else if (time_eligibility.date_will_be_eligible !== null) {
-          return eligibleOn(time_eligibility.date_will_be_eligible);
-        } else {
-          return  <h2 className="fw6 purple bg-washed-purple pv2 ph3 ma2 mb3 dib br3">Eligible but no date on time analysis - please report</h2>;
-        }
-      } else {
-        return <h2 className="fw6 purple bg-washed-purple pv2 ph3 ma2 mb3 dib br3">Eligible but no time analysis - please report</h2>;
-      }
-    };
+  const label_color = (charge_eligibility_status: string) => {
+    switch (charge_eligibility_status) {
+      case "Unknown":
+        return "purple bg-washed-purple";
+      case "Eligible now":
+        return "green bg-washed-green";
+      case "Possibly eligible":
+        return "purple bg-washed-purple"
+      case "Will be eligible":
+        return "dark-blue bg-washed-blue"
+      case "Possibly will be eligible":
+        return "purple bg-washed-purple"
+      case "Ineligible":
+        return "red bg-washed-red"
+    }
+  };
 
     const eligibility = () => {
-
-      // Time ineligibility without a date (meaning "never eligible") beats all other rules for eligibility
-      // Currently this only occurs on Class B felonies, but the rule theoretically applies always, so it is checked first.
-      if (time_eligibility && time_eligibility.status === 'Ineligible' && time_eligibility.date_will_be_eligible == null) {
-        return ineligible;
-      }
-
-      switch (type_eligibility.status) {
-        case 'Eligible':
-          return handleWhenTypeEligibile();
-        case 'Needs more analysis':
-          if (time_eligibility) {
-            return eligibleWithReview(time_eligibility.date_will_be_eligible);
-          } else {
-            return <h2 className="fw6 purple bg-washed-purple pv2 ph3 ma2 mb3 dib br3">Possibly eligible but no time analysis - please report</h2>;
-          }
-        case 'Ineligible':
-          return ineligible;
-        default:
-          return <h2 className="fw6 purple bg-washed-purple pv2 ph3 ma2 mb3 dib br3">Unknown type eligibility - please report</h2>;
-      }
+      return <h2 className= {label_color(charge_eligibility.status) + ' fw6 pv2 ph3 ma2 mb3 dib br3' }>
+        {charge_eligibility.label}
+      </h2>;
     };
 
     return eligibility();
