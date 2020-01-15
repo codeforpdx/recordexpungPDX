4e967b0596f3de62604c4812920f0887f5f60f1b
grbarker <bluecoast86@gmail.com>
2020-01-15 15:06:26 -0800

    Remove leftover lines from rebase.

--- src/frontend/src/components/UserList/index.tsx
+++ src/frontend/src/components/UserList/index.tsx
@@ -49,7 +49,6 @@ class UserList extends React.Component<Props> {
     }
   };
 
-<<<<<<< 3f16e340120dc82665e5ecd0793f996d6f2992ec
   displayUserList = () => {
     const returnList = this.props.users.userList.map(user => {
       return <User key={user.id} user={user} />;
@@ -84,72 +83,11 @@ class UserList extends React.Component<Props> {
       </div>
     </section>
   );
-||||||| merged common ancestors
-  public render() {
-    return (
-      <section className="cf bg-white shadow br3 mb5">
-        <div className="pv4 ph3">
-          <h1 className="f3 fw6 dib">Users</h1>
-          <button
-            onClick={() => history.push('add-user')}
-            className="bg-navy white bg-animate hover-bg-dark-blue fw6 br2 pv2 ph3 fr">
-            New User
-          </button>
-        </div>
-=======
-  public render() {
-    return (
-      <section className="cf bg-white shadow br3 mb5">
-        <div className="pv4 ph3">
-          <h1 className="f3 fw6 dib">Users</h1>
-          <button className="bg-navy white bg-animate hover-bg-dark-blue fw6 br2 pv2 ph3 fr">
-            New User
-          </button>
-        </div>
->>>>>>> Remove onClick in New User button.
 
-<<<<<<< 3f16e340120dc82665e5ecd0793f996d6f2992ec
   render() {
     return this.props.users.userList.length > 0
       ? this.displayUsers()
       : this.displayNoUsers();
-||||||| merged common ancestors
-        <div className="overflow-auto">
-          <table className="f6 w-100 mw8 center" data-cellspacing="0">
-            <thead>
-              <tr>
-                <th className="fw6 bb b--black-20 tl pb3 ph3 bg-white">Name</th>
-                <th className="fw6 bb b--black-20 tl pb3 ph3 bg-white">Role</th>
-                <th className="fw6 bb b--black-20 tl pb3 ph3 bg-white">
-                  Group
-                </th>
-              </tr>
-            </thead>
-            {this.props.users
-              ? this.displayUsers(this.props.users.userList)
-              : null}
-          </table>
-        </div>
-      </section>
-    );
-=======
-        <div className="overflow-auto">
-          <table className="f6 w-100 mw8 center" data-cellspacing="0">
-            <thead>
-              <tr>
-                <th className="fw6 bb b--black-20 tl pb3 ph3 bg-white">Name</th>
-                <th className="fw6 bb b--black-20 tl pb3 ph3 bg-white">Role</th>
-                <th className="fw6 bb b--black-20 tl pb3 ph3 bg-white">Group</th>
-              </tr>
-            </thead>
-            {this.props.users
-              ? this.displayUsers(this.props.users.userList)
-              : null}
-          </table>
-        </div>
-      </section>
-    );
->>>>>>> Remove onClick in New User button.
   }
 }
 
