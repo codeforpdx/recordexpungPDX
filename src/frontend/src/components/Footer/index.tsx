748a19253c334e090a72c98c7306054be4a87d8f
Forrest Longanecker <ForrLong.Tech@gmail.com>
2020-01-19 14:24:12 -0800

    footer now only renders while logged into the app, no longer displayed on landing page

--- src/frontend/src/components/Footer/index.tsx
+++ src/frontend/src/components/Footer/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { AppState } from '../../redux/store';
 
-export class Footer extends React.Component {
+interface Props {
+  isAuthenticated: boolean;
+}
+
+export class Footer extends React.Component<Props> {
   public render() {
-    return (
+    return this.props.isAuthenticated ? (
       <footer className="mt6 pt4 pb5 ph4 bg-white">
         <a className="link underline hover-blue" href="/">
           Terms of use
         </a>
       </footer>
-    );
+    ) : null;
   }
 }
 
-export default Footer;
+const mapStateToProps = (state: AppState) => ({
+  isAuthenticated: state.system.loggedIn
+});
+
+export default connect(mapStateToProps)(Footer);
