03437a7fb771b4a3385135c64abce4dab9c45670
Adam Emrich <adam.emrich@gmail.com>
2020-01-22 12:59:06 -0800

    Fixed issue with generative test (#752)
    Added HealthCheck.too_slow to test_serializer_generative.py
    Resolve https://github.com/codeforpdx/recordexpungPDX/issues/743

--- src/backend/tests/serializer/test_serializer_generative.py
+++ src/backend/tests/serializer/test_serializer_generative.py
@@ -1,12 +1,12 @@
 import json
 
-from hypothesis import given
+from hypothesis import given, settings, HealthCheck
 
 from expungeservice.expunger.expunger import Expunger
 from expungeservice.models.helpers.generator import build_record_strategy
 from expungeservice.serializer import ExpungeModelEncoder
 
-
+@settings(suppress_health_check=[HealthCheck.too_slow])
 @given(build_record_strategy())
 def test_round_trip_various_records(record):
     expunger = Expunger(record)
