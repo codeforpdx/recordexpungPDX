ee306129367266494fe6484e2930b4a04e2766ee
Jordan Witte <wittejm@gmail.com>
2020-02-06 12:12:30 -0800

    Adds the middle name field for record search.
    I chose to keep all the search fields on one line despite the mockup change, since the fields are still plenty large enough when set to 25% width.
    
    The button elements for add/remove extra aliases are included but hidden, since the formatting I used here is meant to account for the aliases feature.
    
    A refactor to abstract out a 'Field' component for the four fields is forthcoming.

--- src/frontend/src/components/RecordSearch/index.tsx
+++ src/frontend/src/components/RecordSearch/index.tsx
@@ -11,6 +11,7 @@ interface Props {
 
 interface State {
   firstName: string;
+  middleName: string;
   lastName: string;
   dateOfBirth: string;
   firstNameHasInput: boolean;
@@ -22,6 +23,7 @@ interface State {
 class RecordSearch extends React.Component<Props, State> {
   state: State = {
     firstName: '',
+    middleName: '',
     lastName: '', // Validation check relies on string length.
     dateOfBirth: '', // Moment expects a string to be passed in as a paramenter in the validateForm function.
     firstNameHasInput: false, // Initially set to false to ensure aria-invalid attribute is rendered.
@@ -48,9 +50,10 @@ class RecordSearch extends React.Component<Props, State> {
         // Dispatch an action.
         let state = this.state;
         let firstName = state.firstName;
+        let middleName = state.middleName;
         let lastName = state.lastName;
         let dateOfBirth = state.dateOfBirth.length > 0 ? state.dateOfBirth : '';
-        this.props.fetchRecords(firstName, lastName, dateOfBirth);
+        this.props.fetchRecords(firstName, middleName, lastName, dateOfBirth);
       }
     });
   };
@@ -79,7 +82,7 @@ class RecordSearch extends React.Component<Props, State> {
         <h1 className="mb4 f4 fw6">Record Search</h1>
         <form onSubmit={this.handleSubmit} noValidate>
           <div className="flex flex-wrap items-end">
-            <div className="w-100 w-30-ns mb3 pr2-ns">
+            <div className="w-100 w-25-ns mb3 pr2-ns">
               <label htmlFor="firstName" className="db mb1 fw6">
                 First Name
               </label>
@@ -95,7 +98,19 @@ class RecordSearch extends React.Component<Props, State> {
                 onChange={this.handleChange}
               />
             </div>
-            <div className="w-100 w-30-ns mb3 pr2-ns">
+            <div className="w-100 w-25-ns mb3 pr2-ns">
+              <label htmlFor="middleName" className="db mb1 ">
+                <span className= "fw6"> Middle Name </span> <span className= "fw1">(Optional) </span>
+              </label>
+              <input
+                id="middleName"
+                type="text"
+                className="w-100 pa3 br2 b--black-20"
+                required
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="w-100 w-25-ns mb3 pr2-ns">
               <label htmlFor="lastName" className="db mb1 fw6">
                 Last Name
               </label>
@@ -111,7 +126,7 @@ class RecordSearch extends React.Component<Props, State> {
                 onChange={this.handleChange}
               />
             </div>
-            <div className="w-100 w-30-ns mb3 pr2-ns">
+            <div className="w-100 w-25-ns mb3 pr2-ns">
               <label htmlFor="dateOfBirth" className="db mb1 fw6">
                 Date of Birth <span className="fw2 f6">MM/DD/YYYY</span>
               </label>
@@ -127,19 +142,39 @@ class RecordSearch extends React.Component<Props, State> {
                 onChange={this.handleChange}
               />
             </div>
-            <div className="w-100 w-10-ns mb3">
+
+            <div className="visually-hidden justify-between w-100 w-20-ns v-top">
+                <div className="mb3 mt2-ns">
+                  <button className="br2 bg-gray-blue-2 link hover-dark-blue mid-gray fw5 pv2 ph3 mt2">
+                    <i aria-hidden="true" className="fas fa-times-circle" />
+                    &nbsp;Remove
+                  </button>
+                </div>
+              </div>
+
+             <div className="visually-hidden justify-between w-100 w-two-thirds-ns">
+                <div className="mb3 mt2-ns">
+                  <button className="br2 bg-gray-blue-2 link hover-dark-blue mid-gray fw5 pv2 ph3 mt2 ml2">
+                    <i aria-hidden="true" className="fas fa-plus-circle" />
+                    &nbsp;Add alias
+                  </button>
+                </div>
+              </div>
+
+            <div className="w-100 mb3">
               <button
                 className="br2 bg-blue white bg-animate hover-bg-dark-blue db w-100 tc pv3 btn--search"
                 type="submit"
               >
                 <span className="visually-hidden">Search Records</span>
+
                 <i aria-hidden="true" className="fas fa-search" />
               </button>
             </div>
             <div role="alert" className="w-100">
               {this.state.missingInputs === true ? (
                 <p id="name_msg" className="bg-washed-red mv4 pa3 br3 fw6">
-                  All search fields are required.
+                  First and last name are required.
                 </p>
               ) : null}
               {this.state.invalidDate === true ? (
