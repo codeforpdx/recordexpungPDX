c2f7dd74b6c1375ac6597f6487e48c671244579a
Jordan Witte <wittejm@gmail.com>
2020-02-05 19:44:07 -0800

    Split Parking Ticket type into Parking Ticket and Civil Offense (#794)
    Split Parking Ticket type into Parking Ticket and Civil Offense
     - A charge is now only classified as a ParkingTicket if the case violation_type contains 'parking'
     - Chapters 1-99 now become classified instead as CivilOffense
     - Civil Offenses that are also Violations are classified as Violations with higher priority, because Violations have eligibility under 5d, whereas CivilOffenses are never eligible.

--- src/backend/expungeservice/models/charge_types/parking_ticket.py
+++ src/backend/expungeservice/models/charge_types/parking_ticket.py
@@ -6,10 +6,17 @@ from expungeservice.models.expungement_result import TypeEligibility, Eligibilit
 
 @dataclass(eq=False)
 class ParkingTicket(Charge):
+    """
+    This is a civil offense, and it is also a traffic offense.
+    """
+
     type_name: str = "Parking Ticket"
 
     def _default_type_eligibility(self):
-        return TypeEligibility(EligibilityStatus.INELIGIBLE, reason="Ineligible under 137.225(5)")
+        if self.convicted():
+            return TypeEligibility(EligibilityStatus.INELIGIBLE, reason="Ineligible under 137.225(7)(a)")
+        elif self.acquitted():
+            return TypeEligibility(EligibilityStatus.INELIGIBLE, reason="Ineligible by omission from statute")
 
     def skip_analysis(self):
         return True
