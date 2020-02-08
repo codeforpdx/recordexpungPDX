772b9a617879b8702e8072c4145e3ef2c58836cb
Jordan Witte <wittejm@gmail.com>
2020-02-08 13:58:21 -0800

    Cleanup to match latest design change.
     * Wrap only DOB to second line for medium-width screen.
    
     * Include "results" label and "+alias", "xremove" buttons for alias feature: (todo: hide them for final branch merge)
    
     * Inline comments to assist developing the new Aliases feature.

--- src/frontend/src/components/RecordSearch/index.tsx
+++ src/frontend/src/components/RecordSearch/index.tsx
@@ -78,99 +78,109 @@ class RecordSearch extends React.Component<Props, State> {
 
   public render() {
     return (
+      // TODO: verify Tachyon differences btwn this section tag and <main> in prototype
       <section className="cf mt4 mb3 pa3 pa4-l bg-white shadow br3">
-        <h1 className="mb4 f4 fw6">Record Search</h1>
-        <form onSubmit={this.handleSubmit} noValidate>
-          <div className="flex flex-wrap items-end">
-            <div className="w-100 w-50-m w-25-ns mb3 pr2-ns">
-              <label htmlFor="firstName" className="db mb1 fw6">
-                First Name
-              </label>
-              <input
-                id="firstName"
-                type="text"
-                className="w-100 pa3 br2 b--black-20"
-                required
-                aria-describedby={
-                  this.state.firstNameHasInput ? 'name_msg' : undefined
-                }
-                aria-invalid={this.state.firstNameHasInput}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="w-100 w-50-m w-25-ns mb3 pr2-ns">
-              <label htmlFor="middleName" className="db mb1 ">
-                <span className= "fw6"> Middle Name </span> <span className= "fw1">(Optional) </span>
-              </label>
-              <input
-                id="middleName"
-                type="text"
-                className="w-100 pa3 br2 b--black-20"
-                required
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="w-100 w-50-m w-25-ns mb3 pr2-ns">
-              <label htmlFor="lastName" className="db mb1 fw6">
-                Last Name
-              </label>
-              <input
-                id="lastName"
-                type="text"
-                className="w-100 pa3 br2 b--black-20"
-                required
-                aria-describedby={
-                  this.state.lastNameHasInput ? 'name_msg' : undefined
-                }
-                aria-invalid={this.state.lastNameHasInput}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="w-100 w-50-m w-25-ns mb3 pr2-ns">
-              <label htmlFor="dateOfBirth" className="db mb1 fw6">
-                Date of Birth <span className="fw2 f6">MM/DD/YYYY</span>
-              </label>
-              <input
-                id="dateOfBirth"
-                type="text"
-                className="w-100 pa3 br2 b--black-20"
-                required
-                aria-describedby={
-                  this.state.invalidDate ? 'dob_msg' : undefined
-                }
-                aria-invalid={this.state.invalidDate}
-                onChange={this.handleChange}
-              />
-            </div>
+        <h1 className="f4 fw6 tc mv4">Record Search</h1>
+        <section className="cf mt4 mb3 pa4 bg-white shadow br3">
+          <form className="mw7 center" onSubmit={this.handleSubmit} noValidate>
+            <div className="flex flex-wrap items-end">
 
-            <div className="visually-hidden justify-between w-100 w-20-ns v-top">
-                <div className="mb3 mt2-ns">
-                  <button className="br2 bg-gray-blue-2 link hover-dark-blue mid-gray fw5 pv2 ph3 mt2">
-                    <i aria-hidden="true" className="fas fa-times-circle" />
-                    &nbsp;Remove
-                  </button>
-                </div>
+              <div className="w-100 w-third-ns w-25-l mb3">
+              { // TODO: abstract this div to a "Field" react component,
+                // to instance as First, middle, last name, and bday
+              }
+                <label htmlFor="firstName" className="db mb1 fw6">
+                  First Name
+                </label>
+                <input
+                  id="firstName"
+                  type="text"
+                  className="w-100 pa3 br2 b--black-20"
+                  required
+                  aria-describedby={
+                    this.state.firstNameHasInput ? 'name_msg' : undefined
+                  }
+                  aria-invalid={this.state.firstNameHasInput}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="w-100 w-third-ns w-25-l mb3">
+                <label htmlFor="middleName" className="db mb1 fw6">
+                  Middle Name<span className= "fw2 f6">(Optional) </span>
+                </label>
+                <input
+                  id="middleName"
+                  type="text"
+                  className="w-100 pa3 br2 b--black-20"
+                  required
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="w-100 w-third-ns w-25-l mb3">
+                <label htmlFor="lastName" className="db mb1 fw6">
+                  Last Name
+                </label>
+                <input
+                  id="lastName"
+                  type="text"
+                  className="w-100 pa3 br2 b--black-20"
+                  required
+                  aria-describedby={
+                    this.state.lastNameHasInput ? 'name_msg' : undefined
+                  }
+                  aria-invalid={this.state.lastNameHasInput}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="w-100 w-third-ns w-25-l pl2-l mb3">
+                <label htmlFor="dateOfBirth" className="db mb1 fw6">
+                  Date of Birth <span className="fw2 f6">mm/dd/yyyy</span>
+                </label>
+                <input
+                  id="dateOfBirth"
+                  type="text"
+                  className="w-100 pa3 br2 b--black-20"
+                  required
+                  aria-describedby={
+                    this.state.invalidDate ? 'dob_msg' : undefined
+                  }
+                  aria-invalid={this.state.invalidDate}
+                  onChange={this.handleChange}
+                />
               </div>
 
-             <div className="visually-hidden justify-between w-100 w-two-thirds-ns">
-                <div className="mb3 mt2-ns">
-                  <button className="br2 bg-gray-blue-2 link hover-dark-blue mid-gray fw5 pv2 ph3 mt2 ml2">
-                    <i aria-hidden="true" className="fas fa-plus-circle" />
-                    &nbsp;Add alias
-                  </button>
-                </div>
+
+              <div className="flex items-center pb1 mb3 ml3-ns ml0-l">
+              { // TODO: keep the #-Results label and Remove buttons as "visually-hidden"
+                // until Aliases feature is complete.
+              }
+                <span className="fw5 bl bw2 b--blue bg-gray-blue-2 pa2 pr3 mr2 mb2">1 Result</span>
+                <button className="br2 bg-gray-blue-2 link hover-dark-blue mid-gray fw5 pa2 mb2">
+                  <i aria-hidden={"true"} className="fas fa-times-circle pr1"></i>Remove
+                </button>
+              </div>
               </div>
 
-            <div className="w-100 mb3">
-              <button
-                className="br2 bg-blue white bg-animate hover-bg-dark-blue db w-100 tc pv3 btn--search"
-                type="submit"
-              >
-                <span className="visually-hidden">Search Records</span>
+              {
+                // TODO: insert this when rendering additional Alias components.
+                //<hr className="ba b--white mt0 mb3" />
+              }
 
-                <i aria-hidden="true" className="fas fa-search" />
-              </button>
-            </div>
+              <div className="flex">
+              {  // Row containing The +Alias and search buttons.
+              }
+                <button className="w4 tc br2 bg-gray-blue-2 link hover-dark-blue mid-gray fw5 pv3 ph3 mr2">
+                {  // TODO: keep Alias button as "visually-hidden" until Aliases feature is complete.
+                }
+                  <i aria-hidden={"true"} className="fas fa-plus-circle pr1"></i>Alias
+
+                </button>
+                <button className="br2 bg-blue white bg-animate hover-bg-dark-blue db w-100 tc pv3 btn--search"  type="submit">
+                  <i aria-hidden="true" className="fas fa-search pr2"></i>
+                  <span className="fw7">Search</span>
+                </button>
+              </div>
+
             <div role="alert" className="w-100">
               {this.state.missingInputs === true ? (
                 <p id="name_msg" className="bg-washed-red mv4 pa3 br3 fw6">
@@ -183,8 +193,8 @@ class RecordSearch extends React.Component<Props, State> {
                 </p>
               ) : null}
             </div>
-          </div>
-        </form>
+          </form>
+        </section>
       </section>
     );
   }
