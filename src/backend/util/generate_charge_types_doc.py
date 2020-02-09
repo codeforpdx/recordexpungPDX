a668e59d18223498b8eb7f5e06a16687442ef756
Jordan Witte <wittejm@gmail.com>
2020-02-09 08:46:23 -0800

    Generate a user-readable doc file for charge types
    This provides a backend util script to generate the manual page for charge types.
    
    It currently generates a Markdown format, though this could be changed to HTML if that's how we provide documentation.
    
    This needs explicit imports in the charge_types in order to scoop up ChargeType subclasses. Apparently, obtaining all the subclasses is really hard without doing this step, and if someone can figure this out, please do! Unfortunately, this approach makes the code a little bit harder to maintain because we need to remember to add the import each time.
    
    The script also needs to be run explicitly to reflect changes, though adding a pre-commit hook to run the thing woud be fantastic. Automatically dropping the file into the correct location for the user-facing website is also a todo item as we put together the rest of the User Manual.

---
+++ src/backend/util/generate_charge_types_doc.py
@@ -0,0 +1,24 @@
+"""
+This util script generates the user-facing documentation for charge types as an md file.
+"""
+
+import pkgutil
+from expungeservice.models.charge import Charge
+
+
+def generate_charge_type_doc():
+
+    manual_doc_string = """# Charge Types
+The expungement system defines this custom set of charge types, to compute their correct type eligibility."
+
+"""
+    for charge_subclass in Charge.__subclasses__():
+        entry_string = f"## {charge_subclass.type_name}\n{charge_subclass.expungement_rules}\n\n\n"
+        manual_doc_string += entry_string
+
+    doc_file = open("../../doc/charge_types.md", "w")
+    doc_file.write(manual_doc_string[:-2])
+    doc_file.close()
+
+
+generate_charge_type_doc()
