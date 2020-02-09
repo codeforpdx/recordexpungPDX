f3ff78cd6c5456c0ccc63b1cee8d3c019ee5e6d1
Ryan Keppel <ryankeppel@M-2.local>
2020-02-09 11:58:01 -0800

    Clean up imports and do not instantiate crawler.

--- src/frontend/developerUtils/search.py
+++ src/frontend/developerUtils/search.py
@@ -1,22 +1,12 @@
-from itertools import groupby
-from typing import List
-
 from flask.views import MethodView
 from flask import request, current_app
 from flask_login import login_required
 import logging
 
 from expungeservice.models.helpers.generator import build_record
-from expungeservice.models.case import Case
-from expungeservice.models.record import Record
 from expungeservice.request import check_data_fields
 from expungeservice.request import error
-from expungeservice.crawler.crawler import Crawler
-from expungeservice.expunger import Expunger
 from expungeservice.serializer import ExpungeModelEncoder
-from expungeservice.crypto import DataCipher
-from expungeservice.stats import save_result
-from expungeservice.models.helpers.record_summarizer import RecordSummarizer
 
 
 class Search(MethodView):
@@ -29,8 +19,6 @@ class Search(MethodView):
 
         for alias in request_data["names"]:
             check_data_fields(alias, ["first_name", "last_name", "middle_name", "birth_date"])
-
-        crawler = Crawler()
 
         record = build_record()
         response_data = {"data": {"record": record}}
