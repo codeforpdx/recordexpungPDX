135556c3b15a4207c55b04c999ba9d6f4ce6a524
Jordan Witte <wittejm@gmail.com>
2020-02-10 15:55:50 -0800

    Fix Subsection12, add Subsection6, with tests and doc strings (#813)
    * Subsection12 is fixed to match the set of statutes listed in the expungement law. It has a new expungement_rules string to explain its weirdness. Also the type is now properly set to ELIGIBLE.
    
     * Subsection6 is introduced as a charge type to capture our out-of-date "List B" type and reflect the actual statutes given in Subsection 6 of the law.
    
    * Test cases are created for each of the statutes named in these two charge types.

--- src/backend/util/generate_charge_types_doc.py
+++ src/backend/util/generate_charge_types_doc.py
@@ -10,12 +10,11 @@ def generate_charge_type_doc():
 
     manual_doc_string = """# Charge Types
 The expungement system defines this custom set of charge types, to compute their correct type eligibility."
-
 """
     for charge_subclass in Charge.__subclasses__():
-        entry_string = f"## {charge_subclass.type_name}\n{charge_subclass.expungement_rules}\n\n\n"
+        entry_string = f"## {charge_subclass.type_name}\n{charge_subclass.expungement_rules}\n\n"
         manual_doc_string += entry_string
-
+    manual_doc_string += "\n"
     doc_file = open("../../doc/charge_types.md", "w")
     doc_file.write(manual_doc_string[:-2])
     doc_file.close()
