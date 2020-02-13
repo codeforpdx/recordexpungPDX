6d36bd7babe2b1193ea353ee47d500483f030967
aemrich <adam.emrich@gmail.com>
2020-02-12 19:52:39 -0800

    Change Class C String with Michael input
    Simplified expungement_rules string with Michael's input

--- src/backend/expungeservice/models/charge_types/felony_class_c.py
+++ src/backend/expungeservice/models/charge_types/felony_class_c.py
@@ -8,19 +8,12 @@ from expungeservice.models.expungement_result import TypeEligibility, Eligibilit
 class FelonyClassC(Charge):
     type_name: str = "Felony Class C"
     expungement_rules: str = (
-        """
-Class C felony convictions are almost always eligible under 137.225(5)(b).
-Class C felony dismissals are always eligible under 137.225(1)(b)
+        """There are certain types of Class C felony which are generally ineligible, including sex crimes, child
+abuse, elder abuse, traffic crimes, and criminally negligent homicide. Other Class C felony convictions are almost
+always eligible under 137.225(5)(b).
+Class C felony dismissals are always eligible under 137.225(1)(b).
 
-Class C felonies are lower in our hierarchy of type classifications, so many Class C
-felonies are superseded by their designations as other types. For example, if a Class C
-felony is a sex crime, its eligibility is first and foremost determined by its sex crime.
-
-Possible edge-case restrictions:
- * Criminally negligent homicide is not eligible if it is classified as a Class C felony, under 137.225(6)(d). This is an edge-case because criminally negligent homicide will almost always be a Class B felony.
-
-Applicable subsections: 137.225(5)(b) for convictions; 137.225(1)(b) for dismissals.
-"""
+Applicable subsections: 137.225(5)(b) for convictions; 137.225(1)(b) for dismissals."""
     )
 
     def _default_type_eligibility(self):
