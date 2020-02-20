c130ffde910117d9f2e5a13f28503d73a53d6604
Jordan Witte <wittejm@gmail.com>
2020-02-19 17:37:29 -0800

    verbiage fixes

--- src/backend/expungeservice/models/charge_types/subsection_6.py
+++ src/backend/expungeservice/models/charge_types/subsection_6.py
@@ -10,13 +10,14 @@ class Subsection6(Charge):
 
     expungement_rules: str = (
         """Subsection (6) names five felony statutes that have specific circumstances of the case under which they are ineligible.
-However, two of the statutes named -- 163.165(1)(h) (Assault in the third degree) and 163.205 (Criminal mistreatment in the first degree) are also named in subection 12 as eligible. Subsection 12 is interpreted to override other statutes, so in effect this clause of subsection 6 has no effect on eligibility.
+However, two of the statutes named -- 163.165(1)(h) (Assault in the third degree)
 This subsection also specifies conditions under which [SexCrimes](#SexCrime) are eligible or ineligible.
 The three remaining specifications in the statute are:
- * 163.200 (Criminal mistreatment in the second degree) is ineligible if the victim at the time of the crime was 65 years of age or older; otherwise eligible as a (Class A) [Misdemeanor](#Misdemeanor).
+ * 163.200 (Criminal mistreatment II) is ineligible if the victim at the time of the crime was 65 years of age or older; otherwise eligible as a (Class A) [Misdemeanor](#Misdemeanor).
+ * 163.205 (Criminal mistreatment I) is ineligible if the victim at the time of the crime was 65 years of age or older or a minor; otherwise eligible as a [Class C Felony](#FelonyClassC).
  * 163.575 (Endangering the welfare of a minor) (1)(a) is ineligible when the offense constitutes child abuse as defined in ORS 419B.005 (Definitions); otherwise eligible as a (Class A) [Misdemeanor](#Misdemeanor).
  * 163.145 (Criminally negligent homicide) is ineligible when that offense was punishable as a Class C felony.
-Applicable subsections: 137.225(6) for convictions; 137.225(1)(b) for dismissals."""
+Dismissals are eligible under 137.225(1)(b)."""
     )
 
     def _type_eligibility(self):
