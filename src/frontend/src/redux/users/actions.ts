bc0a6a1ddddb0dbf90de16a13cc98abea32e9cd5
Greg Barker <grbarker04@gmail.com>
2020-02-26 20:17:43 -0800

    Add action creator, reducer, and type for connecting AddUser component to backend

--- src/frontend/src/redux/users/actions.ts
+++ src/frontend/src/redux/users/actions.ts
@@ -1,6 +1,7 @@
 import { Dispatch } from 'redux';
-import { LOAD_USERS, CLEAR_USERS } from './types';
+import { LOAD_USERS, CLEAR_USERS, ADD_USER } from './types';
 import apiService from '../../service/api-service';
+import history from '../../service/history';
 
 export const loadUsers = () => {
   return (dispatch: Dispatch) => {
@@ -12,6 +13,22 @@ export const loadUsers = () => {
         type: LOAD_USERS,
         users: response.data.users
       });
+    });
+  };
+};
+
+export const addUser = (name: string, email: string, password: string, group_name: string, admin: boolean) => {
+  return (dispatch: Dispatch) => {
+    return apiService(dispatch, {
+      url: '/api/users',
+      data: { name, email, password, group_name, admin },
+      method: 'post',
+    }).then((response: any) => {
+      console.log(response);
+      dispatch({
+        type: ADD_USER,
+      });
+      history.push('/admin');
     });
   };
 };
