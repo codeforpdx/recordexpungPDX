054dfab763a6f5b033c7e74d9043bd82326bbc58
Adam Emrich <adam.emrich@gmail.com>
2020-03-01 15:09:39 -0800

    Handle felony c unrecognized (#890)
    * Adds support and tests in Felony C for no dispositions and unrecognized dispositions
    
    * Adjusts eligibility string for Felony Class C to better reflect laws

--- src/backend/tests/models/charge_types/test_felony_class_c.py
+++ src/backend/tests/models/charge_types/test_felony_class_c.py
@@ -5,8 +5,7 @@ from tests.factories.charge_factory import ChargeFactory
 from tests.models.test_charge import Dispositions
 
 
-# TODO: what is this test name?
-def test_misdemeanor_164055():
+def test_felony_c_conviction():
     charge_dict = ChargeFactory.default_dict(disposition=Dispositions.CONVICTED)
     charge_dict["name"] = "Theft in the first degree"
     charge_dict["statute"] = "164.055"
@@ -16,3 +15,47 @@ def test_misdemeanor_164055():
     assert isinstance(charge, FelonyClassC)
     assert charge.expungement_result.type_eligibility.status is EligibilityStatus.ELIGIBLE
     assert charge.expungement_result.type_eligibility.reason == "Eligible under 137.225(5)(b)"
+
+
+def test_felony_c_dismissal():
+    charge_dict = ChargeFactory.default_dict(disposition=Dispositions.DISMISSED)
+    charge_dict["name"] = "Theft in the first degree"
+    charge_dict["statute"] = "164.055"
+    charge_dict["level"] = "Felony Class C"
+    charge = ChargeFactory.create(**charge_dict)
+
+    assert isinstance(charge, FelonyClassC)
+    assert charge.expungement_result.type_eligibility.status is EligibilityStatus.ELIGIBLE
+    assert charge.expungement_result.type_eligibility.reason == "Dismissals are eligible under 137.225(1)(b)"
+
+
+def test_felony_c_no_disposition():
+    charge_dict = ChargeFactory.default_dict()
+    charge_dict["name"] = "Theft in the first degree"
+    charge_dict["statute"] = "164.055"
+    charge_dict["level"] = "Felony Class C"
+    charge_dict["disposition"] = None
+    charge = ChargeFactory.create(**charge_dict)
+
+    assert isinstance(charge, FelonyClassC)
+    assert charge.expungement_result.type_eligibility.status is EligibilityStatus.ELIGIBLE
+    assert (
+        charge.expungement_result.type_eligibility.reason
+        == "Eligible under 137.225(5)(b) for convictions or under 137.225(1)(b) for dismissals"
+    )
+
+
+def test_felony_c_unrecognized_disposition():
+    charge_dict = ChargeFactory.default_dict(disposition=Dispositions.UNRECOGNIZED_DISPOSITION)
+    charge_dict["name"] = "Theft in the first degree"
+    charge_dict["statute"] = "164.055"
+    charge_dict["level"] = "Felony Class C"
+    charge_dict["disposition"] = None
+    charge = ChargeFactory.create(**charge_dict)
+
+    assert isinstance(charge, FelonyClassC)
+    assert charge.expungement_result.type_eligibility.status is EligibilityStatus.ELIGIBLE
+    assert (
+        charge.expungement_result.type_eligibility.reason
+        == "Eligible under 137.225(5)(b) for convictions or under 137.225(1)(b) for dismissals"
+    )
