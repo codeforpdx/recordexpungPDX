7e245b431b62f34a63c4aaad98d22011f9c501fa
KorynLA <leslieak@oregonstate.edu>
2020-03-04 19:43:46 -0800

    Added unrecognized tests for None, convicted, and unrecognized

--- src/backend/tests/models/charge_types/test_unclassified.py
+++ src/backend/tests/models/charge_types/test_unclassified.py
@@ -31,3 +31,33 @@ def test_charge_that_falls_through():
     assert isinstance(charge, UnclassifiedCharge)
     assert charge.expungement_result.type_eligibility.status is EligibilityStatus.NEEDS_MORE_ANALYSIS
     assert charge.expungement_result.type_eligibility.reason == "Unrecognized Charge : Further Analysis Needed"
+
+def test_unrecognized_disposition():
+    charge_dict = ChargeFactory.default_dict(disposition=Dispositions.UNRECOGNIZED_DISPOSITION)
+    charge_dict["statute"] = "333.333"
+    charge_dict["level"] = "Felony Class F"
+    unclassified_dismissed = ChargeFactory.create(**charge_dict)
+   
+    assert isinstance(unclassified_dismissed, UnclassifiedCharge)
+    assert unclassified_dismissed.expungement_result.type_eligibility.status is EligibilityStatus.NEEDS_MORE_ANALYSIS
+    assert unclassified_dismissed.expungement_result.type_eligibility.reason == "Unrecognized Charge : Further Analysis Needed"
+
+def test_convicted_disposition():
+    charge_dict = ChargeFactory.default_dict(disposition=Dispositions.CONVICTED)
+    charge_dict["statute"] = "333.333"
+    charge_dict["level"] = "Felony Class F"
+    unclassified_convicted = ChargeFactory.create(**charge_dict)
+   
+    assert isinstance(unclassified_convicted, UnclassifiedCharge)
+    assert unclassified_convicted.expungement_result.type_eligibility.status is EligibilityStatus.NEEDS_MORE_ANALYSIS
+    assert unclassified_convicted.expungement_result.type_eligibility.reason == "Unrecognized Charge : Further Analysis Needed"
+
+def test_no_disposition():
+    charge_dict = ChargeFactory.default_dict(disposition=None)
+    charge_dict["statute"] = "333.333"
+    charge_dict["level"] = "Felony Class F"
+    unclassified_dispo_none = ChargeFactory.create(**charge_dict)
+   
+    assert isinstance(unclassified_dispo_none, UnclassifiedCharge)
+    assert unclassified_dispo_none.expungement_result.type_eligibility.status is EligibilityStatus.NEEDS_MORE_ANALYSIS
+    assert unclassified_dispo_none.expungement_result.type_eligibility.reason == "Unrecognized Charge : Further Analysis Needed"
