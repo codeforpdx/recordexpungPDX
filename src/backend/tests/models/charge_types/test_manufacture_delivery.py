22d2ec8e66f628a5f5d626265fda002d1aa341b9
Jordan Witte <wittejm@gmail.com>
2020-03-05 12:52:52 -0800

    fix tests to match recent refactor

--- src/backend/tests/models/charge_types/test_manufacture_delivery.py
+++ src/backend/tests/models/charge_types/test_manufacture_delivery.py
@@ -5,14 +5,9 @@ from tests.models.test_charge import Dispositions
 import pytest
 
 def test_manufacture_delivery_dismissed():
-    charge_dict = ChargeFactory.default_dict()
-    charge_dict["name"] = "Manufacture/Delivery"
-    charge_dict["statute"] = "4759922b"
-    charge_dict["level"] = "Felony Class A"
-    charge_dict["disposition"] = Dispositions.DISMISSED
-
-    manufacture_delivery_charge = ChargeFactory.create(**charge_dict)
-
+    manufacture_delivery_charge = ChargeFactory.create(
+        name="Manufacture/Delivery", statute="4759922b", level="Felony Class A", disposition=Dispositions.DISMISSED
+    )
 
     assert isinstance(manufacture_delivery_charge, ManufactureDelivery)
     assert manufacture_delivery_charge.expungement_result.type_eligibility.status is EligibilityStatus.ELIGIBLE
@@ -21,14 +16,9 @@ def test_manufacture_delivery_dismissed():
     )
 
 def test_manufacture_delivery_missing_disposition():
-    charge_dict = ChargeFactory.default_dict()
-    charge_dict["name"] = "Manufacture/Delivery"
-    charge_dict["statute"] = "4759922b"
-    charge_dict["level"] = "Felony Class A"
-    charge_dict["disposition"] = None
-
-    manufacture_delivery_charge = ChargeFactory.create(**charge_dict)
-
+    manufacture_delivery_charge = ChargeFactory.create(
+        name="Manufacture/Delivery", statute="4759922b", level="Felony Class A", disposition=None
+    )
 
     assert isinstance(manufacture_delivery_charge, ManufactureDelivery)
     assert manufacture_delivery_charge.expungement_result.type_eligibility.status is EligibilityStatus.NEEDS_MORE_ANALYSIS
@@ -37,14 +27,9 @@ def test_manufacture_delivery_missing_disposition():
     )
 
 def test_manufacture_delivery_unrecognized_disposition():
-    charge_dict = ChargeFactory.default_dict()
-    charge_dict["name"] = "Manufacture/Delivery"
-    charge_dict["statute"] = "4759922b"
-    charge_dict["level"] = "Felony Class B"
-    charge_dict["disposition"] = Dispositions.UNRECOGNIZED_DISPOSITION
-
-    manufacture_delivery_charge = ChargeFactory.create(**charge_dict)
-
+    manufacture_delivery_charge = ChargeFactory.create(
+        name="Manufacture/Delivery", statute="4759922b", level="Felony Class B", disposition=Dispositions.UNRECOGNIZED_DISPOSITION
+    )
 
     assert isinstance(manufacture_delivery_charge, ManufactureDelivery)
     assert manufacture_delivery_charge.expungement_result.type_eligibility.status is EligibilityStatus.NEEDS_MORE_ANALYSIS
@@ -53,13 +38,9 @@ def test_manufacture_delivery_unrecognized_disposition():
     )
 
 def test_manufacture_delivery_manudel():
-    charge_dict = ChargeFactory.default_dict()
-    charge_dict["name"] = "Manu/Del Cntrld Sub-SC 1"
-    charge_dict["statute"] = "4759921B"
-    charge_dict["level"] = "Felony Class A"
-    charge_dict["disposition"] = Dispositions.CONVICTED
-
-    manufacture_delivery_charge = ChargeFactory.create(**charge_dict)
+    manufacture_delivery_charge = ChargeFactory.create(
+        name="Manu/Del Cntrld Sub-SC 1", statute="4759921B", level="Felony Class A", disposition=Dispositions.CONVICTED
+    )
 
     assert isinstance(manufacture_delivery_charge, ManufactureDelivery)
     assert manufacture_delivery_charge.expungement_result.type_eligibility.status is EligibilityStatus.NEEDS_MORE_ANALYSIS
@@ -68,36 +49,24 @@ def test_manufacture_delivery_manudel():
     )
 
 def test_manufacture_delivery_manudel_felony_unclassified():
-    charge_dict = ChargeFactory.default_dict()
-    charge_dict["name"] = "Manu/Del Cntrld Sub-SC 1"
-    charge_dict["statute"] = "4759921B"
-    charge_dict["level"] = "Felony Unclassified"
-    charge_dict["disposition"] = Dispositions.CONVICTED
-
-    manufacture_delivery_charge = ChargeFactory.create(**charge_dict)
+    manufacture_delivery_charge = ChargeFactory.create(
+        name="Manu/Del Cntrld Sub-SC 1", statute="4759921B", level="Felony Unclassified", disposition=Dispositions.CONVICTED
+    )
 
     assert isinstance(manufacture_delivery_charge, ManufactureDelivery)
     assert manufacture_delivery_charge.expungement_result.type_eligibility.status is EligibilityStatus.NEEDS_MORE_ANALYSIS
 
 def test_manufacture_delivery_manufacturing_name():
-    charge_dict = ChargeFactory.default_dict()
-    charge_dict["name"] = "MANUFACTURING CONTROLLED SUB"
-    charge_dict["statute"] = "4759921A"
-    charge_dict["level"] = "Felony Unclassified"
-    charge_dict["disposition"] = Dispositions.CONVICTED
-
-    manufacture_delivery_charge = ChargeFactory.create(**charge_dict)
+    manufacture_delivery_charge = ChargeFactory.create(
+        name="MANUFACTURING CONTROLLED SUB", statute="4759921A", level="Felony Unclassified", disposition=Dispositions.CONVICTED
+    )
 
     assert isinstance(manufacture_delivery_charge, ManufactureDelivery)
     assert manufacture_delivery_charge.expungement_result.type_eligibility.status is EligibilityStatus.NEEDS_MORE_ANALYSIS
 
 def test_manufacture_delivery_1():
-    charge_dict = ChargeFactory.default_dict()
-    charge_dict["name"] = "MANUFACTURING CONTROLLED SUB 2"
-    charge_dict["statute"] = "4759921A"
-    charge_dict["level"] = "Felony Unclassified"
-    charge_dict["disposition"] = Dispositions.CONVICTED
-
-    manufacture_delivery_charge = ChargeFactory.create(**charge_dict)
+    manufacture_delivery_charge = ChargeFactory.create(
+        name="MANUFACTURING CONTROLLED SUB 2", statute="4759921A", level="Felony Unclassified", disposition=Dispositions.CONVICTED
+    )
 
     assert not isinstance(manufacture_delivery_charge, ManufactureDelivery)
--- src/backend/tests/models/charge_types/test_marijuana_eligible.py
+++ src/backend/tests/models/charge_types/test_marijuana_eligible.py
@@ -4,16 +4,10 @@ from tests.factories.charge_factory import ChargeFactory
 from tests.models.test_charge import Dispositions
 import pytest
 
-delivery_to_minor_charge_dict = ChargeFactory.default_dict()
-delivery_to_minor_charge_dict["name"] = "Delivery of Marijuana to Minor"
-delivery_to_minor_charge_dict["statute"] = "4758604A"
-delivery_to_minor_charge_dict["level"] = "Felony Class A"
-
 def test_delivery_to_minor_4758604A():
-    delivery_to_minor_charge_dict["disposition"] = Dispositions.DISMISSED
-
-    marijuana_eligible_charge = ChargeFactory.create(**delivery_to_minor_charge_dict)
-
+    marijuana_eligible_charge = ChargeFactory.create(
+        name="Delivery of Marijuana to Minor", statute="4758604A", level="Felony Class A", disposition=Dispositions.DISMISSED
+    )
     assert isinstance(marijuana_eligible_charge, MarijuanaEligible)
     assert marijuana_eligible_charge.expungement_result.type_eligibility.status is EligibilityStatus.ELIGIBLE
     assert (
@@ -21,9 +15,9 @@ def test_delivery_to_minor_4758604A():
     )
 
 def test_marijuana_eligible_convicted():
-    delivery_to_minor_charge_dict["disposition"] = Dispositions.CONVICTED
-
-    marijuana_eligible_charge = ChargeFactory.create(**delivery_to_minor_charge_dict)
+    marijuana_eligible_charge = ChargeFactory.create(
+        name="Delivery of Marijuana to Minor", statute="4758604A", level="Felony Class A", disposition=Dispositions.CONVICTED
+    )
 
     assert isinstance(marijuana_eligible_charge, MarijuanaEligible)
     assert marijuana_eligible_charge.expungement_result.type_eligibility.status is EligibilityStatus.ELIGIBLE
@@ -32,9 +26,9 @@ def test_marijuana_eligible_convicted():
     )
 
 def test_marijuana_eligible_missing_dispo():
-    delivery_to_minor_charge_dict["disposition"] = None
-
-    marijuana_eligible_charge = ChargeFactory.create(**delivery_to_minor_charge_dict)
+    marijuana_eligible_charge = ChargeFactory.create(
+        name="Delivery of Marijuana to Minor", statute="4758604A", level="Felony Class A", disposition=None
+    )
 
     assert isinstance(marijuana_eligible_charge, MarijuanaEligible)
     assert marijuana_eligible_charge.expungement_result.type_eligibility.status is EligibilityStatus.ELIGIBLE
@@ -43,9 +37,9 @@ def test_marijuana_eligible_missing_dispo():
     )
 
 def test_marijuana_eligible_unrecognized_dispo():
-    delivery_to_minor_charge_dict["disposition"] = Dispositions.UNRECOGNIZED_DISPOSITION
-
-    marijuana_eligible_charge = ChargeFactory.create(**delivery_to_minor_charge_dict)
+    marijuana_eligible_charge = ChargeFactory.create(
+        name="Delivery of Marijuana to Minor", statute="4758604A", level="Felony Class A", disposition=Dispositions.UNRECOGNIZED_DISPOSITION
+    )
 
     assert isinstance(marijuana_eligible_charge, MarijuanaEligible)
     assert marijuana_eligible_charge.expungement_result.type_eligibility.status is EligibilityStatus.ELIGIBLE
@@ -54,12 +48,8 @@ def test_marijuana_eligible_unrecognized_dispo():
     )
 
 def test_delivery_4758602():
-    charge_dict = ChargeFactory.default_dict()
-    charge_dict["name"] = "Delivery of Marijuana for Consideration"
-    charge_dict["statute"] = "4758602"
-    charge_dict["level"] = "Felony Class B"
-    charge_dict["disposition"] = Dispositions.CONVICTED
-
-    marijuana_eligible_charge = ChargeFactory.create(**charge_dict)
+    marijuana_eligible_charge = ChargeFactory.create(
+        name="Delivery of Marijuana for Consideration", statute="4758602", level="Felony Class B", disposition=Dispositions.CONVICTED
+    )
 
     assert isinstance(marijuana_eligible_charge, MarijuanaEligible)
