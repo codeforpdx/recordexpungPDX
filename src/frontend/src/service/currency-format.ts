d47a215a4ed719f4adade28bc115034cfc501bf5
Ryan Keppel <ryankeppel@M-2.local>
2020-03-06 23:29:59 -0800

    Use const and name return variable.

--- src/frontend/src/service/currency-format.ts
+++ src/frontend/src/service/currency-format.ts
@@ -1,8 +1,8 @@
 export default function currencyFormat(input: number) {
-  var wholePart = Math.trunc(input);
-  var wholeStr = wholePart.toLocaleString();
-  var fractionPart = Math.abs(input) % 1;
-  var fractionStr = fractionPart.toFixed(2).substring(1);
-  var rt = '$' + wholeStr + fractionStr;
-  return rt;
+  const wholePart = Math.trunc(input);
+  const wholeStr = wholePart.toLocaleString();
+  const fractionPart = Math.abs(input) % 1;
+  const fractionStr = fractionPart.toFixed(2).substring(1);
+  const currencyString = '$' + wholeStr + fractionStr;
+  return currencyString;
 }
