3666f793ee69cd0b6450efce963644af269346b5
Ryan Keppel <ryankeppel@M-2.local>
2020-03-06 23:32:52 -0800

    Rename var in test.

--- src/frontend/src/service/currency-format.test.ts
+++ src/frontend/src/service/currency-format.test.ts
@@ -1,13 +1,13 @@
 import currencyFormat from './currency-format.ts';
 describe('CURRENCY FORMAT SERVICE TEST', () => {
   it('formats currency', () => {
-    var str = currencyFormat(145);
-    expect(str).toEqual('$145.00');
-    str = currencyFormat(1231.45);
-    expect(str).toEqual('$1,231.45');
-    str = currencyFormat(0);
-    expect(str).toEqual('$0.00');
-    str = currencyFormat(-422.42);
-    expect(str).toEqual('$-422.42');
+    var currencyString = currencyFormat(145);
+    expect(currencyString).toEqual('$145.00');
+    currencyString = currencyFormat(1231.45);
+    expect(currencyString).toEqual('$1,231.45');
+    currencyString = currencyFormat(0);
+    expect(currencyString).toEqual('$0.00');
+    currencyString = currencyFormat(-422.42);
+    expect(currencyString).toEqual('$-422.42');
   });
 });
