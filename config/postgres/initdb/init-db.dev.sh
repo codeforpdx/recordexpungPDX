676d986f4abb2a9f59b21becf060a47df9ec92f1
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2020-03-11 22:31:49 -0700

    refactor local dev environment
    * use alpine-based images
    * use library images where possible
    * deprecate nginx (use react-scripts hmr server)
    * add make targets
        * `dev_initdb` - initialize/seed the dev database
        * `dev_dropdb` - drop the dev database
        * `dev_backend_test`  - run mypy and pytest via pipenv in expungeservice container
        * `dev_frontend_test` - run npm test in node container
    * update make targets
        * `dev_up` - use new default compose file
        * `dev_down` - use new default compose file

--- config/postgres/initdb/init-db.dev.sh
+++ config/postgres/initdb/init-db.dev.sh
@@ -1,39 +1,9 @@
 #!/bin/bash
 set -e
 
-# Create database
+cwd=`dirname $0`
 
-psql \
-    -v ON_ERROR_STOP=1 \
-    -U "${POSTGRES_USERNAME}" \
-    -d "${POSTGRES_USERNAME}" \
-    -v POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" \
-    <<-EOSQL
-    CREATE DATABASE ${PGDATABASE};
-EOSQL
-
-# Create tables and functions.
-
- # this syntax stopped working I guess? -v POSTGRES_USER="${POSTGRES_USERNAME}"
- # replaced with: -U "${POSTGRES_USERNAME}"
-
-psql \
-    -v ON_ERROR_STOP=1 \
-    -U "${POSTGRES_USERNAME}" \
-    -v POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" \
-    -d "${PGDATABASE}" \
-    -f /docker-entrypoint-initdb.d/scripts/create-tables.sql
-
-psql \
-    -v ON_ERROR_STOP=1 \
-    -U "${POSTGRES_USERNAME}" \
-    -v POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" \
-    -d "${PGDATABASE}" \
-    -f /docker-entrypoint-initdb.d/scripts/create-functions.sql
-
-psql \
-    -v ON_ERROR_STOP=1 \
-    -U "${POSTGRES_USERNAME}" \
-    -v POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" \
-    -d "${PGDATABASE}" \
-    -f /docker-entrypoint-initdb.d/scripts/initial_credentials.dev.sql
+createdb ${PGDATABASE}
+psql -f ${cwd}/scripts/create-tables.sql
+psql -f ${cwd}/scripts/create-functions.sql
+psql -f ${cwd}/scripts/initial_credentials.dev.sql
