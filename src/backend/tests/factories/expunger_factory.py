41f577c6d45b5a3266cccf026945f5ffe38b5267
Kent Shikama <kent@kentshikama.com>
2020-03-20 03:35:50 -0700

    Remove unused expunger factory

--- src/backend/tests/factories/expunger_factory.py
+++
@@ -1,8 +0,0 @@
-from expungeservice.expunger import Expunger
-from expungeservice.models.record import Record
-
-
-class ExpungerFactory:
-    @staticmethod
-    def create():
-        return Expunger(Record([]))
