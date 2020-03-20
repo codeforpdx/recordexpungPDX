e03a8023ceb868062322bbccf22abe75bf4c33cb
Kent Shikama <kent@kentshikama.com>
2020-03-20 03:10:00 -0700

    Replace crawler factory custom logic with RecordCreator
    We now can finally replace our hacky temporary "search endpoint" mock with the same record creating logic that is used in our non-test (production) code. This was surprisingly difficult to do cleanly before removing the Case weakref from Charge.

--- src/backend/tests/factories/crawler_factory.py
+++ src/backend/tests/factories/crawler_factory.py
@@ -1,10 +1,8 @@
-from itertools import product
-
 import requests_mock
 
 from expungeservice.crawler.crawler import Crawler
 from expungeservice.crawler.request import URL
-from expungeservice.models.record import Record
+from expungeservice.record_creator import RecordCreator
 from tests.fixtures.post_login_page import PostLoginPage
 from tests.fixtures.search_page_response import SearchPageResponse
 from tests.fixtures.case_details import CaseDetails
@@ -18,10 +16,8 @@ class CrawlerFactory:
         cases={"X0001": CaseDetails.case_x(), "X0002": CaseDetails.case_x(), "X0003": CaseDetails.case_x()},
     ):
         base_url = "https://publicaccess.courts.oregon.gov/PublicAccessLogin/"
-        crawler = Crawler()
         with requests_mock.Mocker() as m:
             m.post(URL.login_url(), text=PostLoginPage.POST_LOGIN_PAGE)
-            crawler.login("username", "password")
 
             m.post(
                 "{}{}".format(base_url, "Search.aspx?ID=100"), [{"text": SearchPageResponse.RESPONSE}, {"text": record}]
@@ -30,7 +26,5 @@ class CrawlerFactory:
             for key, value in cases.items():
                 m.get("{}{}{}".format(base_url, "CaseDetail.aspx?CaseID=", key), text=value)
 
-            ambiguous_cases = crawler.search("John", "Doe")
-            cases_list = [list(cases) for cases in product(*ambiguous_cases)]
-            ambiguous_record = [Record(cases) for cases in cases_list]
-            return ambiguous_record[0]  # TODO: We currently pretend we cannot get an ambiguous record/case/charge
+            aliases = [{"first_name": "John", "last_name": "Doe", "middle_name": "", "birth_date": ""}]
+            return RecordCreator.build_record("username", "password", aliases)
