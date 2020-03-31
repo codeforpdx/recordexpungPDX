1cff043aa513c539d4133a3d7ca5b415789ecf20
Kent Shikama <kent@kentshikama.com>
2020-03-31 05:46:28 -0700

    Add server side Flask sessions
    The default client-side session storage was not designed to pass back and forth large amounts of data to and from the backend. We eventually might want to consider moving to redis for performance.
    
    We should install git in the expungeservicebase but I have no access to the dockerhub account.

--- src/backend/expungeservice/config.py
+++ src/backend/expungeservice/config.py
@@ -12,6 +12,7 @@ class Development(object):
     SECRET_KEY = "1234567890987654321234567890987654321234567890987654321234567890987654321234567890987654321234567"
     JWT_EXPIRY_TIMER = datetime.timedelta(minutes=60)
     SESSION_COOKIE_SECURE = False
+    SESSION_TYPE = "filesystem"
 
 
 class Production(object):
@@ -24,6 +25,7 @@ class Production(object):
     SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_EXPIRY_TIMER = datetime.timedelta(minutes=60)
     SESSION_COOKIE_SECURE = True
+    SESSION_TYPE = "filesystem"
 
 
 app_config = {
