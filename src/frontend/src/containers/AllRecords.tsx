2d09c646e00837f5b20e33cac244fe0c5ef19131
Hunter Marcks <hmarcks@gmail.com>
2020-04-01 16:35:23 -0500

    adds assumptions

--- src/frontend/src/containers/AllRecords.tsx
+++ src/frontend/src/containers/AllRecords.tsx
@@ -38,6 +38,17 @@ class AllRecords extends Component<Props> {
         ) : (
           <AllStatus />
         )}
+        <div className="bg-white shadow mv4 pa4 br3">
+          <h2 className="fw6 mb3">Assumptions</h2>
+          <p className="mb3">We are only able to access your public Oregon records.</p>
+          <p className="mb2">Your analysis may be different if you have had cases which were:</p>
+          <ul className="lh-copy pl4 mw6">
+            <li className="mb2">Previously expunged</li>
+            <li className="mb2">From States besides Oregon within the last ten years</li>
+            <li className="mb2">From Federal Court within the last ten years</li>
+            <li className="mb2">From local District Courts, e.g. Medford Municipal Court (not Jackson County Circuit Court) from within the last ten years</li>
+          </ul>
+        </div>
       </main>
     );
   }
