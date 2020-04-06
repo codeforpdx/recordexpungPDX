9483e48bae4d0cb237db477a09e9bb81f010d8a1
Jonathan James <jjamesnwa1011@gmail.com>
2020-04-06 12:11:40 -0700

    updated line spacing in test

--- src/backend/tests/test_sort_record_by_date.py
+++ src/backend/tests/test_sort_record_by_date.py
@@ -18,7 +18,6 @@ def test_sort_by_case_date():
     mock_data = CrawlerFactory.create()
     sortedRecord = RecordCreator.sort_record_by_case_date(mock_data)
     sorted_mock_cases = sortedRecord.cases
-
     assert sorted_mock_cases[0].date == datetime.date(2012, 4, 1)
     assert sorted_mock_cases[1].date == datetime.date(1963, 4, 11) #sorted by case date
     assert sorted_mock_cases[2].date == datetime.date(1963, 3, 23)
@@ -30,16 +29,12 @@ def test_sort_if_all_dates_are_same():
     mock_cases[0].date = datetime.date(1963, 3, 23)
     mock_cases[1].date = datetime.date(1963, 3, 23)
     mock_cases[2].date = datetime.date(1963, 3, 23)
-    
     # Before sort
     assert mock_cases[0].case_number == 'X0001'
     assert mock_cases[1].case_number == 'X0002'
     assert mock_cases[2].case_number == 'X0003'
-    
-    
     sortedData = RecordCreator.sort_record_by_case_date(mock_data)
     sortedCases = sortedData.cases
-    
     #after sort should be same case order
     assert sortedCases[0].case_number == 'X0001'
     assert sortedCases[1].case_number == 'X0002'
