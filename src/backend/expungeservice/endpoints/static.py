d0ffdf1ee425d47c0486e240c42119fb02402697
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2020-04-06 12:34:44 -0700

    add explicit http methods and negative handler to static catch all route

--- src/backend/expungeservice/endpoints/static.py
+++ src/backend/expungeservice/endpoints/static.py
@@ -1,13 +1,18 @@
 import os.path
+from flask import abort
 
 def register(app):
     # mimic nginx 'try_files' directive for react
     #
     @app.route("/", defaults={"path": ""})
-    @app.route("/<path:path>")
+    @app.route("/<path:path>", methods=["GET", "HEAD", "OPTIONS"])
     def try_files_static_index(path):
         static_path = os.path.join(app.static_folder, path)
         if os.path.exists(static_path) and not os.path.isdir(static_path):
             return app.send_static_file(path)
         else:
             return app.send_static_file("index.html")
+
+    @app.route("/<path:path>", methods=["POST", "PUT", "PATCH", "DELETE"])
+    def bad_static_file_http_method(path):
+        abort(404)
