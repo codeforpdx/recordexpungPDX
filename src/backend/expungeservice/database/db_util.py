2a2b1b580f2debf78d44cc46c112fb172367e350
Kent Shikama <kent@kentshikama.com>
2020-04-22 07:03:42 -0700

    Move disambiguate endpoint functions into search endpoint
    This temporarily duplicates functionality as the frontend needs to be updated to use the new search endpoint for both calls.

--- src/backend/expungeservice/database/db_util.py
+++ src/backend/expungeservice/database/db_util.py
@@ -5,6 +5,7 @@ import uuid
 import hashlib
 from flask_login import current_user
 
+
 def rollback_errors(db_operation):
     @functools.wraps(db_operation)
     def wrapper(database, *args, **kwargs):
@@ -18,9 +19,9 @@ def rollback_errors(db_operation):
     return wrapper
 
 
-def save_search_event(request_data, record):
+def save_search_event(aliases_data):
     user_id = current_user.user_id
-    search_param_string = user_id + json.dumps(request_data["aliases"], sort_keys=True)
+    search_param_string = user_id + json.dumps(aliases_data, sort_keys=True)
     hashed_search_params = hashlib.sha256(search_param_string.encode()).hexdigest()
     _db_insert_search_event(g.database, user_id, hashed_search_params)
 
@@ -32,8 +33,5 @@ def _db_insert_search_event(database, user_id, hashed_search_params):
         INSERT INTO  SEARCH_RESULTS(search_result_id, user_id, hashed_search_params)
         VALUES ( uuid_generate_v4(), %(user_id)s, %(params)s);
         """,
-        {
-            "user_id": uuid.UUID(user_id).hex,
-            "params": hashed_search_params,
-        },
+        {"user_id": uuid.UUID(user_id).hex, "params": hashed_search_params,},
     )
--- src/backend/tests/test_save_search_event.py
+++ src/backend/tests/test_save_search_event.py
@@ -2,9 +2,9 @@ import pytest
 from flask import g
 
 from tests.endpoints.endpoint_util import EndpointShared
-from tests.factories.crawler_factory import CrawlerFactory
 from expungeservice.database import get_database
 from expungeservice.database.db_util import save_search_event
+
 
 @pytest.fixture
 def service():
@@ -23,17 +23,9 @@ def test_save_search_event(service):
         request_data = {
             "aliases": [{"first_name": "John", "last_name": "Doe", "middle_name": "Test", "birth_date": "01/01/1980",}]
         }
-
-        record = CrawlerFactory.create()
-
         service.login(service.user_data["user1"]["email"], service.user_data["user1"]["password"])
-
         g.database = get_database()
-
-        save_search_event(request_data, record)
-
+        save_search_event(request_data["aliases"])
         g.database.connection.commit()
 
-    service.check_search_event_saved(
-        service.user_data["user1"]["user_id"], request_data
-    )
+    service.check_search_event_saved(service.user_data["user1"]["user_id"], request_data)
