dcbabe257171d96970f1cd0736a3d5d782765a2f
Kent Shikama <kent@kentshikama.com>
2020-05-07 22:26:08 -0700

    Apply minor formatting modification and refactor to auth

--- src/pdf-frontend/auth.py
+++ src/pdf-frontend/auth.py
@@ -24,13 +24,9 @@ class Auth:
         oeci_logged_in = False
         while not oeci_logged_in:
             oeci_credentials = {
-                "oeci_username": input("OECI email address: "),
+                "oeci_username": input("OECI username: "),
                 "oeci_password": getpass.getpass("Password: "),
             }
-            if not oeci_credentials:
-                oeci_credentials["oeci_username"] = input("OECI username: ")
-                oeci_credentials["oeci_password"] = input("Password: ")
-
             oeci_login_response = client.post("http://localhost:5000/api/oeci_login", json=oeci_credentials)
             if oeci_login_response.status_code == 201:
                 oeci_logged_in = True
