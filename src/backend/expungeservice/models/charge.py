c8702c9de873f75ae4a43d7970eb5c792b0876d7
Jordan Witte <wittejm@gmail.com>
2020-06-11 15:30:39 -0700

    Adjust backend for prettier badge display

--- src/backend/expungeservice/models/charge.py
+++ src/backend/expungeservice/models/charge.py
@@ -105,5 +105,5 @@ class Charge(OeciCharge):
         short_name = self.name.split("(")[0]
         arrested_date = self.date.strftime("%b %-d, %Y")
         disposition = str(self.disposition.status.name)
-        owed = f" - $ owed" if self.balance_due_in_cents > 0 else ""
+        owed = f" $ owed" if self.balance_due_in_cents > 0 else ""
         return f"{short_name} ({disposition}) - Arrested {arrested_date}{owed}"
--- src/backend/tests/pdf/test_markdown_serializer.py
+++ src/backend/tests/pdf/test_markdown_serializer.py
@@ -9,22 +9,22 @@ from tests.factories.crawler_factory import CrawlerFactory
 from tests.fixtures.case_details import CaseDetails
 from tests.fixtures.john_doe import JohnDoe
 
-EXPECTED_MARKDOWN = """# EXPUNGEMENT ANALYSIS REPORT  
-## Search Terms  
-Name: JOHN  SMITH DOB: 2/2/2020  
-Name: JOHN  DOE  
-  
-  
-## Charges Eligible Now  
- - Possession of Cocaine (DISMISSED) - Arrested Feb 17, 2009 - $ owed  
-  
-  
-## Future Eligible Charges  
-The following charges (dismissed and convicted) are eligible at the designated dates.  
-### Eligible Nov 9, 2020  
- - Possession of Cocaine (CONVICTED) - Arrested Jun 13, 2009 - $ owed  
+EXPECTED_MARKDOWN = """# EXPUNGEMENT ANALYSIS REPORT
+## Search Terms
+Name: JOHN  SMITH DOB: 2/2/2020
+Name: JOHN  DOE
 
-  
+
+## Charges Eligible Now
+ - Possession of Cocaine (DISMISSED) - Arrested Feb 17, 2009 $ owed
+
+
+## Future Eligible Charges
+The following charges (dismissed and convicted) are eligible at the designated dates.
+### Eligible Nov 9, 2020
+ - Possession of Cocaine (CONVICTED) - Arrested Jun 13, 2009 $ owed
+
+
 """
 
 
