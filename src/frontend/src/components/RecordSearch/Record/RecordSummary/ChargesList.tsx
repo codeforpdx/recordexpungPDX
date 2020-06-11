a0c3623435dd1b68964b55ef7b4644e6f86be9de
Jordan Witte <wittejm@gmail.com>
2020-06-11 15:38:19 -0700

    Prettier balance badge in summary panel

--- src/frontend/src/components/RecordSearch/Record/RecordSummary/ChargesList.tsx
+++ src/frontend/src/components/RecordSearch/Record/RecordSummary/ChargesList.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 interface Props {
-  eligibleChargesByDate: { [label: string]: any[]; };
+  eligibleChargesByDate: { [label: string]: any[] };
   totalCases: number;
   totalCharges: number;
 }
@@ -10,54 +10,88 @@ export default class ChargesList extends React.Component<Props> {
   render() {
     const summarizedCharges = Object.entries(this.props.eligibleChargesByDate)
       .sort((a, b) => this.extractLabel(a).localeCompare(this.extractLabel(b)))
-      .map((([eligibilityDate, chargesNames]: [string, any]) => {
-      const listItems = this.buildListItems(chargesNames);
-      const labelColor = (eligibilityDate==="Eligible Now" ? "green" : eligibilityDate==="Ineligible" ? "red" : eligibilityDate==="Needs More Analysis" ? "purple" : "dark-blue");
-      const SHOW_ALL_CHARGES_THRESHOLD = 20;
-      return (
-        <div key={eligibilityDate}>
-          <div className="mb1">
-            <span className={"fw7 ttc mb2 " + labelColor}> {eligibilityDate} </span>
-            <span> {(chargesNames.length > 0 ? `(${chargesNames.length})` : "" )} </span>
+      .map(([eligibilityDate, chargesNames]: [string, any]) => {
+        const listItems = this.buildListItems(chargesNames);
+        const labelColor =
+          eligibilityDate === "Eligible Now"
+            ? "green"
+            : eligibilityDate === "Ineligible"
+            ? "red"
+            : eligibilityDate === "Needs More Analysis"
+            ? "purple"
+            : "dark-blue";
+        const SHOW_ALL_CHARGES_THRESHOLD = 20;
+        return (
+          <div key={eligibilityDate}>
+            <div className="mb1">
+              <span className={"fw7 ttc mb2 " + labelColor}>
+                {" "}
+                {eligibilityDate}{" "}
+              </span>
+              <span>
+                {" "}
+                {chargesNames.length > 0 ? `(${chargesNames.length})` : ""}{" "}
+              </span>
+            </div>
+            <p className="f6 mb2">
+              {eligibilityDate === "Ineligible" &&
+              this.props.totalCharges > SHOW_ALL_CHARGES_THRESHOLD
+                ? "Excludes traffic violations, which are always ineligible"
+                : eligibilityDate === "Needs More Analysis"
+                ? "These charges need clarification before an accurate analysis can be determined"
+                : ""}
+            </p>
+            <ul className="list mb3">{listItems}</ul>
           </div>
-          <p className="f6 mb2">{
-            eligibilityDate==="Ineligible" && this.props.totalCharges > SHOW_ALL_CHARGES_THRESHOLD ? "Excludes traffic violations, which are always ineligible" :
-            eligibilityDate==="Needs More Analysis" ? "These charges need clarification before an accurate analysis can be determined" :
-            ""}</p>
-          <ul className="list mb3">
-             {listItems}
-          </ul>
-        </div>
-      )
-    }));
+        );
+      });
 
     return (
       <div className="w-100 w-two-thirds-l mb3">
-        <h3 className="bt b--light-gray pt2 mb3"><span className="fw7">Cases</span> ({this.props.totalCases})</h3>
-        <h3 className="bt b--light-gray pt2 mb3"><span className="fw7">Charges</span> ({this.props.totalCharges})</h3>
+        <h3 className="bt b--light-gray pt2 mb3">
+          <span className="fw7">Cases</span> ({this.props.totalCases})
+        </h3>
+        <h3 className="bt b--light-gray pt2 mb3">
+          <span className="fw7">Charges</span> ({this.props.totalCharges})
+        </h3>
         {summarizedCharges}
       </div>
     );
   }
 
   buildListItems(chargesNames: any[]) {
-    const listItems = chargesNames.map((([id, chargeName]: [string, string], index:number) => {
+    const listItems = chargesNames.map(
+      ([id, chargeName]: [string, string], index: number) => {
         const highlightMoneyOwed = (chargeName: string) => {
           if (chargeName.includes("$ owed")) {
             const text = chargeName.replace("$ owed", "");
             return (
-              <span>{text}<span className="red">$ owed</span></span>
+              <span>
+                {text}
+                <span className="visually-hidden">
+                  Money is owed for this case
+                </span>
+                <span
+                  aria-hidden="true"
+                  className="fw6 red bg-washed-red br2 ph2 ml2"
+                >
+                  $
+                </span>
+              </span>
             );
           } else {
             return chargeName;
           }
         };
         return (
-            <li key={"chargeItem" + index} className="bt b--light-gray pt1 mb2">
-              <a href={"#" + id} className="link hover-blue">{highlightMoneyOwed(chargeName)}</a>
-            </li>
-        )
-      }));
+          <li key={"chargeItem" + index} className="bt b--light-gray pt1 mb2">
+            <a href={"#" + id} className="link hover-blue">
+              {highlightMoneyOwed(chargeName)}
+            </a>
+          </li>
+        );
+      }
+    );
     return listItems;
   }
 
@@ -67,8 +101,8 @@ export default class ChargesList extends React.Component<Props> {
     function* zip(array: any, n: number): any {
       let i = 0;
       while (i + n <= array.length) {
-          yield array.slice(i, i + n);
-          i++;
+        yield array.slice(i, i + n);
+        i++;
       }
     }
 
