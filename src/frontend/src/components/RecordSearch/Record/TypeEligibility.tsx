c665391828d12f1b9704dc46b02d41aed8c59ec3
Ark20 <hello@reggiemikel.com>
2020-06-11 13:50:33 -0700

    removed icon

--- src/frontend/src/components/RecordSearch/Record/TypeEligibility.tsx
+++ src/frontend/src/components/RecordSearch/Record/TypeEligibility.tsx
@@ -2,58 +2,54 @@ import React from "react";
 import { TypeEligibilityData } from "./types";
 
 interface Props {
-  type_eligibility: TypeEligibilityData;
-  type_name: string;
+	type_eligibility: TypeEligibilityData;
+	type_name: string;
 }
 
 export default class RecordType extends React.Component<Props> {
-  render() {
-    const { status, reason } = this.props.type_eligibility;
+	render() {
+		const { status, reason } = this.props.type_eligibility;
 
-    const eligible = (reason: string) => (
-      <div className="relative mb3">
-        <i
-          aria-hidden="true"
-          className="absolute fas fa-check-circle green"
-        ></i>
-        <div className="ml3 pl1">
-          <span className="fw7">Type:</span> {this.props.type_name + " "}
-          <div>({reason})</div>
-        </div>
-      </div>
-    );
+		const eligible = (reason: string) => (
+			<div className="relative mb3">
+				<div className="ml3 pl1">
+					<span className="fw7">Type:</span> {this.props.type_name + " "}
+					<div>({reason})</div>
+				</div>
+			</div>
+		);
 
-    const review = (
-      <div className="relative mb3">
-        <i
-          aria-hidden="true"
-          className="absolute fas fa-question-circle purple"
-        ></i>
-        <div className="ml3 pl1">
-          <span className="fw7">Type:</span> {this.props.type_name + " "}
-          <div>({reason})</div>
-        </div>
-      </div>
-    );
+		const review = (
+			<div className="relative mb3">
+				<i
+					aria-hidden="true"
+					className="absolute fas fa-question-circle purple"
+				/>
+				<div className="ml3 pl1">
+					<span className="fw7">Type:</span> {this.props.type_name + " "}
+					<div>({reason})</div>
+				</div>
+			</div>
+		);
 
-    const ineligible = (reason: string) => (
-      <div className="relative mb3">
-        <i aria-hidden="true" className="absolute fas fa-times-circle red"></i>
-        <div className="ml3 pl1">
-          <span className="fw7">Type:</span> {this.props.type_name + " "}
-          <div>({reason})</div>
-        </div>
-      </div>
-    );
+		const ineligible = (reason: string) => (
+			<div className="relative mb3">
+				<i aria-hidden="true" className="absolute fas fa-times-circle red" />
+				<div className="ml3 pl1">
+					<span className="fw7">Type:</span> {this.props.type_name + " "}
+					<div>({reason})</div>
+				</div>
+			</div>
+		);
 
-    if (status === "Eligible") {
-      return eligible(reason);
-    } else if (status === "Needs More Analysis") {
-      return review;
-    } else if (status === "Ineligible") {
-      return ineligible(reason);
-    } else {
-      return "Unknown type eligibility";
-    }
-  }
+		if (status === "Eligible") {
+			return eligible(reason);
+		} else if (status === "Needs More Analysis") {
+			return review;
+		} else if (status === "Ineligible") {
+			return ineligible(reason);
+		} else {
+			return "Unknown type eligibility";
+		}
+	}
 }
