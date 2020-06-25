ad5085edfaed9afc0f05419487b2f3dc6d04de0b
Jordan Witte <wittejm@gmail.com>
2020-06-25 11:49:18 -0700

    Footer tweak: revert to original text size; spacing fix

--- src/frontend/src/components/Footer/index.tsx
+++ src/frontend/src/components/Footer/index.tsx
@@ -3,14 +3,14 @@ import React from "react";
 export default class Footer extends React.Component {
   public render() {
     return (
-      <footer className="mw8 center bg-white pt5 pb6 ph4 f6">
+      <footer className="mw8 center bg-white pt5 pb6 ph4">
         <a className="link hover-blue underline pr5" href="/privacy-policy">
           Privacy Policy
         </a>
-        <a className="link hover-blue underline pr5" href="/faq">
+        <a className="link hover-blue underline pr5 pl4" href="/faq">
           FAQ
         </a>
-        <a className="link hover-blue underline" href="/appendix">
+        <a className="link hover-blue underline pl4" href="/appendix">
           Appendix
         </a>
       </footer>
