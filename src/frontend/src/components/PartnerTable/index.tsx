b7b10077c9a3f9b38705856fe2d445ddcfe51d69
Ark20 <hello@reggiemikel.com>
2020-06-28 17:35:09 -0700

    mapped over details to DRY code

--- src/frontend/src/components/PartnerTable/index.tsx
+++ src/frontend/src/components/PartnerTable/index.tsx
@@ -7,6 +7,67 @@ import {
 
 class PartnerTable extends React.Component {
   render() {
+    let partnerData = [
+      {
+        details: [
+          ["Analysis Cost", "Free"],
+          ["Filing Cost", "NA"],
+          ["Income Restrictions", " Under 45k Annual"],
+          ["Locations", "Portland Metro"],
+          ["Court Fees", "Not Included"],
+        ],
+        name: "Portland Community College",
+        area: "Portland Metro",
+        instructions: "Send an email with ...",
+        contacts: ["email@domain.com", "555-555-5555"],
+      },
+      {
+        details: [
+          ["Analysis Cost", "Free"],
+          ["Filing Cost", "NA"],
+          ["Income Restrictions", " Under 45k Annual"],
+          ["Locations", "Portland Metro"],
+          ["Court Fees", "Not Included"],
+        ],
+        name: "Portland Community College",
+        area: "Portland Metro",
+        instructions: "Send an email with ...",
+        contacts: ["email@domain.com", "555-555-5555"],
+      },
+    ];
+    let partners;
+
+    partners = partnerData.map((partner, index) => (
+      <li className="bt bw2 b--lightest-blue1">
+        <Disclosure>
+          <DisclosureButton className="flex-ns w-100 relative navy hover-blue pv3 ph3 ph4-ns">
+            <span className="w-70 db fw7 pr3 mb2 mb0-ns">{partner.name}</span>
+            <span className="w-30 pr3"> {partner.area}</span>
+            <span className="absolute top-0 right-0 pt3 ph3">
+              <span aria-hidden="true" className="fas fa-angle-down"></span>
+            </span>
+          </DisclosureButton>
+          <DisclosurePanel>
+            <div className="bl bw2 f5 b--blue pb3 ph3 ml3 ml4-ns">
+              <ul className="list mb3">
+                {partner.details.map((detail, index) => (
+                  <li className="flex-ns mb3">
+                    <span className="w4-ns db fw6 mr3">{detail[0]}</span>
+                    <span>{detail[1]}</span>
+                  </li>
+                ))}
+              </ul>
+              <p className="mw6 bt b--black-10 pv3">{partner.instructions}</p>
+              <ul className="list mb3">
+                <li className="fw6 mb3">{partner.contacts[0]}</li>
+                <li className="fw6 mb2">{partner.contacts[1]}</li>
+              </ul>
+            </div>
+          </DisclosurePanel>
+        </Disclosure>
+      </li>
+    ));
+
     return (
       <React.Fragment>
         <div className="bg-navy pv6">
@@ -22,131 +83,7 @@ class PartnerTable extends React.Component {
             </div>
             <div className="ba bw3 br3 b--lightest-blue1 bg-white mb6">
               <h3 className="f3 fw9 pa4">Partners</h3>
-              <ul className="list">
-                <li className="bt bw2 b--lightest-blue1">
-                  <Disclosure>
-                    <DisclosureButton className="flex-ns w-100 relative navy hover-blue pv3 ph3 ph4-ns">
-                      <span className="w-70 db fw7 pr3 mb2 mb0-ns">
-                        Portland Community College
-                      </span>
-                      <span className="w-30 pr3">Northeast Portland</span>
-                      <span className="absolute top-0 right-0 pt3 ph3">
-                        <span
-                          aria-hidden="true"
-                          className="fas fa-angle-down"
-                        ></span>
-                      </span>
-                    </DisclosureButton>
-                    <DisclosurePanel>
-                      <div className="bl bw2 f5 b--blue pb3 ph3 ml3 ml4-ns">
-                        Here I am! I am the buried treasure!
-                      </div>
-                    </DisclosurePanel>
-                  </Disclosure>{" "}
-                </li>
-
-                <li className="bt bw2 b--lightest-blue1">
-                  <Disclosure>
-                    <DisclosureButton className="flex-ns w-100 relative navy hover-blue pv3 ph3 ph4-ns">
-                      <span className="w-70 db fw7 pr3 mb2 mb0-ns">
-                        Qiu-qiu Law
-                      </span>
-                      <span className="w-30 pr3">Portland</span>
-                      <span className="absolute top-0 right-0 pt3 ph3">
-                        <span
-                          aria-hidden="true"
-                          className="fas fa-angle-up"
-                        ></span>
-                      </span>
-                    </DisclosureButton>
-                    <DisclosurePanel>
-                      <div className="bl bw2 f5 b--blue pb3 ph3 ml3 ml4-ns">
-                        <ul className="list mb3">
-                          <li className="flex-ns mb3">
-                            <span className="w4-ns db fw6 mr3">
-                              Analysis Cost
-                            </span>
-                            <span>Free</span>
-                          </li>
-                          <li className="flex-ns mb3">
-                            <span className="w4-ns db fw6 mr3">
-                              Filing Cost
-                            </span>
-                            <span>NA</span>
-                          </li>
-                          <li className="flex-ns mb3">
-                            <span className="w4-ns db fw6 mr3">Court Fees</span>
-                            <span>Not Included</span>
-                          </li>
-                          <li className="flex-ns mb3">
-                            <span className="w4-ns db fw6 mr3">
-                              Income Restrictions
-                            </span>
-                            <span>Under 45k Annual</span>
-                          </li>
-                          <li className="flex-ns mb3">
-                            <span className="w4-ns db fw6 mr3">Locations</span>
-                            <span>Portland Metro</span>
-                          </li>
-                        </ul>
-                        <p className="mw6 bt b--black-10 pv3">
-                          Send an email with your full name, previous names
-                          (including aliases and maiden names), and date of
-                          birth.
-                        </p>
-                        <ul className="list mb3">
-                          <li className="fw6 mb3">email@domain.com</li>
-                          <li className="fw6 mb2">555-555-5555</li>
-                        </ul>
-                      </div>
-                    </DisclosurePanel>
-                  </Disclosure>
-                </li>
-
-                <li className="bt bw2 b--lightest-blue1">
-                  <Disclosure>
-                    <DisclosureButton className="flex-ns w-100 relative navy hover-blue pv3 ph3 ph4-ns">
-                      <span className="w-70 db fw7 pr3 mb2 mb0-ns">
-                        Criminals Anonymous
-                      </span>
-                      <span className="w-30 pr3">East Portland</span>
-                      <span className="absolute top-0 right-0 pt3 ph3">
-                        <span
-                          aria-hidden="true"
-                          className="fas fa-angle-down"
-                        ></span>
-                      </span>
-                    </DisclosureButton>
-                    <DisclosurePanel>
-                      <div className="bl bw2 f5 b--blue pb3 ph3 ml3 ml4-ns">
-                        Here I am! I am the buried treasure!
-                      </div>
-                    </DisclosurePanel>
-                  </Disclosure>
-                </li>
-
-                <li className="bt bw2 b--lightest-blue1">
-                  <Disclosure>
-                    <DisclosureButton className="flex-ns w-100 relative navy hover-blue pv3 ph3 ph4-ns">
-                      <span className="w-70 db fw7 pr3 mb2 mb0-ns">
-                        Probation and Parole, Community Justice Center
-                      </span>
-                      <span className="w-30 pr3">Medford</span>
-                      <span className="absolute top-0 right-0 pt3 ph3">
-                        <span
-                          aria-hidden="true"
-                          className="fas fa-angle-down"
-                        ></span>
-                      </span>
-                    </DisclosureButton>
-                    <DisclosurePanel>
-                      <div className="bl bw2 f5 b--blue pb3 ph3 ml3 ml4-ns">
-                        Here I am! I am the buried treasure!
-                      </div>
-                    </DisclosurePanel>
-                  </Disclosure>
-                </li>
-              </ul>
+              <ul className="list">{partners}</ul>
             </div>
 
             <span className="db w4 center bb bw2 b--blue mb3"></span>
