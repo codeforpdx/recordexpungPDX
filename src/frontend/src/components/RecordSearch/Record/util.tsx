7a325a2080aeb341cfdd1c39e3555e14f70ab293
Kent Shikama <kent@kentshikama.com>
2020-08-13 12:59:02 -0700

    Convert case numbers in reason strings to links

--- src/frontend/src/components/RecordSearch/Record/util.tsx
+++ src/frontend/src/components/RecordSearch/Record/util.tsx
@@ -10,9 +10,25 @@ export function newlineOrsInString(
     return (
       <div className={(index > 0 && "bt b--light-gray pt2 mt2") + ""}>
         {index === 0 ? leading_label : <span className="fw7">OR </span>}
-        {element}
+        {convertCaseNumberIntoLinks(element)}
       </div>
     );
   });
   return boldSpliced;
+}
+
+export function convertCaseNumberIntoLinks(eligibilityString: string) {
+  const elements = eligibilityString.split(/(\[.*?\])/g);
+  return elements.map((element: string, index: number) => {
+    if (element.match(/^\[.*\]$/)) {
+      const caseNumber = element.slice(1, -1);
+      return (
+        <a className="underline" href={"#" + caseNumber} key={caseNumber}>
+          {caseNumber}
+        </a>
+      );
+    } else {
+      return element;
+    }
+  });
 }
