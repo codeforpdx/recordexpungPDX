76c2cf02c384f7f1e2ba78663674ef8d49ba6ff1
Jordan Witte <wittejm@gmail.com>
2021-08-02 09:20:57 -0700

    typo

--- src/backend/expungeservice/models/charge_types/violation.py
+++ src/backend/expungeservice/models/charge_types/violation.py
@@ -8,7 +8,7 @@ from expungeservice.models.expungement_result import TypeEligibility, Eligibilit
 @dataclass(frozen=True)
 class Violation(ChargeType):
     type_name: str = "Violation"
-    expungement_rules: str = """Violation are eligible under ORS 137.225(5)(c) for convictions and 137.225(1)(b) for dismissals.
+    expungement_rules: str = """Violations are eligible under ORS 137.225(5)(c) for convictions and 137.225(1)(b) for dismissals.
 Examples include Fare Violation, Minor in Possession of Alcohol, Failure to Send or Maintain a Child in School.
 However, traffic violations are ineligible under (7)(a).
 Moreover, certain civil matters are not considered violations and are thus not eligible, namely Extradition.
