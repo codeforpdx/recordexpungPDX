d5fbdf31377bd305c01bc2c0ccd888a853dbd452
Jordan Witte <wittejm@gmail.com>
2021-11-13 16:26:34 -0800

    Updated rules string to for dismissed traffic violations

--- src/backend/expungeservice/models/charge_types/traffic_violation.py
+++ src/backend/expungeservice/models/charge_types/traffic_violation.py
@@ -9,8 +9,10 @@ from expungeservice.models.expungement_result import TypeEligibility, Eligibilit
 class TrafficViolation(ChargeType):
     type_name: str = "Traffic Violation"
     expungement_rules: str = """Convictions for traffic-related offenses are not eligible for expungement under ORS 137.225(7)(a). This includes Violations, Misdemeanors, and Felonies.
-            Dismissed traffic violations are also ineligible, because Violations generally are omitted from the statute.
-            However, dismissed traffic-related Misdemeanors and Felonies, like all dismissed Misdemeanors and Felonies, ARE eligible for expungement."""
+The eligibility of a dismissed traffic violation is subject to some debate. 137.225(1)(d) says that "At any time after an acquittal or a dismissal...an arrested, cited or charged person may apply to the court in the county in which the person was arrested, cited or charged, for entry of an order setting aside the record of the arrest, citation or charge." A driving offense probably qualifies as a citation under any definition of the word.
+However, DAs will probably object to filing for these, at least initially, based off their understanding of the previous version of the law, and their relationships with the other parts of the State, and the practical administrative burdens this would place on those systems.
+Most directly, it’s not clear that the local municipal systems have a good way of expunging traffic tickets, dismissed or not.
+As such, petitioners filing to expunge dismissed traffic tickets may receive pushback and have their petition rejected, and do so at their own risk."""
     blocks_other_charges: bool = False
     severity_level: str = "Violation"
 
