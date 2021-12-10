3dfc199ad31c9a86b59c023f9047f327a6a19e2b
michaelzhang43 <59623009+michaelzhang43@users.noreply.github.com>
2021-12-09 22:32:46 -0500

    Updated Misdemeanor A per SB397

--- src/backend/expungeservice/models/charge_types/misdemeanor_class_a.py
+++ src/backend/expungeservice/models/charge_types/misdemeanor_class_a.py
@@ -10,16 +10,16 @@ class MisdemeanorClassA(ChargeType):
     type_name: str = "Misdemeanor Class A"
     expungement_rules: str = """Convictions for misdemeanors are generally eligible under ORS 137.225(5)(b).
 Exceptions include convictions related to sex, child and elder abuse, and driving, including DUII.
-Dismissals for misdemeanors are generally eligible under ORS 137.225(1)(b). Exceptions include cases dismissed due to successful completion of DUII diversion."""
+Dismissals for misdemeanors are generally eligible under ORS 137.225(1)(d). Exceptions include cases dismissed due to successful completion of DUII diversion."""
     severity_level: str = "Misdemeanor Class A"
 
     def type_eligibility(self, disposition):
         if ChargeUtil.dismissed(disposition):
             raise ValueError("Dismissed criminal charges should have been caught by another class.")
         elif ChargeUtil.convicted(disposition):
-            return TypeEligibility(EligibilityStatus.ELIGIBLE, reason="Eligible under 137.225(5)(b)")
+            return TypeEligibility(EligibilityStatus.ELIGIBLE, reason="Eligible under 137.225(5)(d)")
         else:
             return TypeEligibility(
                 EligibilityStatus.ELIGIBLE,
-                reason="Always eligible under 137.225(5)(b) for convictions, or 137.225(1)(b) for dismissals",
+                reason="Always eligible under 137.225(5)(b) for convictions, or 137.225(1)(d) for dismissals",
             )
