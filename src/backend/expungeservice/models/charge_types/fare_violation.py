e912afbf7c18aae4deae33d06f4d7bc2da717429
michaelzhang43 <59623009+michaelzhang43@users.noreply.github.com>
2021-12-09 22:26:33 -0500

    Updated for SB397

--- src/backend/expungeservice/models/charge_types/fare_violation.py
+++ src/backend/expungeservice/models/charge_types/fare_violation.py
@@ -10,13 +10,13 @@ class FareViolation(ChargeType):
     type_name: str = "FareViolation"
     expungement_rules: str = """A Fare Violation follows normal eligibility rules of a Violation, even though it typically falls under a statute number that would identify it as a Civil Offense.
 Violation convictions are eligible under ORS 137.225(5)(c).
-Dismissed violations are eligible under 137.225(1)(b)."""
+Dismissed violations are eligible under 137.225(1)(d)."""
     severity_level: str = "Violation"
 
     def type_eligibility(self, disposition):
         if ChargeUtil.dismissed(disposition):
             return TypeEligibility(
-                EligibilityStatus.ELIGIBLE, reason="Eligible under 137.225(1)(b)"
+                EligibilityStatus.ELIGIBLE, reason="Eligible under 137.225(1)(d)"
             )
         elif ChargeUtil.convicted(disposition):
             return TypeEligibility(EligibilityStatus.ELIGIBLE, reason="Eligible under 137.225(5)(c)")
