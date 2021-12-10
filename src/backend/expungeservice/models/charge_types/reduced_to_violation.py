416109bb65b9e750dce4b07d91b0255ad0ff712e
michaelzhang43 <59623009+michaelzhang43@users.noreply.github.com>
2021-12-09 22:34:22 -0500

    Updated per 397

--- src/backend/expungeservice/models/charge_types/reduced_to_violation.py
+++ src/backend/expungeservice/models/charge_types/reduced_to_violation.py
@@ -8,19 +8,23 @@ from expungeservice.models.expungement_result import TypeEligibility, Eligibilit
 @dataclass(frozen=True)
 class ReducedToViolation(ChargeType):
     type_name: str = "Reduced to Violation"
-    expungement_rules: str = """A criminal charge may sometimes be reduced to a violation, most often in community courts. Violations are eligible under 137.225(5)(d) if convicted, but are typically ineligible if dismissed, by omission. However a reduced charge that is dismissed will usually be accepted as eligible in most courts, as it is still considered a criminal charge and is thus eligible under 137.225(1)(b).
+    expungement_rules: str = """A criminal charge may sometimes be reduced to a violation, 
+    most often in community courts. Violations are eligible under 137.225(5)(d) if convicted, 
+    but are typically ineligible if dismissed, by omission.
+    However a reduced charge that is dismissed will usually be accepted as eligible in most courts, 
+    as it is still considered a criminal charge and is thus eligible under 137.225(1)(d).
     A charge is recognizable as reduced to violation if its level is Violation, and it contains "reduced" in the charge name."""
     severity_level: str = "Violation"
 
     def type_eligibility(self, disposition):
         if ChargeUtil.dismissed(disposition):
             return TypeEligibility(
-                EligibilityStatus.ELIGIBLE, reason="Dismissed criminal charge eligible under 137.225(1)(b)"
+                EligibilityStatus.ELIGIBLE, reason="Dismissed criminal charge eligible under 137.225(1)(d)"
             )
         elif ChargeUtil.convicted(disposition):
             return TypeEligibility(EligibilityStatus.ELIGIBLE, reason="Eligible under 137.225(5)(d)")
         else:
             return TypeEligibility(
                 EligibilityStatus.ELIGIBLE,
-                reason="Reduced Violations are always eligible under 137.225(5)(d) for convictions, or 137.225(1)(b) for dismissals",
+                reason="Reduced Violations are always eligible under 137.225(5)(d) for convictions, or 137.225(1)(d) for dismissals",
             )
