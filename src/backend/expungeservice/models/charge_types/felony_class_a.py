d13d2cd217c908c2283377dec0a513586bd03ef4
michaelzhang43 <59623009+michaelzhang43@users.noreply.github.com>
2021-12-09 22:27:43 -0500

    Updated Felony A to conform to SB397

--- src/backend/expungeservice/models/charge_types/felony_class_a.py
+++ src/backend/expungeservice/models/charge_types/felony_class_a.py
@@ -10,7 +10,7 @@ class FelonyClassA(ChargeType):
     type_name: str = "Felony Class A"
     expungement_rules: str = (
         """Class A felony convictions generally are omitted from the statute, and are ineligible, unless that Class A Felony was related to the Manufacturing/Delivery/Possession of Marijuana.
-Class A felony dismissals are always eligible under 137.225(5)(a).
+Class A felony dismissals are always eligible under 137.225(1)(d).
 """
     )
     severity_level: str = "Felony Class A"
