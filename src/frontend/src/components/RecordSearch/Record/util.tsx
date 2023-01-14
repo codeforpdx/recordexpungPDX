77409aabf37ef4ed63c23b3ec7c72d2ab1bafaa1
monocle <monocle.github@gmail.com>
2023-01-14 15:45:43 -0800

    Fix React key errors
    for:
    
    * ChargestList
    
    * util.newlineOrsInString()

--- src/frontend/src/components/RecordSearch/Record/util.tsx
+++ src/frontend/src/components/RecordSearch/Record/util.tsx
@@ -8,7 +8,10 @@ export function newlineOrsInString(
 
   let boldSpliced = splittedElements.map((element: string, index: number) => {
     return (
-      <div className={(index > 0 && "bt b--light-gray pt2 mt2") + ""}>
+      <div
+        key={index}
+        className={(index > 0 && "bt b--light-gray pt2 mt2") + ""}
+      >
         {index === 0 ? leading_label : <span className="fw7">OR </span>}
         {convertCaseNumberIntoLinks(element)}
       </div>
