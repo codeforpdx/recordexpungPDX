c35c11ab86a9397aebc1c52ffa7c5fceafe39de9
Treasure Porth <treasure@scrimba.com>
2023-01-22 10:38:29 -0800

    delete unecessary stuff from logout endpoint

--- src/backend/expungeservice/endpoints/oeci_logout.py
+++ src/backend/expungeservice/endpoints/oeci_logout.py
@@ -1,26 +1,11 @@
-import requests
 from flask.views import MethodView
-from flask import request, make_response, current_app
-import time
-import os
-
-from expungeservice.crawler.crawler import Crawler, InvalidOECIUsernamePassword, OECIUnavailable
-from expungeservice.request import check_data_fields
-from expungeservice.request import error
-from expungeservice.crypto import DataCipher
-
+from flask import make_response
 
 class OeciLogout(MethodView):
     def post(self):
-        """
-        Attempts to log in to the OECI web site using the provided username
-        and password if successful, encrypt those credentials and return them
-        in a cookie. If the credentials
-        """
-        response = make_response()
-        # TODO: We will need an OECILogout endpoint to remove httponly=true cookies from frontend
-        response.delete_cookie("oeci_token")
-        return response
+      response = make_response()
+      response.delete_cookie("oeci_token")
+      return response
 
 
 def register(app):
