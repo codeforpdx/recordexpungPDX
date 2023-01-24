f3c142b73fc5e234f018d41d595b40d08f666886
monocle <monocle.github@gmail.com>
2023-01-24 15:16:15 -0800

    Wrap dispatch in useEffect in Demo
    Add a blank record for testing

--- src/frontend/src/components/Demo/index.tsx
+++ src/frontend/src/components/Demo/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { startDemo } from "../../redux/search/actions";
 import useSetupPage from "../../hooks/useSetupPage";
@@ -8,13 +8,21 @@ import Status from "../RecordSearch/Status";
 import Record from "../RecordSearch/Record";
 import Assumptions from "../RecordSearch/Assumptions";
 // import johnCommonRecord from "../../data/demo/johnCommon";
+// import multipleCharges from "../../data/demo/multipleCharges";
+// import blankRecord from "../../data/blankRecord";
 
 export default function Demo() {
+  const dispatch = useAppDispatch();
   let record = useAppSelector((state) => state.search.record);
+  // record = multipleCharges;
   // record = johnCommonRecord;
+  // record = blankRecord;
 
   useSetupPage("Demo");
-  useAppDispatch()(startDemo());
+
+  useEffect(() => {
+    dispatch(startDemo());
+  });
 
   return (
     <main className="mw8 center f6 f5-l ph2">
