6729ac44aa7846638342f3c3e69d534c541487f3
monocle <monocle.github@gmail.com>
2023-02-04 16:54:47 -0800

    Update Demo tests

--- src/frontend/src/components/Demo/Demo.test.tsx
+++ src/frontend/src/components/Demo/Demo.test.tsx
@@ -1,72 +1,37 @@
 import React from "react";
-import { MemoryRouter } from "react-router-dom";
-import renderer from "react-test-renderer";
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { Provider } from "react-redux";
-import store from "../../redux/store";
+import { appRender } from "../../test/testHelpers";
 import Demo from ".";
 
-const mockUseAppSelector = jest.fn();
+beforeEach(() => {
+  jest.useFakeTimers().setSystemTime(new Date(2023, 0, 11));
+});
 
-jest.mock("../../redux/hooks", () => ({
-  ...jest.requireActual("../../redux/hooks"),
-  useAppSelector: () => mockUseAppSelector(),
-}));
-
-function doRender() {
-  render(
-    <Provider store={store}>
-      <MemoryRouter>
-        <Demo />
-      </MemoryRouter>
-    </Provider>
-  );
-}
-
-it("renders correctly without a record", () => {
-  const RealDate = Date;
-  const mockDate = new Date(2023, 0, 11);
-  // @ts-ignore
-  const spy = jest.spyOn(global, "Date").mockImplementation(() => mockDate);
-  // @ts-ignore
-  spy.now = RealDate.now;
-
-  const tree = renderer
-    .create(
-      <Provider store={store}>
-        <MemoryRouter>
-          <Demo />
-        </MemoryRouter>
-      </Provider>
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-
-  global.Date = RealDate;
+afterEach(() => {
+  jest.useRealTimers();
 });
 
 describe("Without a record", () => {
-  it("calls useAppSelector", () => {
-    doRender();
-    expect(mockUseAppSelector).toHaveBeenCalled();
-  });
+  it("renders correctly without a record", () => {
+    const { container } = appRender(<Demo />);
 
-  it("dispatches the stop demo action", () => {
-    const dispatchSpy = jest.spyOn(store, "dispatch");
-
-    doRender();
-    expect(dispatchSpy).toHaveBeenCalledWith({
-      type: "START_DEMO",
-    });
-  });
-
-  it("displays the correct document title", () => {
-    doRender();
+    expect(container).toMatchSnapshot();
     expect(global.window.document.title).toBe("Demo - RecordSponge");
+    expect(screen.queryByText(/search summary/i)).not.toBeInTheDocument();
   });
 
-  it("does not display search summary", () => {
-    expect(screen.queryByText(/search summary/i)).not.toBeInTheDocument();
+  it("turns on the demo state", () => {
+    const { store } = appRender(<Demo />);
+    expect(store.getState().search.demo).toBe(true);
+  });
+});
+
+describe("With the multiple charges record", () => {
+  it("correctly renders", () => {
+    const { container } = appRender(<Demo />, "multiple");
+
+    expect(container).toMatchSnapshot();
+    expect(screen.queryByText(/search summary/i)).toBeInTheDocument();
   });
 });
--- src/frontend/src/components/Demo/index.tsx
+++ src/frontend/src/components/Demo/index.tsx
@@ -7,16 +7,10 @@ import SearchPanel from "../RecordSearch/SearchPanel";
 import Status from "../RecordSearch/Status";
 import Record from "../RecordSearch/Record";
 import Assumptions from "../RecordSearch/Assumptions";
-// import johnCommonRecord from "../../data/demo/johnCommon";
-// import multipleCharges from "../../data/demo/multipleCharges";
-// import blankRecord from "../../data/blankRecord";
 
 export default function Demo() {
   const dispatch = useAppDispatch();
   let record = useAppSelector((state) => state.search.record);
-  // record = multipleCharges;
-  // record = johnCommonRecord;
-  // record = blankRecord;
 
   useSetupPage("Demo");
 
