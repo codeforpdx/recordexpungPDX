92666e74a3de2349f57da27f0b3eb69a9ff2459d
monocle <monocle.github@gmail.com>
2023-02-10 05:38:16 -0800

    Refactor SearchPanel

--- src/frontend/src/components/RecordSearch/SearchPanel/index.tsx
+++ src/frontend/src/components/RecordSearch/SearchPanel/index.tsx
@@ -1,234 +1,121 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
-import { connect } from "react-redux";
-import { AppState } from "../../../redux/store";
 import { AliasData, AliasFieldNames } from "./types";
-import Alias from "./Alias";
-import InvalidInputs from "../../InvalidInputs";
+import { useAppSelector, useAppDispatch } from "../../../redux/hooks";
+import {
+  selectSearchFormAliases,
+  selectSearchFormDate,
+  setSearchFormDate,
+  updateSearchFormAlias,
+  addSearchFormAlias,
+  removeSearchFormAlias,
+} from "../../../redux/searchFormSlice";
+import {
+  isValidOptionalWildcard,
+  areAnyBlank,
+  isValidOptionalDate,
+} from "./validators";
 import { searchRecord } from "../../../redux/search/actions";
-import { isValidWildcard } from "./validators";
+import Alias from "./Alias";
 import Field from "./Field";
+import InvalidInputs from "../../InvalidInputs";
 
-interface Props {
-  searchRecord: Function;
-  aliases: AliasData[];
-  today: string;
-}
+export default function SearchPanel() {
+  const dispatch = useAppDispatch();
+  const aliases = useAppSelector(selectSearchFormAliases);
+  const date = useAppSelector(selectSearchFormDate);
+  const [errorMessages, setErorrMessages] = useState<string[]>([]);
 
-interface State {
-  aliases: AliasData[];
-  today: string;
-  missingInputs: boolean;
-  invalidDate: boolean;
-  invalidFirstNameWildcard: boolean;
-  invalidLastNameWildcard: boolean;
-}
+  if (date === "") dispatch(setSearchFormDate(moment().format("M/D/YYYY")));
 
-class SearchPanel extends React.Component<Props, State> {
-  state: State = {
-    aliases: this.props.aliases,
-    today: this.props.today || todayToMMDDYYYY(),
-    missingInputs: false,
-    invalidDate: false,
-    invalidFirstNameWildcard: false,
-    invalidLastNameWildcard: false,
+  // A map of error messages and validators that return true if messages should be shown
+  const validators = {
+    "First and last name are required.": (alias: AliasData) =>
+      areAnyBlank(alias.first_name, alias.last_name),
+
+    "The date format must be MM/DD/YYYY.": (alias: AliasData) =>
+      !isValidOptionalDate(alias.birth_date),
+
+    "A wildcard in First Name field must be at the end and follow at least one letter.":
+      (alias: AliasData) => !isValidOptionalWildcard(alias.first_name, 2),
+
+    "A wildcard in the Last Name field must be at the end and follow at least two letters.":
+      (alias: AliasData) => !isValidOptionalWildcard(alias.last_name, 3),
   };
 
-  handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    this.validateForm().then(() => {
-      if (
-        !this.state.missingInputs &&
-        !this.state.invalidDate &&
-        !this.state.invalidFirstNameWildcard &&
-        !this.state.invalidLastNameWildcard
-      ) {
-        this.props.searchRecord(this.state.aliases, this.state.today);
-      }
-    });
-  };
 
-  validateForm = () => {
-    return new Promise((resolve: (value?: unknown) => void) => {
-      let missingInputs: boolean = false;
-      let invalidFirstNameWildcard: boolean = false;
-      let invalidLastNameWildcard: boolean = false;
-      for (let i: number = 0; i < this.state.aliases.length; i++) {
-        if (
-          this.state.aliases[i].first_name.trim().length === 0 ||
-          this.state.aliases[i].last_name.trim().length === 0
-        ) {
-          missingInputs = true;
-        }
-        if (
-          this.state.aliases[i].first_name.indexOf("*") > -1 &&
-          !isValidWildcard(this.state.aliases[i].first_name, 2)
-        ) {
-          invalidFirstNameWildcard = true;
-        }
-        if (
-          this.state.aliases[i].last_name.indexOf("*") > -1 &&
-          !isValidWildcard(this.state.aliases[i].last_name, 3)
-        ) {
-          invalidLastNameWildcard = true;
-        }
-      }
-      this.setState(
-        {
-          missingInputs,
-          invalidFirstNameWildcard,
-          invalidLastNameWildcard,
-          invalidDate:
-            moment(
-              this.state.aliases[0].birth_date,
-              "M/D/YYYY",
-              true
-            ).isValid() === false &&
-            this.state.aliases[0].birth_date.length !== 0,
-        },
-        resolve
-      );
-    });
-  };
-
-  handleAliasContentChange = (
-    ind: number,
-    fieldName: AliasFieldNames,
-    fieldValue: string
-  ) => {
-    let updatedAliases: AliasData[] = JSON.parse(
-      JSON.stringify(this.state.aliases)
+    let messages = Object.entries(validators).reduce(
+      (errors, [message, isInvalid]) => {
+        if (aliases.some(isInvalid)) errors.push(message);
+        return errors;
+      },
+      [] as string[]
     );
-    updatedAliases[ind][fieldName] = fieldValue;
-    this.setState({
-      aliases: updatedAliases,
-    });
+
+    setErorrMessages(messages);
+
+    if (messages.length === 0) dispatch(searchRecord());
   };
 
-  addAlias = () => {
-    const lastAlias: AliasData =
-      this.state.aliases[this.state.aliases.length - 1];
-    let updatedAliases = JSON.parse(JSON.stringify(this.state.aliases));
-    updatedAliases.push({
-      first_name: lastAlias.first_name,
-      middle_name: lastAlias.middle_name,
-      last_name: lastAlias.last_name,
-      birth_date: lastAlias.birth_date,
-    });
-
-    this.setState({
-      aliases: updatedAliases,
-    });
-  };
-
-  handleAliasRemoveClick = (aliasIndex: number) => {
-    let updatedAliases: AliasData[] = JSON.parse(
-      JSON.stringify(this.state.aliases)
-    );
-    updatedAliases.splice(aliasIndex, 1);
-    this.setState({
-      aliases: updatedAliases,
-    });
-  };
-
-  public render() {
-    const aliasComponents = this.state.aliases.map((alias: AliasData, i) => {
-      const separator =
-        i > 0 ? <hr className="bb b--black-05 mt2 mt3-ns mb3 mb4-ns" /> : null;
-      return (
-        <React.Fragment key={i}>
-          {separator}
-          <Alias
-            ind={i}
-            aliasData={alias}
-            onChange={(fieldName: AliasFieldNames, fieldValue: string) => {
-              this.handleAliasContentChange(i, fieldName, fieldValue);
-            }}
-            onRemoveClick={() => {
-              this.handleAliasRemoveClick(i);
-            }}
-            hideRemoveButton={this.state.aliases.length === 1}
-          />
-        </React.Fragment>
-      );
-    });
-    return (
-      <form className="mw7 center" onSubmit={this.handleSubmit} noValidate>
-        <div className="flex">
-          <Field
-            coda="mm/dd/yyyy"
-            name="today"
-            label="Expunge Date"
-            content={this.state.today}
-            divMarkup="pl2-r"
-            onChange={(fieldValue: string) => {
-              this.setState({ today: fieldValue });
-            }}
-            required={true}
-            errorMessage="today_msg"
-          />
-        </div>
-        {aliasComponents}
-        <div className="flex">
-          <button
-            className="w4 tc br2 bg-gray-blue-2 link hover-dark-blue mid-gray fw5 pv3 ph3 mr2"
-            onClick={this.addAlias}
-            type="button"
-          >
-            <i aria-hidden={"true"} className="fas fa-plus-circle pr1"></i>
-            Alias
-          </button>
-          <button
-            className="br2 bg-blue white bg-animate hover-bg-dark-blue db w-100 tc pv3 btn--search"
-            type="submit"
-          >
-            <i aria-hidden="true" className="fas fa-search pr2"></i>
-            <span className="fw7">Search</span>
-          </button>
-        </div>
-        <InvalidInputs
-          conditions={[
-            this.state.missingInputs,
-            this.state.invalidDate,
-            this.state.invalidFirstNameWildcard,
-            this.state.invalidLastNameWildcard,
-          ]}
-          contents={[
-            <span>First and last name are required.</span>,
-            <span>The date format must be MM/DD/YYYY.</span>,
-            <span>
-              A wildcard in First Name field must be at the end and follow at
-              least one letter.
-            </span>,
-            <span>
-              A wildcard in the Last Name field must be at the end and follow at
-              least two letters.
-            </span>,
-          ]}
-        />
-      </form>
-    );
-  }
-}
-
-const todayToMMDDYYYY = () => {
-  const date = new Date();
   return (
-    (date.getMonth() + 1).toString() +
-    "/" +
-    date.getDate().toString() +
-    "/" +
-    date.getFullYear().toString()
+    <form className="mw7 center" onSubmit={handleSubmit} noValidate>
+      <div className="flex">
+        <Field
+          coda="mm/dd/yyyy"
+          name="today"
+          label="Expunge Date"
+          content={date}
+          divMarkup="pl2-r"
+          onChange={(newDate: string) => {
+            dispatch(setSearchFormDate(newDate));
+          }}
+          required={true}
+          errorMessage="today_msg"
+        />
+      </div>
+
+      {aliases.map((alias: AliasData, index: number) => {
+        return (
+          <React.Fragment key={index}>
+            {index > 0 && (
+              <hr className="bb b--black-05 mt2 mt3-ns mb3 mb4-ns" />
+            )}
+            <Alias
+              ind={index}
+              aliasData={alias}
+              onChange={(attribute: AliasFieldNames, value: string) => {
+                dispatch(updateSearchFormAlias({ index, attribute, value }));
+              }}
+              onRemoveClick={() => {
+                dispatch(removeSearchFormAlias(index));
+              }}
+              hideRemoveButton={aliases.length === 1}
+            />
+          </React.Fragment>
+        );
+      })}
+
+      <div className="flex">
+        <button
+          className="w4 tc br2 bg-gray-blue-2 link hover-dark-blue mid-gray fw5 pv3 ph3 mr2"
+          onClick={() => dispatch(addSearchFormAlias())}
+          type="button"
+        >
+          <i aria-hidden="true" className="fas fa-plus-circle pr1"></i>
+          Alias
+        </button>
+        <button
+          className="br2 bg-blue white bg-animate hover-bg-dark-blue db w-100 tc pv3 btn--search"
+          type="submit"
+        >
+          <i aria-hidden="true" className="fas fa-search pr2"></i>
+          <span className="fw7">Search</span>
+        </button>
+      </div>
+
+      <InvalidInputs contents={errorMessages} />
+    </form>
   );
-};
-
-const mapStateToProps = (state: AppState) => {
-  return {
-    aliases: JSON.parse(JSON.stringify(state.search.aliases)),
-    today: JSON.parse(JSON.stringify(state.search.today)),
-  };
-};
-
-export default connect(mapStateToProps, {
-  searchRecord: searchRecord,
-})(SearchPanel);
+}
--- src/frontend/src/components/RecordSearch/SearchPanel/validators.ts
+++ src/frontend/src/components/RecordSearch/SearchPanel/validators.ts
@@ -1,12 +1,25 @@
-export function isValidWildcard(
-  searchTerm: string,
-  requiredLength: number
-): boolean {
-  if (searchTerm.length < requiredLength) {
-    return false;
-  } else if (searchTerm.indexOf("*") < searchTerm.length - 1) {
-    return false;
-  } else {
-    return true;
-  }
+import moment from "moment";
+
+export function isValidWildcard(str: string, minLength: number): boolean {
+  return str.endsWith("*") && str.length >= minLength;
+}
+
+export function isValidOptionalWildcard(str: string, minLength: number) {
+  return !str.includes("*") || isValidWildcard(str, minLength);
+}
+
+export function isBlank(str: string) {
+  return str.trim().length === 0;
+}
+
+export function areAnyBlank(...args: string[]) {
+  return args.some(isBlank);
+}
+
+export function isValidDate(str: string, format = "M/D/YYYY") {
+  return moment(str, format, true).isValid();
+}
+
+export function isValidOptionalDate(str: string, format = "M/D/YYYY") {
+  return isBlank(str) || moment(str, format, true).isValid();
 }
