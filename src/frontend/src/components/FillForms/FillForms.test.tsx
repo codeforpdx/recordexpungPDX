4e8e1ce398e6975c0b5ee11ea50d6aafdfedae2c
Matth5050 <matthew.f.herbert@gmail.com>
2023-02-21 20:54:55 -0800

    refactor existing tests and added test for modal render

--- src/frontend/src/components/FillForms/FillForms.test.tsx
+++ src/frontend/src/components/FillForms/FillForms.test.tsx
@@ -6,6 +6,8 @@ import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import store from "../../redux/store";
 import FillForms from ".";
+import { appRender, fillExpungementPacketForm } from "../../test/testHelpers";
+import EmptyFieldsModal from "./EmptyFieldsModal";
 
 const mockDownloadExpungementPacketFunc = jest.fn();
 
@@ -14,55 +16,92 @@ jest.mock("../../redux/search/actions", () => ({
   downloadExpungementPacket: () => mockDownloadExpungementPacketFunc,
 }));
 
-it("renders correctly", () => {
-  const tree = renderer
-    .create(
+describe("masterRender", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <MemoryRouter>
+            <FillForms />
+          </MemoryRouter>
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("renderCorrect", () => {
+  it("renders correctly", async () => {
+    const { asFragment } = appRender(<FillForms />);
+    const user = userEvent.setup();
+    const downloadButton = screen.getByRole("button", {
+      name: /download expungement packet/i,
+    });
+    await user.click(downloadButton);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe("beforeEach", () => {
+  beforeEach(() => {
+    render(
       <Provider store={store}>
         <MemoryRouter>
           <FillForms />
         </MemoryRouter>
       </Provider>
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
-beforeEach(() => {
-  render(
-    <Provider store={store}>
-      <MemoryRouter>
-        <FillForms />
-      </MemoryRouter>
-    </Provider>
-  );
-});
-
-it("will not submit a blank form", async () => {
-  const user = userEvent.setup();
-  const downloadButton = screen.getByRole("button", {
-    name: /download expungement packet/i,
+    );
   });
 
-  await user.click(downloadButton);
+  it("will not submit a blank form", async () => {
+    const user = userEvent.setup();
+    const downloadButton = screen.getByRole("button", {
+      name: /download expungement packet/i,
+    });
 
-  expect(mockDownloadExpungementPacketFunc).not.toHaveBeenCalled();
+    await user.click(downloadButton);
+
+    expect(mockDownloadExpungementPacketFunc).not.toHaveBeenCalled();
+  });
+
+  test("with a valid zip code, dob, and phone number the form can be submitted", async () => {
+    const user = userEvent.setup();
+    const downloadButton = screen.getByRole("button", {
+      name: /download expungement packet/i,
+    });
+    await fillExpungementPacketForm(user);
+    await user.click(downloadButton);
+    expect(mockDownloadExpungementPacketFunc).toHaveBeenCalled();
+  });
 });
 
-test("with a valid zip code, dob, and phone number the form can be submitted", async () => {
-  const user = userEvent.setup();
-  const downloadButton = screen.getByRole("button", {
-    name: /download expungement packet/i,
+//tests if modal renders
+describe("modalRenders", () => {
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <FillForms />
+        </MemoryRouter>
+      </Provider>
+    );
   });
-  const zipCodeField = screen.getByLabelText(/zip code/i);
-  const dobCodeField = screen.getByLabelText(/date of birth/i);
-  const phoneCodeField = screen.getByLabelText(/phone number/i);
 
-  await user.click(dobCodeField);
-  await user.keyboard("11/13/1995");
-  await user.click(phoneCodeField);
-  await user.keyboard("1112223333");
-  await user.click(zipCodeField);
-  await user.keyboard("12345");
-  await user.click(downloadButton);
-  expect(mockDownloadExpungementPacketFunc).toHaveBeenCalled();
+  it("modal renders correctly", async () => {
+    const modal = render(
+      <EmptyFieldsModal
+        close={true}
+        onClose={() => void {}}
+        onDownload={() => void {}}
+      />
+    );
+
+    const user = userEvent.setup();
+    const downloadButton = screen.getByRole("button", {
+      name: /download expungement packet/i,
+    });
+    await user.click(downloadButton);
+    expect(modal).not.toBeNull();
+  });
 });
