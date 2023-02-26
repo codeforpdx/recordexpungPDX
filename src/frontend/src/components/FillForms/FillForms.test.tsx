720e4b289e8ad7ef9b2359711fc935c73622a571
Matth5050 <matthew.f.herbert@gmail.com>
2023-02-25 19:49:38 -0800

    added new test and removed deprecated tests

--- src/frontend/src/components/FillForms/FillForms.test.tsx
+++ src/frontend/src/components/FillForms/FillForms.test.tsx
@@ -7,7 +7,6 @@ import { Provider } from "react-redux";
 import store from "../../redux/store";
 import FillForms from ".";
 import { appRender, fillExpungementPacketForm } from "../../test/testHelpers";
-import EmptyFieldsModal from "./EmptyFieldsModal";
 
 const mockDownloadExpungementPacketFunc = jest.fn();
 
@@ -65,19 +64,23 @@ describe("beforeEach", () => {
     expect(mockDownloadExpungementPacketFunc).not.toHaveBeenCalled();
   });
 
-  test("with a valid zip code, dob, and phone number the form can be submitted", async () => {
+  it("packet will download when all fields are properly filled", async () => {
     const user = userEvent.setup();
+
+    fillExpungementPacketForm(user);
+
     const downloadButton = screen.getByRole("button", {
       name: /download expungement packet/i,
     });
-    await fillExpungementPacketForm(user);
+
     await user.click(downloadButton);
-    expect(mockDownloadExpungementPacketFunc).toHaveBeenCalled();
+
+    expect(mockDownloadExpungementPacketFunc).not.toHaveBeenCalled();
   });
 });
 
-//tests if modal renders
-describe("modalRenders", () => {
+//modal will still render if certain fields are filled in or not
+describe("modal with certain fields filled out", () => {
   beforeEach(() => {
     render(
       <Provider store={store}>
@@ -88,20 +91,189 @@ describe("modalRenders", () => {
     );
   });
 
-  it("modal renders correctly", async () => {
-    const modal = render(
-      <EmptyFieldsModal
-        close={true}
-        onClose={() => void {}}
-        onDownload={() => void {}}
-      />
-    );
-
+  it("modal renders correctly when only name is filled out", async () => {
     const user = userEvent.setup();
+
+    await user.click(screen.getByLabelText(/full name/i));
+    await user.keyboard("username");
+
     const downloadButton = screen.getByRole("button", {
       name: /download expungement packet/i,
     });
+
     await user.click(downloadButton);
-    expect(modal).not.toBeNull();
+    expect(screen.getByText(/incomplete/i)).toBeInTheDocument;
+  });
+
+  it("modal renders correctly when only name and DOB are filled out", async () => {
+    const user = userEvent.setup();
+
+    await user.click(screen.getByLabelText(/full name/i));
+    await user.keyboard("username");
+    await user.click(screen.getByLabelText(/birth/i));
+    await user.keyboard("12/12/1999");
+
+    const downloadButton = screen.getByRole("button", {
+      name: /download expungement packet/i,
+    });
+
+    await user.click(downloadButton);
+    expect(screen.getByText(/incomplete/i)).toBeInTheDocument;
+  });
+
+  it("modal renders correctly when only name, DOB, and ZipCode are filled out", async () => {
+    const user = userEvent.setup();
+
+    await user.click(screen.getByLabelText(/full name/i));
+    await user.keyboard("username");
+    await user.click(screen.getByLabelText(/birth/i));
+    await user.keyboard("12/12/1999");
+    await user.click(screen.getByLabelText(/zip code/i));
+    await user.keyboard("12345");
+
+    const downloadButton = screen.getByRole("button", {
+      name: /download expungement packet/i,
+    });
+
+    await user.click(downloadButton);
+    expect(screen.getByText(/incomplete/i)).toBeInTheDocument;
+  });
+
+  it("modal renders correctly when only name, DOB, ZipCode, and PhoneNumber are filled out", async () => {
+    const user = userEvent.setup();
+
+    await user.click(screen.getByLabelText(/full name/i));
+    await user.keyboard("username");
+    await user.click(screen.getByLabelText(/birth/i));
+    await user.keyboard("12/12/1999");
+    await user.click(screen.getByLabelText(/zip code/i));
+    await user.keyboard("12345");
+    await user.click(screen.getByLabelText(/phone/i));
+    await user.keyboard("123-456-7890");
+
+    const downloadButton = screen.getByRole("button", {
+      name: /download expungement packet/i,
+    });
+
+    await user.click(downloadButton);
+    expect(screen.getByText(/incomplete/i)).toBeInTheDocument;
+  });
+
+  it("modal renders correctly when only name, DOB, ZipCode, PhoneNumber, and MailingAddress are filled out", async () => {
+    const user = userEvent.setup();
+
+    await user.click(screen.getByLabelText(/full name/i));
+    await user.keyboard("username");
+    await user.click(screen.getByLabelText(/birth/i));
+    await user.keyboard("12/12/1999");
+    await user.click(screen.getByLabelText(/zip code/i));
+    await user.keyboard("12345");
+    await user.click(screen.getByLabelText(/phone/i));
+    await user.keyboard("123-456-7890");
+    await user.click(screen.getByLabelText(/address/i));
+    await user.keyboard("1111 NE anywhere");
+
+    const downloadButton = screen.getByRole("button", {
+      name: /download expungement packet/i,
+    });
+
+    await user.click(downloadButton);
+    expect(screen.getByText(/incomplete/i)).toBeInTheDocument;
+  });
+
+  it("modal does NOT render when all fields are filled out", async () => {
+    const user = userEvent.setup();
+
+    await user.click(screen.getByLabelText(/full name/i));
+    await user.keyboard("username");
+    await user.click(screen.getByLabelText(/birth/i));
+    await user.keyboard("12/12/1999");
+    await user.click(screen.getByLabelText(/zip code/i));
+    await user.keyboard("12345");
+    await user.click(screen.getByLabelText(/phone/i));
+    await user.keyboard("123-456-7890");
+    await user.click(screen.getByLabelText(/address/i));
+    await user.keyboard("1111 NE anywhere");
+    await user.click(screen.getByLabelText(/city/i));
+    await user.keyboard("Portland");
+    await user.selectOptions(
+      screen.getByRole("combobox"),
+      screen.getByRole("option", { name: /oregon/i })
+    );
+
+    const downloadButton = screen.getByRole("button", {
+      name: /download expungement packet/i,
+    });
+
+    await user.click(downloadButton);
+
+    expect(screen.queryByText(/incomplete/i)).not.toBeInTheDocument;
+  });
+});
+
+// possible test for modal pop and successful download when mailingAddress or City is not filled
+describe("test if certain fields left empty will still trigger the modal and a download", () => {
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <FillForms />
+        </MemoryRouter>
+      </Provider>
+    );
+  });
+
+  it("modal and download cannot happen at the same time when city field isnt completed", async () => {
+    const user = userEvent.setup();
+
+    await user.click(screen.getByLabelText(/full name/i));
+    await user.keyboard("username");
+    await user.click(screen.getByLabelText(/birth/i));
+    await user.keyboard("12/12/1999");
+    await user.click(screen.getByLabelText(/zip code/i));
+    await user.keyboard("12345");
+    await user.click(screen.getByLabelText(/phone/i));
+    await user.keyboard("123-456-7890");
+    await user.click(screen.getByLabelText(/address/i));
+    await user.keyboard("1111 NE anywhere");
+    await user.selectOptions(
+      screen.getByRole("combobox"),
+      screen.getByRole("option", { name: /oregon/i })
+    );
+    const downloadButton = screen.getByRole("button", {
+      name: /download expungement packet/i,
+    });
+
+    await user.click(downloadButton);
+
+    expect(mockDownloadExpungementPacketFunc).not.toHaveBeenCalled();
+    expect(screen.queryByText(/incomplete/i)).not.toBeNull();
+  });
+
+  it("modal and downlaod cannot happen at the same time when mailing address field isnt completed", async () => {
+    const user = userEvent.setup();
+
+    await user.click(screen.getByLabelText(/full name/i));
+    await user.keyboard("username");
+    await user.click(screen.getByLabelText(/birth/i));
+    await user.keyboard("12/12/1999");
+    await user.click(screen.getByLabelText(/zip code/i));
+    await user.keyboard("12345");
+    await user.click(screen.getByLabelText(/phone/i));
+    await user.keyboard("123-456-7890");
+    await user.click(screen.getByLabelText(/city/i));
+    await user.keyboard("Portland");
+    await user.selectOptions(
+      screen.getByRole("combobox"),
+      screen.getByRole("option", { name: /oregon/i })
+    );
+
+    const downloadButton = screen.getByRole("button", {
+      name: /download expungement packet/i,
+    });
+
+    await user.click(downloadButton);
+    expect(mockDownloadExpungementPacketFunc).not.toHaveBeenCalled();
+    expect(screen.queryByText(/incomplete/i)).not.toBeNull();
   });
 });
