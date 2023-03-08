e0f32089b4ef7479a6d1c292c97da4f897fd4a9f
monocle <monocle.github@gmail.com>
2023-03-07 18:17:22 -0800

    Remove unnecessary React fragments

--- src/frontend/src/components/FillForms/EmptyFieldsModal.tsx
+++ src/frontend/src/components/FillForms/EmptyFieldsModal.tsx
@@ -17,30 +17,27 @@ const EmptyFieldsModal: React.FC<Props> = ({
   }
 
   return !close ? (
-    <>
-      <div className="fixed top-0 bottom-0 left-0 right-0 z-9999 bg-black-30">
-        <div className="modalContainer flex flex-column tc mw7 w-100 fixed z-9999 bg-white shadow-5 black f6 f5-l fw6 br2 pb2 pt3">
-          <p>
-            You are about to download an incomplete form. Do you want to
-            continue?
-          </p>
-          <div className="flex justify-center pa2 mt3">
-            <button
-              onClick={clickDownload}
-              className="bg-blue white bg-animate hover-bg-dark-blue fw6 db w-100 br2 pv3 ph4 tc w-100 ma2"
-            >
-              Download
-            </button>
-            <button
-              onClick={onClose}
-              className="ba bw2 b--light-gray bg-white black bg-animate hover-bg-light-gray fw6 db w-100 br2 pv3 ph4 tc w-100 ma2"
-            >
-              Cancel
-            </button>
-          </div>
+    <div className="fixed top-0 bottom-0 left-0 right-0 z-9999 bg-black-30">
+      <div className="modalContainer flex flex-column tc mw7 w-100 fixed z-9999 bg-white shadow-5 black f6 f5-l fw6 br2 pb2 pt3">
+        <p>
+          You are about to download an incomplete form. Do you want to continue?
+        </p>
+        <div className="flex justify-center pa2 mt3">
+          <button
+            onClick={clickDownload}
+            className="bg-blue white bg-animate hover-bg-dark-blue fw6 db w-100 br2 pv3 ph4 tc w-100 ma2"
+          >
+            Download
+          </button>
+          <button
+            onClick={onClose}
+            className="ba bw2 b--light-gray bg-white black bg-animate hover-bg-light-gray fw6 db w-100 br2 pv3 ph4 tc w-100 ma2"
+          >
+            Cancel
+          </button>
         </div>
       </div>
-    </>
+    </div>
   ) : null;
 };
 
