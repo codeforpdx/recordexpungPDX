8e3a16c76d6b7e883a08c33534b9799d2641b2b0
Treasure Porth <treasure@scrimba.com>
2023-03-22 13:31:41 -0700

    Add button to header

--- src/frontend/src/components/Header/index.tsx
+++ src/frontend/src/components/Header/index.tsx
@@ -1,36 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import { Link, useLocation } from "react-router-dom";
 import Logo from "../Logo";
+import { isLoggedIn, oeciLogout } from "../../service/cookie-service";
 
-export default class Header extends React.Component {
-  public render() {
-    return (
-      <div className="bg-white shadow">
-        <nav 
-          className="mw8 relative center flex flex-wrap justify-between pa3"
-          aria-label="Primary"
-        >
-          <div className="logo mb4 mb0-ns">
-            <Link to="/" aria-label="Home">
-              <Logo />
-            </Link>
-          </div>
-          <div className="mt5 mt2-ns">
-            <Link
-              to="/manual"
-              className="link hover-blue f5 fw6 pv2 ph0 ph3-ns mr4-ns"
+
+export default function Header (){
+  const [loggedIn, setLoggedIn] = useState(isLoggedIn());
+  const location = useLocation();
+
+  useEffect(() => {
+    setLoggedIn(isLoggedIn());
+  }, [location]);
+  
+  return (
+    <div className="bg-white shadow">
+      <nav 
+        className="mw8 relative center flex flex-wrap justify-between pa3"
+        aria-label="Primary"
+      >
+        <div className="logo mb4 mb0-ns">
+          <Link to="/" aria-label="Home">
+            <Logo />
+          </Link>
+        </div>
+        <div className="mt5 mt2-ns">
+          <Link
+            to="/manual"
+            className="link hover-blue f5 fw6 pv2 ph0 ph3-ns mr4-ns"
+          >
+            Manual
+          </Link>
+          <Link
+            to="/record-search"
+            className="absolute top-1 right-1 static-ns bg-blue white bg-animate hover-bg-dark-blue f5 fw6 br2 pv2 ph3"
+          >
+            Search
+          </Link>
+          {loggedIn && (
+            <button
+              onClick={oeciLogout}
+              className="absolute top-1 left-2 static-ns bg-white f5 fw6 br2 ba b--blue blue link hover-dark-blue pv2 ph3 ml2"
             >
-              Manual
-            </Link>
-            <Link
-              to="/record-search"
-              className="absolute top-1 right-1 static-ns bg-blue white bg-animate hover-bg-dark-blue f5 fw6 br2 pv2 ph3"
-            >
-              Search
-            </Link>
-          </div>
-        </nav>
-      </div>
-    );
-  }
+              Log Out
+            </button>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
 }
