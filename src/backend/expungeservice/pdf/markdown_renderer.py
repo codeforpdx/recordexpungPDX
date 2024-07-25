12505621d59f894fd1d4866fda6e317a4859f2f3
ekong5 <ekong5@illinois.edu>
2024-07-25 14:30:43 -0700

    resolved some style issues

--- src/backend/expungeservice/pdf/markdown_renderer.py
+++ src/backend/expungeservice/pdf/markdown_renderer.py
@@ -22,8 +22,8 @@ class MarkdownRenderer:
         ]
         
         county_fines = [x for x in record["summary"]["county_fines"]]
-        #print(record["summary"]["county_fines"])
-        fines_total = record["summary"]["total_fines_due"]
+        
+        #fines_total = record["summary"]["total_fines_due"]
         eligible_charges_by_date = record["summary"]["charges_grouped_by_eligibility_and_case"]
         future_eligible_charges = [
             (key, eligible_charges_for_date)
