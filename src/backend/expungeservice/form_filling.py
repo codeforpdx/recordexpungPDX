35361b14a615a4b51dc6b53d2c91dcc13ee8008e
emko-274 <emmakg99@gmail.com>
2024-07-26 20:57:48 -0700

    Update form_filling.py

--- src/backend/expungeservice/form_filling.py
+++ src/backend/expungeservice/form_filling.py
@@ -396,11 +396,12 @@ class PDFFieldMapper(UserDict):
             # Is the OSP Form
             osp_fields: Dict[str, object] = {}
             for i in range(10):
-                osp_fields[f"(Court {i+1})"] = f"Circuit Court for {s.counties_with_cases_to_expunge[i]} County" if i<len(s.counties_with_cases_to_expunge) else ""
-            if s.has_eligible_convictions:
+                #osp_fields[f"(Court {i+1})"] = f"Circuit Court for {s.counties_with_cases_to_expunge[i]} County" if i<len(s.counties_with_cases_to_expunge) else ""
+                osp_fields[f"(Court {i+1})"] = ""
+            """if s.has_eligible_convictions:
                 osp_fields["(Include a Conviction Yes)"] = True
             else:
-                osp_fields["(Include a Conviction No)"] = True
+                osp_fields["(Include a Conviction No)"] = True"""
             return osp_fields
         return {
             "(FOR THE COUNTY OF)": s.county,
@@ -695,7 +696,7 @@ class FormFilling:
 
         return write_file_path, write_file_name
 
-    @staticmethod
+    """@staticmethod
     def counties_with_cases_to_expunge(all_case_results: List[CaseResults]):
         counties_with_eligible_charge: List[str] = []
         counties_with_balances: List[str] = []
@@ -705,4 +706,4 @@ class FormFilling:
             if case_result.case.summary.balance_due_in_cents != 0:
                 counties_with_balances.append(case_result.case.summary.location)
         counties_with_expungements = [county for county in counties_with_eligible_charge if county not in counties_with_balances]
-        return counties_with_expungements
+        return counties_with_expungements"""
