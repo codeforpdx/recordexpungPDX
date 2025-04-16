cabb2d1ac99abccdcd7dd8b24eb7bbc197fddf06
Jordan Witte <wittejm@gmail.com>
2025-04-16 13:20:45 -0700

    wrap explain text, smarter buttons, fix adobe render issue

--- src/backend/expungeservice/waiver_form_filling.py
+++ src/backend/expungeservice/waiver_form_filling.py
@@ -3,13 +3,27 @@ from tempfile import mkdtemp
 from os import path
 from pathlib import Path
 from typing import Dict, Tuple
-from pdfrw import PdfReader, PdfDict, PdfString, PdfWriter, PdfName
+from pdfrw import PdfReader, PdfDict, PdfString, PdfWriter, PdfName, PdfObject
 from zipfile import ZipFile
 from datetime import date
 
 from expungeservice.models.record_summary import RecordSummary
 from expungeservice.models.case import Case
 from expungeservice.form_filling import DA_ADDRESSES
+
+def wrap_text(text):
+    lines = []
+    while text:
+        if len(text) <= 90:
+            lines.append(text)
+            break
+        split_index = text.find(' ', 90)
+        if split_index == -1:
+            lines.append(text)
+            break
+        lines.append(text[:split_index])
+        text = text[split_index+1:]
+    return lines[:4]
 
 @dataclass
 class CaseData:
@@ -22,6 +36,8 @@ def get_mapping(case_data: CaseData):
     benefit = case_data.waiver_information_dict["snap"] or case_data.waiver_information_dict["snap"] or case_data.waiver_information_dict["snap"] or case_data.waiver_information_dict["snap"]
     today = date.today().strftime("%b %-d, %Y")
     da_address_lines = DA_ADDRESSES[case_data.case.summary.location.lower()].split("-")
+    explain_lines = wrap_text(case_data.waiver_information_dict["explain"])
+    ask_lines = wrap_text(case_data.waiver_information_dict["explain2"])
     mapping = {
         "(Case No)": case_data.case.summary.case_number,
         "(Defendant)":case_data.case.summary.name,
@@ -32,14 +48,14 @@ def get_mapping(case_data: CaseData):
         "(SSI)": case_data.waiver_information_dict["ssi"],
         "(TANF)": case_data.waiver_information_dict["tanf"],
         "(OHP)": case_data.waiver_information_dict["ohp"],
-        "(Explain1)": case_data.waiver_information_dict["explain"][:70],
-        "(Explain2)": case_data.waiver_information_dict["explain"][70:140],
-        "(Explain3)": case_data.waiver_information_dict["explain"][140:210],
-        "(Explain4)": case_data.waiver_information_dict["explain"][210:280],
-        "(Ask1)": case_data.waiver_information_dict["explain2"][:70],
-        "(Ask2)": case_data.waiver_information_dict["explain2"][70:140],
-        "(Ask3)": case_data.waiver_information_dict["explain2"][140:210],
-        "(Ask4)": case_data.waiver_information_dict["explain2"][210:270],
+        "(Explain1)": explain_lines[0] if len(explain_lines) > 0 else "",
+        "(Explain2)": explain_lines[1] if len(explain_lines) > 1 else "",
+        "(Explain3)": explain_lines[2] if len(explain_lines) > 2 else "",
+        "(Explain4)": explain_lines[3] if len(explain_lines) > 3 else "",
+        "(Ask1)": ask_lines[0] if len(ask_lines) > 0 else "",
+        "(Ask2)": ask_lines[1] if len(ask_lines) > 1 else "",
+        "(Ask3)": ask_lines[2] if len(ask_lines) > 2 else "",
+        "(Ask4)": ask_lines[3] if len(ask_lines) > 3 else "",
         "(Custody)": case_data.waiver_information_dict["custody"],
         "(Name)": case_data.user_information_dict["full_name"],
         "(Address)": case_data.user_information_dict["mailing_address"],
@@ -88,13 +104,15 @@ class WaiverFormFilling:
         pdf = PdfReader(pdf_path)
 
         acroform = pdf.Root.AcroForm
-        for key, val in acroform.items():
-            print(f"Key: {key} (type: {type(key)}), Value: {val} (type: {type(val)})")
+        acroform.update(
+        PdfDict(NeedAppearances=PdfObject('true'))
+    )
+        #for key, val in acroform.items():
+        #    print(f"Key: {key} (type: {type(key)}), Value: {val} (type: {type(val)})")
 
         fields = acroform['/Fields']
         for field in fields:
             field_name = field.get('/T')
-            print(f"Field Name: {field_name}")
             mapping = get_mapping(case_data)
             value = mapping[field_name]
             if isinstance(value, str):
@@ -102,10 +120,11 @@ class WaiverFormFilling:
                     V=PdfString.encode(value),
                     DV=PdfString.encode(value)
                 ))
+                field.update(PdfDict(AP=""))
+
             elif isinstance(value, bool):
                 if value:
                     if PdfName("On") in field.AP.N.keys():
-                    # Common "On" value is usually "Yes", but you can inspect field['/AP']['/N'].keys() for actual value
                         on_value = PdfName("On")
                     elif PdfName("Yes") in field.AP.N.keys():
                         on_value = PdfName('Yes')
